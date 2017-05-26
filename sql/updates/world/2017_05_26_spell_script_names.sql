ee87e7e5df7cb32a87e7d5aa04a7881e5d9e4b6a
Afgann <Marcin60000@gmail.com>
2017-05-26 21:24:49 +0200

    Molten Core - Magmadar update.

---
+++ sql/updates/world/2017_05_26_spell_script_names.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName` = "spell_magmadar_lava_bomb";
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(19411, "spell_magmadar_lava_bomb");
--- src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/boss_magmadar.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/boss_magmadar.cpp
@@ -1,28 +1,3 @@
-/*
- * Copyright (C) 
- * Copyright (C) 
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
-/* ScriptData
-SDName: Boss_Magmadar
-SD%Complete: 75
-SDComment: Conflag on ground nyi
-SDCategory: Molten Core
-EndScriptData */
-
 #include "ObjectMgr.h"
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
@@ -38,14 +13,15 @@ enum Spells
     SPELL_FRENZY        = 19451,
     SPELL_MAGMA_SPIT    = 19449,
     SPELL_PANIC         = 19408,
-    SPELL_LAVA_BOMB     = 19428,
+    SPELL_LAVA_BOMB     = 19411,
+    SPELL_SUMMON_FLAME  = 20494
 };
 
 enum Events
 {
     EVENT_FRENZY        = 1,
-    EVENT_PANIC         = 2,
-    EVENT_LAVA_BOMB     = 3,
+    EVENT_PANIC,
+    EVENT_LAVA_BOMB
 };
 
 class boss_magmadar : public CreatureScript
@@ -55,51 +31,46 @@ class boss_magmadar : public CreatureScript
 
         struct boss_magmadarAI : public BossAI
         {
-            boss_magmadarAI(Creature* creature) : BossAI(creature, BOSS_MAGMADAR)
-            {
-            }
+            boss_magmadarAI(Creature* creature) : BossAI(creature, BOSS_MAGMADAR) { }
 
-            void Reset()
+            void Reset() override
             {
-                BossAI::Reset();
+                _Reset();
                 DoCast(me, SPELL_MAGMA_SPIT, true);
             }
 
-            void EnterCombat(Unit* victim)
+            void EnterCombat(Unit* /*victim*/) override
             {
-                BossAI::EnterCombat(victim);
+                _EnterCombat();
                 events.ScheduleEvent(EVENT_FRENZY, 30000);
-                events.ScheduleEvent(EVENT_PANIC, 20000);
+                events.ScheduleEvent(EVENT_PANIC, 7000);
                 events.ScheduleEvent(EVENT_LAVA_BOMB, 12000);
             }
 
-            void UpdateAI(uint32 diff)
+            void UpdateAI(uint32 diff) override
             {
                 if (!UpdateVictim())
                     return;
 
                 events.Update(diff);
 
-                if (me->HasUnitState(UNIT_STATE_CASTING))
-                    return;
-
-                while (uint32 eventId = events.ExecuteEvent())
+                while (uint32 eventId = events.GetEvent())
                 {
                     switch (eventId)
                     {
                         case EVENT_FRENZY:
                             Talk(EMOTE_FRENZY);
                             DoCast(me, SPELL_FRENZY);
-                            events.ScheduleEvent(EVENT_FRENZY, 15000);
+                            events.RepeatEvent(urand(15000, 20000));
                             break;
                         case EVENT_PANIC:
                             DoCastVictim(SPELL_PANIC);
-                            events.ScheduleEvent(EVENT_PANIC, 35000);
+                            events.RepeatEvent(urand(30000, 40000));
                             break;
                         case EVENT_LAVA_BOMB:
-                            if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true, -SPELL_LAVA_BOMB))
+                            if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true))
                                 DoCast(target, SPELL_LAVA_BOMB);
-                            events.ScheduleEvent(EVENT_LAVA_BOMB, 12000);
+                            events.RepeatEvent(urand(12000, 15000));
                             break;
                         default:
                             break;
@@ -110,13 +81,49 @@ class boss_magmadar : public CreatureScript
             }
         };
 
-        CreatureAI* GetAI(Creature* creature) const
+        CreatureAI* GetAI(Creature* creature) const override
         {
             return new boss_magmadarAI(creature);
         }
 };
 
+class spell_magmadar_lava_bomb : public SpellScriptLoader
+{
+    public:
+        spell_magmadar_lava_bomb() : SpellScriptLoader("spell_magmadar_lava_bomb") { }
+
+        class spell_magmadar_lava_bomb_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_magmadar_lava_bomb_SpellScript);
+
+            bool Validate(SpellInfo const* /*spellInfo*/) override
+            {
+                if (!sSpellMgr->GetSpellInfo(SPELL_LAVA_BOMB) || !sSpellMgr->GetSpellInfo(SPELL_SUMMON_FLAME))
+                    return false;
+                return true;
+            }
+
+            void HandleScript(SpellEffIndex /*effIndex*/)
+            {
+                if (Unit* target = GetHitUnit())
+                    target->CastSpell(target, SPELL_SUMMON_FLAME, true);
+            }
+
+            void Register() override
+            {
+                OnEffectHitTarget += SpellEffectFn(spell_magmadar_lava_bomb_SpellScript::HandleScript, EFFECT_0, SPELL_EFFECT_DUMMY);
+            }
+        };
+
+        SpellScript* GetSpellScript() const override
+        {
+            return new spell_magmadar_lava_bomb_SpellScript();
+        }
+};
+
+
 void AddSC_boss_magmadar()
 {
     new boss_magmadar();
+    new spell_magmadar_lava_bomb();
 }
