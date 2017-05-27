182d62d2f38041bcd4de4ee3c33a1da9d5e5b96b
Afgann <Marcin60000@gmail.com>
2017-05-28 00:19:17 +0200

    Blackwing Lair - Ebonroc, Firemaw and Flamegor update.

--- src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_ebonroc.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_ebonroc.cpp
@@ -1,27 +1,10 @@
-/*
- * Copyright (C) 
- * Copyright (C) 
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
 #include "blackwing_lair.h"
 
 enum Spells
 {
+    SPELL_THRASH                = 3391,
     SPELL_SHADOWFLAME           = 22539,
     SPELL_WINGBUFFET            = 23339,
     SPELL_SHADOWOFEBONROC       = 23340
@@ -30,44 +13,35 @@ enum Spells
 enum Events
 {
     EVENT_SHADOWFLAME           = 1,
-    EVENT_WINGBUFFET            = 2,
-    EVENT_SHADOWOFEBONROC       = 3
+    EVENT_WINGBUFFET,
+    EVENT_SHADOWOFEBONROC,
+    EVENT_THRASH
 };
 
 class boss_ebonroc : public CreatureScript
 {
-public:
-    boss_ebonroc() : CreatureScript("boss_ebonroc") { }
+    public:
+        boss_ebonroc() : CreatureScript("boss_ebonroc") { }
 
-    struct boss_ebonrocAI : public BossAI
-    {
-        boss_ebonrocAI(Creature* creature) : BossAI(creature, BOSS_EBONROC) { }
-
-        void EnterCombat(Unit* /*who*/)
+        struct boss_ebonrocAI : public BossAI
         {
-            if (instance->GetBossState(BOSS_BROODLORD) != DONE)
+            boss_ebonrocAI(Creature* creature) : BossAI(creature, BOSS_EBONROC) { }
+
+            void EnterCombat(Unit* /*who*/)
             {
-                EnterEvadeMode();
-                return;
+                if (instance->GetBossState(BOSS_BROODLORD) != DONE)
+                {
+                    EnterEvadeMode();
+                    return;
+                }
+                _EnterCombat();
+
+                events.ScheduleEvent(EVENT_SHADOWFLAME, urand(10000, 20000));
+                events.ScheduleEvent(EVENT_WINGBUFFET, 30000);
+                events.ScheduleEvent(EVENT_SHADOWOFEBONROC, urand(8000, 10000));
             }
-            _EnterCombat();
 
-            events.ScheduleEvent(EVENT_SHADOWFLAME, urand(10000, 20000));
-            events.ScheduleEvent(EVENT_WINGBUFFET, 30000);
-            events.ScheduleEvent(EVENT_SHADOWOFEBONROC, urand(8000, 10000));
-        }
-
-        void UpdateAI(uint32 diff)
-        {
-            if (!UpdateVictim())
-                return;
-
-            events.Update(diff);
-
-            if (me->HasUnitState(UNIT_STATE_CASTING))
-                return;
-
-            while (uint32 eventId = events.ExecuteEvent())
+            void ExecuteEvent(uint32 eventId) override
             {
                 switch (eventId)
                 {
@@ -83,17 +57,20 @@ public:
                         DoCastVictim(SPELL_SHADOWOFEBONROC);
                         events.ScheduleEvent(EVENT_SHADOWOFEBONROC, urand(8000, 10000));
                         break;
+                    case EVENT_THRASH:
+                        DoCast(me, SPELL_THRASH);
+                        events.ScheduleEvent(EVENT_THRASH, 18000);
+                        break;
+                    default:
+                        break;
                 }
             }
+        };
 
-            DoMeleeAttackIfReady();
+        CreatureAI* GetAI(Creature* creature) const
+        {
+            return GetInstanceAI<boss_ebonrocAI>(creature);
         }
-    };
-
-    CreatureAI* GetAI(Creature* creature) const
-    {
-        return GetInstanceAI<boss_ebonrocAI>(creature);
-    }
 };
 
 void AddSC_boss_ebonroc()
--- src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_firemaw.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_firemaw.cpp
@@ -1,27 +1,10 @@
-/*
- * Copyright (C) 
- * Copyright (C) 
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
 #include "blackwing_lair.h"
 
 enum Spells
 {
+    SPELL_THRASH            = 3391,
     SPELL_SHADOWFLAME       = 22539,
     SPELL_WINGBUFFET        = 23339,
     SPELL_FLAMEBUFFET       = 23341
@@ -30,44 +13,36 @@ enum Spells
 enum Events
 {
     EVENT_SHADOWFLAME       = 1,
-    EVENT_WINGBUFFET        = 2,
-    EVENT_FLAMEBUFFET       = 3
+    EVENT_WINGBUFFET,
+    EVENT_FLAMEBUFFET,
+    EVENT_THRASH
 };
 
 class boss_firemaw : public CreatureScript
 {
-public:
-    boss_firemaw() : CreatureScript("boss_firemaw") { }
+    public:
+        boss_firemaw() : CreatureScript("boss_firemaw") { }
 
-    struct boss_firemawAI : public BossAI
-    {
-        boss_firemawAI(Creature* creature) : BossAI(creature, BOSS_FIREMAW) { }
-
-        void EnterCombat(Unit* /*who*/)
+        struct boss_firemawAI : public BossAI
         {
-            if (instance->GetBossState(BOSS_BROODLORD) != DONE)
+            boss_firemawAI(Creature* creature) : BossAI(creature, BOSS_FIREMAW) { }
+
+            void EnterCombat(Unit* /*who*/)
             {
-                EnterEvadeMode();
-                return;
+                if (instance->GetBossState(BOSS_BROODLORD) != DONE)
+                {
+                    EnterEvadeMode();
+                    return;
+                }
+                _EnterCombat();
+
+                events.ScheduleEvent(EVENT_SHADOWFLAME, urand(10000, 20000));
+                events.ScheduleEvent(EVENT_WINGBUFFET, 30000);
+                events.ScheduleEvent(EVENT_FLAMEBUFFET, 5000);
+                events.ScheduleEvent(EVENT_THRASH, 5000);
             }
-            _EnterCombat();
 
-            events.ScheduleEvent(EVENT_SHADOWFLAME, urand(10000, 20000));
-            events.ScheduleEvent(EVENT_WINGBUFFET, 30000);
-            events.ScheduleEvent(EVENT_FLAMEBUFFET, 5000);
-        }
-
-        void UpdateAI(uint32 diff)
-        {
-            if (!UpdateVictim())
-                return;
-
-            events.Update(diff);
-
-            if (me->HasUnitState(UNIT_STATE_CASTING))
-                return;
-
-            while (uint32 eventId = events.ExecuteEvent())
+            void ExecuteEvent(uint32 eventId) override
             {
                 switch (eventId)
                 {
@@ -85,17 +60,20 @@ public:
                         DoCastVictim(SPELL_FLAMEBUFFET);
                         events.ScheduleEvent(EVENT_FLAMEBUFFET, 5000);
                         break;
+                    case EVENT_THRASH:
+                        DoCast(me, SPELL_THRASH);
+                        events.ScheduleEvent(EVENT_THRASH, 18000);
+                        break;
+                    default:
+                        break;
                 }
             }
+        };
 
-            DoMeleeAttackIfReady();
+        CreatureAI* GetAI(Creature* creature) const
+        {
+            return GetInstanceAI<boss_firemawAI>(creature);
         }
-    };
-
-    CreatureAI* GetAI(Creature* creature) const
-    {
-        return GetInstanceAI<boss_firemawAI>(creature);
-    }
 };
 
 void AddSC_boss_firemaw()
