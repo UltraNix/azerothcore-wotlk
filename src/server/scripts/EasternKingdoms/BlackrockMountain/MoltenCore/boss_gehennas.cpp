a5db637863467104cc6680920a20229e0035ce50
Afgann <Marcin60000@gmail.com>
2017-05-27 18:27:17 +0200

    Molten Core - Gehennas Update.

--- src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/boss_gehennas.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/boss_gehennas.cpp
@@ -1,28 +1,3 @@
-/*
- * Copyright (C) 
- * Copyright (C) 
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
-/* ScriptData
-SDName: Boss_Gehennas
-SD%Complete: 90
-SDComment: Adds MC NYI
-SDCategory: Molten Core
-EndScriptData */
-
 #include "ObjectMgr.h"
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
@@ -30,16 +5,17 @@ EndScriptData */
 
 enum Spells
 {
-    SPELL_GEHENNAS_CURSE    = 19716,
-    SPELL_RAIN_OF_FIRE      = 19717,
-    SPELL_SHADOW_BOLT       = 19728,
+    SPELL_GEHENNAS_CURSE        = 19716,
+    SPELL_RAIN_OF_FIRE          = 19717,
+    SPELL_SHADOW_BOLT           = 19728,
 };
 
 enum Events
 {
-    EVENT_GEHENNAS_CURSE    = 1,
-    EVENT_RAIN_OF_FIRE      = 2,
-    EVENT_SHADOW_BOLT       = 3,
+    EVENT_GEHENNAS_CURSE        = 1,
+    EVENT_RAIN_OF_FIRE,
+    EVENT_SHADOW_BOLT_RANDOM,
+    EVENT_SHADOW_BOLT_VICTIM
 };
 
 class boss_gehennas : public CreatureScript
@@ -49,56 +25,62 @@ class boss_gehennas : public CreatureScript
 
         struct boss_gehennasAI : public BossAI
         {
-            boss_gehennasAI(Creature* creature) : BossAI(creature, BOSS_GEHENNAS)
+            boss_gehennasAI(Creature* creature) : BossAI(creature, BOSS_GEHENNAS) { }
+
+            void EnterCombat(Unit* /*victim*/)
             {
+                _EnterCombat();
+                events.ScheduleEvent(EVENT_GEHENNAS_CURSE, urand(5000, 10000));
+                events.ScheduleEvent(EVENT_RAIN_OF_FIRE, urand(6000, 12000));
+                events.ScheduleEvent(EVENT_SHADOW_BOLT_RANDOM, urand(3000, 6000));
+                events.ScheduleEvent(EVENT_SHADOW_BOLT_VICTIM, urand(3000, 6000));
             }
 
-            void EnterCombat(Unit* victim)
+            void EnterEvadeMode() override
             {
-                BossAI::EnterCombat(victim);
-                events.ScheduleEvent(EVENT_GEHENNAS_CURSE, 12000);
-                events.ScheduleEvent(EVENT_RAIN_OF_FIRE, 10000);
-                events.ScheduleEvent(EVENT_SHADOW_BOLT, 6000);
-            }
-
-            void UpdateAI(uint32 diff)
-            {
-                if (!UpdateVictim())
-                    return;
-
-                events.Update(diff);
-
-                if (me->HasUnitState(UNIT_STATE_CASTING))
-                    return;
-
-                while (uint32 eventId = events.ExecuteEvent())
+                std::list<Creature*> addList;
+                me->GetCreatureListWithEntryInGrid(addList, 11661, 100.0f);
+                if (!addList.empty())
                 {
-                    switch (eventId)
+                    for (auto itr : addList)
                     {
-                        case EVENT_GEHENNAS_CURSE:
-                            DoCastVictim(SPELL_GEHENNAS_CURSE);
-                            events.ScheduleEvent(EVENT_GEHENNAS_CURSE, urand(22000, 30000));
-                            break;
-                        case EVENT_RAIN_OF_FIRE:
-                            if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0))
-                                DoCast(target, SPELL_RAIN_OF_FIRE);
-                            events.ScheduleEvent(EVENT_RAIN_OF_FIRE, urand(4000, 12000));
-                            break;
-                        case EVENT_SHADOW_BOLT:
-                            if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 1))
-                                DoCast(target, SPELL_SHADOW_BOLT);
-                            events.ScheduleEvent(EVENT_SHADOW_BOLT, 7000);
-                            break;
-                        default:
-                            break;
+                        if (!itr->IsAlive())
+                            itr->Respawn();
+                        if (itr->IsAIEnabled)
+                            itr->AI()->EnterEvadeMode();
                     }
                 }
+                CreatureAI::EnterEvadeMode();
+            }
 
-                DoMeleeAttackIfReady();
+            void ExecuteEvent(uint32 eventId) override
+            {
+                switch (eventId)
+                {
+                    case EVENT_GEHENNAS_CURSE:
+                        DoCastVictim(SPELL_GEHENNAS_CURSE);
+                        events.ScheduleEvent(EVENT_GEHENNAS_CURSE, urand(25000, 30000));
+                        break;
+                    case EVENT_RAIN_OF_FIRE:
+                        if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0))
+                            DoCast(target, SPELL_RAIN_OF_FIRE);
+                        events.ScheduleEvent(EVENT_RAIN_OF_FIRE, urand(6000, 12000));
+                        break;
+                    case EVENT_SHADOW_BOLT_RANDOM:
+                        if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 1))
+                            DoCast(target, SPELL_SHADOW_BOLT);
+                        events.ScheduleEvent(EVENT_SHADOW_BOLT_RANDOM, urand(3000, 6000));
+                        break;
+                    case EVENT_SHADOW_BOLT_VICTIM:
+                        DoCastVictim(SPELL_SHADOW_BOLT);
+                        events.ScheduleEvent(EVENT_SHADOW_BOLT_VICTIM, urand(3000, 6000));
+                    default:
+                        break;
+                }
             }
         };
 
-        CreatureAI* GetAI(Creature* creature) const
+        CreatureAI* GetAI(Creature* creature) const override
         {
             return new boss_gehennasAI(creature);
         }
