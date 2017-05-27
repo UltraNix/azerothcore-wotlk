fc05040495c1610aa609bc806c6250d169290d25
crackaw <crackaw@outlook.com>
2017-05-27 13:30:48 +0200

    issue #1012 Mana Drain

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -6552,7 +6552,7 @@ void AuraEffect::HandlePeriodicManaLeechAuraTick(Unit* target, Unit* caster) con
     target->AddThreat(caster, float(gainedAmount) * 0.5f, GetSpellInfo()->GetSchoolMask(), GetSpellInfo());
     
     // remove CC auras
-    target->RemoveAurasWithInterruptFlags(AURA_INTERRUPT_FLAG_TAKE_DAMAGE);
+    target->RemoveAurasWithInterruptFlags(0x480000);//Fear AuraInterruptFlags (TODO: Enum using)
 
     // Drain Mana
     if (m_spellInfo->SpellFamilyName == SPELLFAMILY_WARLOCK
