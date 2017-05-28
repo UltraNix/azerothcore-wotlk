da5fbf13012668d7907ac0e65252d5de2a2c3952
Afgann <Marcin60000@gmail.com>
2017-05-28 20:36:04 +0200

    Blackwing Lair - Vaelastrasz update.

--- sql/updates/world/2017_05_27_spell_script_names.sql
+++ sql/updates/world/2017_05_27_spell_script_names.sql
@@ -1,3 +1,6 @@
 DELETE FROM `spell_script_names` WHERE `ScriptName` = "spell_chromaggus_brood_affliction";
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (23173, "spell_chromaggus_brood_affliction");
+DELETE FROM `spell_script_names` WHERE `ScriptName` = "spell_vael_burning_adrenaline";
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(18173, "spell_vael_burning_adrenaline");
