1fb79a0804855260e3694784612cbe1b3bea5a4a
peyton <admin@darklab.pl>
2017-05-31 03:54:28 +0200

    sql update typo.

--- sql/updates/world/2017_05_31_disables.sql
+++ sql/updates/world/2017_05_31_disables.sql
@@ -1,3 +1,3 @@
-DELETE FROM `world`.`disables` WHERE `disables`.`entry` = 23605;
+DELETE FROM `disables` WHERE `disables`.`entry` = 23605;
 INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES
 (0, 23605, 1, '', '', 'Spell for deprecated item');
