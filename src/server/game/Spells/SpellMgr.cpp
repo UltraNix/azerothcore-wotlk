b317dc1b81f0f98df368b63601487c8fc4f886eb
peyton <admin@darklab.pl>
2017-06-01 13:14:48 +0200

    Cleave, Cat swipe 8 yards jump radius.

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3557,9 +3557,6 @@ void SpellMgr::LoadDbcDataCorrections()
         case 8983:  // Druid - Bash  - R3
             spellInfo->AttributesEx7 |= SPELL_ATTR7_INTERRUPT_ONLY_NONPLAYER;
             break;
-        case 62078: // Druid - Swipe (Cat)
-            spellInfo->rangeIndex = 137; // 8yd
-            break;
         /////////////////////////////////
         ///// PALADIN
         /////////////////////////////////
