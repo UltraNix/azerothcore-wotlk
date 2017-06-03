51372ee245f7579a86f5e5ae9cf5fb52190ba594
Afgann <Marcin60000@gmail.com>
2017-06-03 21:43:55 +0200

    SAI flags correction.

--- sql/updates/world/2017_06_03_wailing_caverns.sql
+++ sql/updates/world/2017_06_03_wailing_caverns.sql
@@ -3,14 +3,14 @@ SET @ENTRY := 3638;
 UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,2,0,100,3,0,50,0,0,11,7952,0,0,0,0,0,1,0,0,0,0,0,0,0,"Devouring Ectoplasm - Between 0-50% Health - Cast 'Clone' (No Repeat) (Normal Dungeon)");
+(@ENTRY,0,0,0,2,0,100,1,0,50,0,0,11,7952,0,0,0,0,0,1,0,0,0,0,0,0,0,"Devouring Ectoplasm - Between 0-50% Health - Cast 'Clone' (No Repeat)");
 
 -- Deviate Stalker SAI
 SET @ENTRY := 3634;
 UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,11,0,100,3,0,0,0,0,11,22766,0,0,0,0,0,1,0,0,0,0,0,0,0,"Deviate Stalker - On Respawn - Cast 'Sneak' (No Repeat) (Normal Dungeon)");
+(@ENTRY,0,0,0,11,0,100,1,0,0,0,0,11,22766,0,0,0,0,0,1,0,0,0,0,0,0,0,"Deviate Stalker - On Respawn - Cast 'Sneak' (No Repeat)");
 
 -- Deviate Dreadfang SAI
 SET @ENTRY := 5056;
@@ -32,17 +32,13 @@ SET @ENTRY := 3672;
 UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,0,0,100,2,0,0,3400,4700,11,9532,64,0,0,0,0,2,0,0,0,0,0,0,0,"Boahn - In Combat - Cast 'Lightning Bolt' (Normal Dungeon)"),
-(@ENTRY,0,1,2,2,0,100,3,0,15,0,0,25,0,0,0,0,0,0,1,0,0,0,0,0,0,0,"Boahn - Between 0-15% Health - Flee For Assist (No Repeat) (Normal Dungeon)"),
-(@ENTRY,0,2,0,61,0,100,3,0,15,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,"Boahn - Between 0-15% Health - Say Line 0 (No Repeat) (Normal Dungeon)"),
-(@ENTRY,0,3,0,2,0,100,3,0,30,0,0,11,23381,0,0,0,0,0,1,0,0,0,0,0,0,0,"Boahn - Between 0-30% Health - Cast 'Healing Touch' (No Repeat) (Normal Dungeon)");
-DELETE FROM `creature_text` WHERE `entry`=@ENTRY;
-INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES
-(@ENTRY,0,0, "%s attempts to run away in fear!",16,0,100,0,0,0, "flee");
+(@ENTRY,0,0,0,0,0,100,0,0,0,3400,4700,11,9532,64,0,0,0,0,2,0,0,0,0,0,0,0,"Boahn - In Combat - Cast 'Lightning Bolt'"),
+(@ENTRY,0,1,0,2,0,100,1,0,15,0,0,25,1,0,0,0,0,0,1,0,0,0,0,0,0,0,"Boahn - Between 0-15% Health - Flee For Assist (No Repeat)"),
+(@ENTRY,0,2,0,2,0,100,1,0,30,0,0,11,23381,0,0,0,0,0,1,0,0,0,0,0,0,0,"Boahn - Between 0-30% Health - Cast 'Healing Touch' (No Repeat)");
 
 -- Biletoad SAI
 SET @ENTRY := 3835;
 UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,6,0,100,3,0,0,0,0,11,10251,3,0,0,0,0,1,0,0,0,0,0,0,0,"Biletoad - On Just Died - Cast 'Biletoad Infection' (No Repeat) (Normal Dungeon)");
+(@ENTRY,0,0,0,6,0,100,1,0,0,0,0,11,10251,3,0,0,0,0,1,0,0,0,0,0,0,0,"Biletoad - On Just Died - Cast 'Biletoad Infection' (No Repeat)");
