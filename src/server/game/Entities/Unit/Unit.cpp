e2a6b64c7c0582d4397a85c87e85a9b8e7c6c645
Afgann <Marcin60000@gmail.com>
2017-06-11 20:23:18 +0200

    issue #1030 - Seal of Vengeance.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -7075,19 +7075,30 @@ bool Unit::HandleDummyAuraProc(Unit* victim, uint32 damage, AuraEffect* triggere
 
                     triggered_spell_id = 31803;
 
-                    // On target with 5 stacks of Holy Vengeance direct damage is done
+                    /* Patch 3.2
+                    While the seal is active, each melee swing or ability (excluding judgements) that lands on the target will deal a percentage of weapon damage as Holy damage to the target. 
+                    This damage maxes out at 33% weapon damage with 5 applications of the debuff and scales upward evenly based on how many applications of the debuff are active.
+                    */
                     if (Aura* aur = victim->GetAura(triggered_spell_id, GetGUID()))
                     {
+                        int32 basepoints = 0;
+                        uint8 stackAmount = aur->GetStackAmount();
+                        if (stackAmount == 1) basepoints = 7;
+                        if (stackAmount == 2) basepoints = 13;
+                        if (stackAmount == 3) basepoints = 20;
+                        if (stackAmount == 4) basepoints = 26;
+                        if (stackAmount == 5) basepoints = 33;
+                        CastCustomSpell(victim, 42463, &basepoints, NULL, NULL, true, castItem, triggeredByAura);
+
                         if (aur->GetStackAmount() == 5)
                         {
                             if (stacker)
                                 aur->RefreshDuration();
 
-                            CastSpell(victim, 42463, true, castItem, triggeredByAura);
                             return true;
                         }
                     }
-
+                    
                     if (!stacker)
                         return false;
                     break;
@@ -7108,15 +7119,26 @@ bool Unit::HandleDummyAuraProc(Unit* victim, uint32 damage, AuraEffect* triggere
 
                     triggered_spell_id = 53742;
 
-                    // On target with 5 stacks of Blood Corruption direct damage is done
+                    /* Patch 3.2
+                    While the seal is active, each melee swing or ability (excluding judgements) that lands on the target will deal a percentage of weapon damage as Holy damage to the target.
+                    This damage maxes out at 33% weapon damage with 5 applications of the debuff and scales upward evenly based on how many applications of the debuff are active.
+                    */
                     if (Aura* aur = victim->GetAura(triggered_spell_id, GetGUID()))
                     {
+                        int32 basepoints = 0;
+                        uint8 stackAmount = aur->GetStackAmount();
+                        if (stackAmount == 1) basepoints = 7;
+                        if (stackAmount == 2) basepoints = 13;
+                        if (stackAmount == 3) basepoints = 20;
+                        if (stackAmount == 4) basepoints = 26;
+                        if (stackAmount == 5) basepoints = 33;
+                        CastCustomSpell(victim, 53739, &basepoints, NULL, NULL, true, castItem, triggeredByAura);
+
                         if (aur->GetStackAmount() == 5)
                         {
                             if (stacker)
                                 aur->RefreshDuration();
 
-                            CastSpell(victim, 53739, true, castItem, triggeredByAura);
                             return true;
                         }
                     }
