2871935bb788611cec6ba45ecb378eff575e9d6b
Afgann <Marcin60000@gmail.com>
2017-06-12 12:09:43 +0200

    Onyxia's Lair - Correct damage for Onyxian Whelp and Onyxian Lair Guard.

--- sql/updates/world/2017_06_12_creature_template.sql
+++ sql/updates/world/2017_06_12_creature_template.sql
@@ -1,1 +1,7 @@
-UPDATE creature_template SET Armor_mod=81799/82100 WHERE entry=10184;
+UPDATE `creature_template` SET `Armor_mod`=81799/82100 WHERE `entry`=10184;
+-- 10 man
+UPDATE `creature_template` SET `mindmg`=400, `maxdmg`=500, `dmg_multiplier`=10 WHERE `entry`=11262;
+UPDATE `creature_template` SET `mindmg`=3700, `maxdmg`=3900, `dmg_multiplier`=10 WHERE `entry`=36561;
+-- 25 man
+UPDATE `creature_template` SET `mindmg`=600, `maxdmg`=700, `dmg_multiplier`=10 WHERE `entry`=36566;
+UPDATE `creature_template` SET `mindmg`=5400, `maxdmg`=5600, `dmg_multiplier`=10 WHERE `entry`=36571;
