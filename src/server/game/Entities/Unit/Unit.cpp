c515ea2d93e84b4d2c1de9fe2a8f29f7791ca995
Monich <michalrostocki@o2.pl>
2017-06-17 17:46:18 +0200

    Battle of the Guardians, aka bugfix #991

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -9443,7 +9443,7 @@ bool Unit::Attack(Unit* victim, bool meleeAttack)
     //if (GetTypeId() == TYPEID_UNIT)
     //    ToCreature()->SetCombatStartPosition(GetPositionX(), GetPositionY(), GetPositionZ());
 
-    if (GetTypeId() == TYPEID_UNIT && !IsPet()) 
+    if (GetTypeId() == TYPEID_UNIT && !(IsPet() || IsPetLikeGuardian()))
     {
         // should not let player enter combat by right clicking target - doesn't helps
         SetInCombatWith(victim);
