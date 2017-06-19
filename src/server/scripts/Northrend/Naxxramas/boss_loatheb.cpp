010c9f4402404b7f3b870a68b716892cbd90640a
Afgann <Marcin60000@gmail.com>
2017-06-19 19:56:50 +0200

    Naxxramas (Loatheb):
    - Fix boss emotes not displaying.
    - Corrected codestyle.

--- src/server/scripts/Northrend/Naxxramas/boss_loatheb.cpp
+++ src/server/scripts/Northrend/Naxxramas/boss_loatheb.cpp
@@ -21,159 +21,132 @@ enum Spells
 enum Events
 {
     EVENT_SPELL_NECROTIC_AURA          = 1,
-    EVENT_SPELL_DEATHBLOOM             = 2,
-    EVENT_SPELL_INEVITABLE_DOOM        = 3,
-    EVENT_SPELL_BERSERK                = 4
+    EVENT_SPELL_DEATHBLOOM,
+    EVENT_SPELL_INEVITABLE_DOOM,
+    EVENT_SPELL_BERSERK,
+    EVENT_AURA_REMOVED,
+    EVENT_AURA_FADING
 };
 
 enum Texts
 {
-    SAY_NECROTIC_AURA_APPLIED          = 0,
-    SAY_NECROTIC_AURA_REMOVED          = 1,
-    SAY_NECROTIC_AURA_FADING           = 2
+    SAY_NECROTIC_AURA_APPLIED,
+    SAY_NECROTIC_AURA_REMOVED,
+    SAY_NECROTIC_AURA_FADING
 };
 
-class boss_loatheb : public CreatureScript
+struct boss_loathebAI : public ScriptedAI
 {
-public:
-    boss_loatheb() : CreatureScript("boss_loatheb") { }
-
-    CreatureAI* GetAI(Creature* pCreature) const
+    boss_loathebAI(Creature *c) : ScriptedAI(c)
     {
-        return new boss_loathebAI (pCreature);
+        pInstance = me->GetInstanceScript();
     }
 
-    struct boss_loathebAI : public ScriptedAI
+    InstanceScript* pInstance;
+    EventMap events;
+
+    void Reset()
     {
-        boss_loathebAI(Creature *c) : ScriptedAI(c)
+        events.Reset();
+
+        if (pInstance)
         {
-            pInstance = me->GetInstanceScript();
+            pInstance->SetData(EVENT_LOATHEB, NOT_STARTED);
+            if (GameObject* go = me->GetMap()->GetGameObject(pInstance->GetData64(DATA_LOATHEB_GATE)))
+                go->SetGoState(GO_STATE_ACTIVE);
+        }
+    }
+
+    void JustSummoned(Creature* cr) { cr->SetInCombatWithZone(); }
+
+    void SummonedCreatureDies(Creature* cr, Unit*)
+    {
+        if (pInstance)
+            pInstance->SetData(DATA_SPORE_KILLED, 0);
+    }
+
+    void KilledUnit(Unit* who)
+    {
+        if (who->GetTypeId() == TYPEID_PLAYER && pInstance)
+            pInstance->SetData(DATA_IMMORTAL_FAIL, 0);
+    }
+
+    void EnterCombat(Unit *who)
+    {
+        if (pInstance)
+        {
+            pInstance->SetData(EVENT_LOATHEB, IN_PROGRESS);
+            if (GameObject* go = me->GetMap()->GetGameObject(pInstance->GetData64(DATA_LOATHEB_GATE)))
+                go->SetGoState(GO_STATE_READY);
         }
 
-        InstanceScript* pInstance;
-        EventMap events;
+        me->SetInCombatWithZone();
+        events.ScheduleEvent(EVENT_SPELL_NECROTIC_AURA, 0);
+        events.ScheduleEvent(EVENT_SPELL_DEATHBLOOM, 25000);
+        events.ScheduleEvent(EVENT_SPELL_INEVITABLE_DOOM, 120000);
+        events.ScheduleEvent(EVENT_SPELL_BERSERK, 720000);
+    }
 
-        void Reset()
+    void JustDied(Unit* Killer)
+    {
+        if (pInstance)
+            pInstance->SetData(EVENT_LOATHEB, DONE);
+    }
+
+    void UpdateAI(uint32 diff)
+    {
+        if (!UpdateVictim())
+            return;
+
+        events.Update(diff);
+        if (me->HasUnitState(UNIT_STATE_CASTING))
+            return;
+
+        while (uint32 eventId = events.ExecuteEvent())
         {
-            events.Reset();
-
-            if (pInstance)
-            {
-                pInstance->SetData(EVENT_LOATHEB, NOT_STARTED);
-                if (GameObject* go = me->GetMap()->GetGameObject(pInstance->GetData64(DATA_LOATHEB_GATE)))
-                    go->SetGoState(GO_STATE_ACTIVE);
-            }
-        }
-
-        void JustSummoned(Creature* cr) { cr->SetInCombatWithZone(); }
-
-        void SummonedCreatureDies(Creature* cr, Unit*)
-        {
-            if (pInstance)
-                pInstance->SetData(DATA_SPORE_KILLED, 0);
-        }
-
-        void KilledUnit(Unit* who)
-        {
-            if (who->GetTypeId() == TYPEID_PLAYER && pInstance)
-                pInstance->SetData(DATA_IMMORTAL_FAIL, 0);
-        }
-
-        void EnterCombat(Unit *who)
-        {
-            if (pInstance)
-            {
-                pInstance->SetData(EVENT_LOATHEB, IN_PROGRESS);
-                if (GameObject* go = me->GetMap()->GetGameObject(pInstance->GetData64(DATA_LOATHEB_GATE)))
-                    go->SetGoState(GO_STATE_READY);
-            }
-
-            me->SetInCombatWithZone();
-            events.ScheduleEvent(EVENT_SPELL_NECROTIC_AURA, 0);
-            events.ScheduleEvent(EVENT_SPELL_DEATHBLOOM, 25000);
-            events.ScheduleEvent(EVENT_SPELL_INEVITABLE_DOOM, 120000);
-            events.ScheduleEvent(EVENT_SPELL_BERSERK, 720000);
-        }
-
-        void JustDied(Unit* Killer)
-        {
-            if (pInstance)
-                pInstance->SetData(EVENT_LOATHEB, DONE);
-        }
-
-        void UpdateAI(uint32 diff)
-        {
-            if (!UpdateVictim())
-                return;
-
-            events.Update(diff);
-            if (me->HasUnitState(UNIT_STATE_CASTING))
-                return;
-
-            switch (events.GetEvent())
+            switch (eventId)
             {
                 case EVENT_SPELL_NECROTIC_AURA:
+                    Talk(SAY_NECROTIC_AURA_APPLIED);
+                    if (int32 duration = sSpellMgr->GetSpellInfo(SPELL_NECROTIC_AURA)->GetDuration())
+                    {
+                        events.ScheduleEvent(EVENT_AURA_FADING, duration - 4000);
+                        events.ScheduleEvent(EVENT_AURA_REMOVED, duration);
+                    }
                     me->CastSpell(me, SPELL_NECROTIC_AURA, true);
-                    events.RepeatEvent(20000);
+                    events.Repeat(20000);
                     break;
                 case EVENT_SPELL_DEATHBLOOM:
                     me->CastSpell(me, SPELL_SUMMON_SPORE, true);
                     me->CastSpell(me, RAID_MODE(SPELL_DEATHBLOOM_10, SPELL_DEATHBLOOM_25), false);
-                    events.RepeatEvent(30000);
+                    events.Repeat(30000);
                     break;
                 case EVENT_SPELL_INEVITABLE_DOOM:
                     me->CastSpell(me, RAID_MODE(SPELL_INEVITABLE_DOOM_10, SPELL_INEVITABLE_DOOM_25), false);
-                    events.RepeatEvent(events.GetTimer() < 5 * MINUTE*IN_MILLISECONDS ? 30000 : 15000);
+                    events.Repeat(events.GetTimer() < 5 * MINUTE*IN_MILLISECONDS ? 30000 : 15000);
                     break;
                 case EVENT_SPELL_BERSERK:
                     me->CastSpell(me, SPELL_BERSERK, true);
-                    events.PopEvent();
+                    break;
+                case EVENT_AURA_FADING:
+                    Talk(SAY_NECROTIC_AURA_FADING);
+                    break;
+                case EVENT_AURA_REMOVED:
+                    Talk(SAY_NECROTIC_AURA_REMOVED);
+                    break;
+                default:
                     break;
             }
 
-            DoMeleeAttackIfReady();
+            if (me->HasUnitState(UNIT_STATE_CASTING))
+                return;
         }
-    };
-};
 
-class spell_loatheb_necrotic_aura_warning : public SpellScriptLoader
-{
-    public:
-        spell_loatheb_necrotic_aura_warning() : SpellScriptLoader("spell_loatheb_necrotic_aura_warning") { }
-
-        class spell_loatheb_necrotic_aura_warning_AuraScript : public AuraScript
-        {
-            PrepareAuraScript(spell_loatheb_necrotic_aura_warning_AuraScript);
-
-            void HandleEffectApply(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
-            {
-                Creature* target = GetTarget()->ToCreature();
-                if (target->IsAIEnabled)
-                    target->AI()->Talk(SAY_NECROTIC_AURA_APPLIED);
-            }
-
-            void HandleEffectRemove(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
-            {
-                Creature* target = GetTarget()->ToCreature();
-                if (target->IsAIEnabled)
-                    target->AI()->Talk(SAY_NECROTIC_AURA_REMOVED);
-            }
-
-            void Register()
-            {
-                AfterEffectApply += AuraEffectApplyFn(spell_loatheb_necrotic_aura_warning_AuraScript::HandleEffectApply, EFFECT_0, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
-                AfterEffectRemove += AuraEffectRemoveFn(spell_loatheb_necrotic_aura_warning_AuraScript::HandleEffectRemove, EFFECT_0, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
-            }
-        };
-
-        AuraScript* GetAuraScript() const
-        {
-            return new spell_loatheb_necrotic_aura_warning_AuraScript();
-        }
+        DoMeleeAttackIfReady();
+    }
 };
 
 void AddSC_boss_loatheb()
 {
-    new boss_loatheb();
-    new spell_loatheb_necrotic_aura_warning();
+    new CreatureAILoader<boss_loathebAI>("boss_loatheb");
 }
