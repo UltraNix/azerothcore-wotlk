7308411258f9155738ab24222c342e5c9ca1fa42
peyton <admin@darklab.pl>
2017-06-28 02:31:55 +0200

    New world db release 0.0.5

--- sql/updates/world/old/0.0.4/2017_06_24_smart_scripts.sql
+++ sql/updates/world/old/0.0.4/2017_06_24_smart_scripts.sql
@@ -11,6 +11,6 @@ SET @ENTRY := 9502;
 UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,-1,0,0,0,100,0,11000,13000,9000,11000,11,8732,0,0,0,0,0,2,0,0,0,0,0,0,0,"Phalanx - In Combat - Cast 'Thunderclap'"),
-(@ENTRY,0,-1,0,2,0,100,0,0,51,14000,15000,11,22425,0,0,0,0,0,2,0,0,0,0,0,0,0,"Phalanx - Between 0-51% Health - Cast 'Fireball Volley'"),
-(@ENTRY,0,-1,0,0,0,100,0,14000,16000,9000,11000,11,14099,0,0,0,0,0,2,0,0,0,0,0,0,0,"Phalanx - In Combat - Cast 'Mighty Blow'");
+(@ENTRY,0,0,0,0,0,100,0,11000,13000,9000,11000,11,8732,0,0,0,0,0,2,0,0,0,0,0,0,0,"Phalanx - In Combat - Cast 'Thunderclap'"),
+(@ENTRY,0,1,0,2,0,100,0,0,51,14000,15000,11,22425,0,0,0,0,0,2,0,0,0,0,0,0,0,"Phalanx - Between 0-51% Health - Cast 'Fireball Volley'"),
+(@ENTRY,0,2,0,0,0,100,0,14000,16000,9000,11000,11,14099,0,0,0,0,0,2,0,0,0,0,0,0,0,"Phalanx - In Combat - Cast 'Mighty Blow'");
