f2e75109f9526a67a6e084fc59285efd223a9879
peyton <admin@darklab.pl>
2017-06-29 01:57:12 +0200

    Partial resist temporary fix - build

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -67,6 +67,50 @@
 
 #include <math.h>
 
+std::vector<uint32> partialCorrection =
+{
+    1120,   // Drain Soul r1
+    8288,   // Drain Soul r2
+    8289,   // Drain Soul r3
+    11675,  // Drain Soul r4
+    27217,  // Drain Soul r5
+    47855,  // Drain Soul r6
+    49184,  // Howling Blast r1
+    51409,  // Howling Blast r2
+    51410,  // Howling Blast r3
+    51411,  // Howling Blast r4
+    5570,   // Insect Swarm r1
+    24974,  // Insect Swarm r2
+    24975,  // Insect Swarm r3
+    24976,  // Insect Swarm r4
+    24977,  // Insect Swarm r5
+    27013,  // Insect Swarm r6
+    48468,  // Insect Swarm r7
+    20733,  // Black Arrow r1
+    63668,  // Black Arrow r2
+    63669,  // Black Arrow r3
+    63670,  // Black Arrow r4
+    63671,  // Black Arrow r5
+    63672,  // Black Arrow r6
+    47528,  // Mind Freeze r1
+    57994,  // Wind Shear  r1
+    60103,  // Lava Lash r1
+    8042,   // Earth Shock r1
+    8044,   // Earth Shock r2
+    8045,   // Earth Shock r3
+    8046,   // Earth Shock r4
+    10412,  // Earth Shock r5
+    10413,  // Earth Shock r6
+    10414,  // Earth Shock r7
+    25454,  // Earth Shock r8
+    49230,  // Earth Shock r9
+    49231,  // Earth Shock r10
+    32645,  // Envenom r1
+    32684,  // Envenom r2
+    57992,  // Envenom r3
+    57993,  // Envenom r4
+};
+
 float baseMoveSpeed[MAX_MOVE_TYPE] =
 {
     2.5f,                  // MOVE_WALK
