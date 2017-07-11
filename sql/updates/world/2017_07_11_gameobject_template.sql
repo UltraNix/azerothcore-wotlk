1344626d019caefe05623920c52e09095af0a4b0
crackaw <crackaw@outlook.com>
2017-07-12 00:48:14 +0200

    Huge Seaforium Bombs cooldown

---
+++ sql/updates/world/2017_07_11_gameobject_template.sql
@@ -0,0 +1,12 @@
+-- Huge Seaforium Bombs SAI
+SET @ENTRY := 195332;
+UPDATE `gameobject_template` SET `AIName`="SmartGameObjectAI" , `data3` = 10000, `data5` = 0, `data6` = 10000 WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,1,0,0,64,0,100,0,0,0,0,0,70,10,0,0,0,0,0,1,0,0,0,0,0,0,0,"Huge Seaforium Bombs - On Gossip Hello - Reset Gameobject");
+
+SET @ENTRY := 195333;
+UPDATE `gameobject_template` SET `AIName`="SmartGameObjectAI" , `data3` = 10000, `data5` = 0, `data6` = 10000 WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,1,0,0,64,0,100,0,0,0,0,0,70,10,0,0,0,0,0,1,0,0,0,0,0,0,0,"Huge Seaforium Bombs - On Gossip Hello - Reset Gameobject");
