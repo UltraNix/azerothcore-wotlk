ec33116724393a754615598269795fe83f697c0b
Afgann <Marcin60000@gmail.com>
2017-07-27 16:22:39 +0200

    Ulduar:
    - Added missing fragment in Hodir 25 hm cache.

---
+++ sql/updates/world/2017_07_26_gameobject_loot_template.sql
@@ -0,0 +1,3 @@
+DELETE FROM `gameobject_loot_template` WHERE `entry`=26950 AND `item`=45038;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(26950, 45038, 13, 1, 0, 1, 1);
