ed85260f996bf7d3fe8b7ceb97d2eebb68eb1bc0
Afgann <Marcin60000@gmail.com>
2017-07-30 02:23:32 +0200

    Update boss_argent_challenge.cpp

--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheChampion/boss_argent_challenge.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheChampion/boss_argent_challenge.cpp
@@ -702,7 +702,7 @@ class spell_toc5_light_rain_SpellScript : public SpellScript
     {
         for (std::list<WorldObject*>::iterator itr = targets.begin(); itr != targets.end(); )
         {
-            if ((*itr)->IsCreature())
+            if ((*itr)->GetTypeId() == TYPEID_UNIT)
                 if ((*itr)->ToCreature()->GetEntry() == NPC_FOUNTAIN_OF_LIGHT)
                 {
                     targets.erase(itr);
