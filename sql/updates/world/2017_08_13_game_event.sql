473e41c02d151610fabc68f9d5fc216ad1a8856e
crackaw <crackaw@outlook.com>
2017-08-19 12:29:57 +0200

    Lottery Cave
    -Piotrek ester eggs

---
+++ sql/updates/world/2017_08_13_game_event.sql
@@ -0,0 +1,4 @@
+REPLACE INTO `game_event` (`eventEntry`, `start_time`, `end_time`, `occurence`, `length`, `holiday`, `description`, `world_event`) VALUES 
+(132, '2017-08-13 20:00:00', '2030-12-31 06:00:00', 10080, 1, 0, 'Lottery Gold', 0);
+REPLACE INTO `game_event` (`eventEntry`, `start_time`, `end_time`, `occurence`, `length`, `holiday`, `description`, `world_event`) VALUES 
+(133, '2017-08-13 20:05:00', '2020-12-31 06:00:00', 10080, 1, 0, 'Lottery Sunwell Coins', 0);
