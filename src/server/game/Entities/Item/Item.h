0a73ef36d1c4f4c56a07acece29b8a83f8b14e74
Afgann <Marcin60000@gmail.com>
2017-08-30 03:15:17 +0200

    Block selling items to Ethereal Soul-Trader.
    (cherry picked from commit 109fbdcd92f931355317fe86f099e806f02a81e8)

--- src/server/game/Entities/Item/Item.h
+++ src/server/game/Entities/Item/Item.h
@@ -149,7 +149,8 @@ enum SellResult
     SELL_ERR_CANT_FIND_VENDOR                    = 3,       // merchant doesn't like you
     SELL_ERR_YOU_DONT_OWN_THAT_ITEM              = 4,       // you don't own that item
     SELL_ERR_UNK                                 = 5,       // nothing appears...
-    SELL_ERR_ONLY_EMPTY_BAG                      = 6        // can only do with empty bags
+    SELL_ERR_ONLY_EMPTY_BAG                      = 6,       // can only do with empty bags
+    SELL_ERR_CANT_SELL_TO_THIS_MERCHANT          = 7        // You cannot sell items to this merchant.
 };
 
 // -1 from client enchantment slot number
