88fa79491bb93ff75ad9dd1474cd44bd7ea29d5b
Afgann <Marcin60000@gmail.com>
2017-08-31 02:14:58 +0200

    Wintergrasp Commendation
    -Added to vendor 32296 and 32294
    
    (cherry picked from commit b8edfaf2c0678aabeac894d49c22ad2dad8a509e)

---
+++ sql/updates/world/2017_08_30_npc_vendor_01.sql
@@ -0,0 +1,8 @@
+DELETE FROM `npc_vendor` WHERE `entry`=32296 AND `item`=44115 AND `ExtendedCost`=2546;
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`) VALUES (32296, 0, 44115, 0, 0, 2546);
+DELETE FROM `npc_vendor` WHERE `entry`=32294 AND `item`=44115 AND `ExtendedCost`=2546;
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`) VALUES (32294, 0, 44115, 0, 0, 2546);
+DELETE FROM `npc_vendor` WHERE `entry`=32296 AND `item`=44115 AND `ExtendedCost`=2576;
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`) VALUES (32296, 0, 44115, 0, 0, 2576);
+DELETE FROM `npc_vendor` WHERE `entry`=32294 AND `item`=44115 AND `ExtendedCost`=2576;
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`) VALUES (32294, 0, 44115, 0, 0, 2576);
