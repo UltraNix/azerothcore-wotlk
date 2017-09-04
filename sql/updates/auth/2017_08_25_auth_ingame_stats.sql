6a79402a01a7994d442d0a60127e57bef9b639ec
crackaw <crackaw@outlook.com>
2017-09-04 20:45:55 +0200

    Ingame Stats
    -Typo

--- sql/updates/auth/2017_08_25_auth_ingame_stats.sql
+++ sql/updates/auth/2017_08_25_auth_ingame_stats.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS ingame_stats; 
 CREATE TABLE ingame_stats ( 
-  `playersCount` int(11) unsigned NOT NULL DEFAULT '0', 
   `connectedPlayers` int(11) unsigned NOT NULL DEFAULT '0', 
+  `playersCount` int(11) unsigned NOT NULL DEFAULT '0', 
   `diff` smallint(3) unsigned NOT NULL DEFAULT '0',
   `avgDiff` smallint(3) unsigned NOT NULL DEFAULT '0',
   `currentTime` varchar(30) NOT NULL
