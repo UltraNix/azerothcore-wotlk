b0979e5fe2ce7026cd5a677aef9e520fae533dba
Afgann <Marcin60000@gmail.com>
2017-09-24 15:39:41 +0200

    FleeingMovementGenerator rewrite.

--- src/server/game/Movement/MovementGenerators/FleeingMovementGenerator.h
+++ src/server/game/Movement/MovementGenerators/FleeingMovementGenerator.h
@@ -1,72 +1,45 @@
-/*
- * Copyright (C) 
- * Copyright (C) 
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 #ifndef TRINITY_FLEEINGMOVEMENTGENERATOR_H
 #define TRINITY_FLEEINGMOVEMENTGENERATOR_H
 
 #include "MovementGenerator.h"
+#include "Timer.h"
+#include "PathGenerator.h"
 
 template<class T>
 class FleeingMovementGenerator : public MovementGeneratorMedium< T, FleeingMovementGenerator<T> >
 {
     public:
-        FleeingMovementGenerator(uint64 fright) : i_frightGUID(fright), i_nextCheckTime(0) {}
+        explicit FleeingMovementGenerator(uint64 fleeTargetGUID) : _path(nullptr), _fleeTargetGUID(fleeTargetGUID), _timer(0), _interrupt(false) { }
+        ~FleeingMovementGenerator();
+
+        MovementGeneratorType GetMovementGeneratorType() { return FLEEING_MOTION_TYPE; }
 
         void DoInitialize(T*);
         void DoFinalize(T*);
         void DoReset(T*);
         bool DoUpdate(T*, uint32);
 
-        MovementGeneratorType GetMovementGeneratorType() { return FLEEING_MOTION_TYPE; }
-
     private:
-        void _setTargetLocation(T*);
-        bool _getPoint(T*, float &x, float &y, float &z);
-        bool _setMoveData(T* owner);
-        void _Init(T* );
+        void SetTargetLocation(T*);
+        void GetPoint(T*, Position &position);
 
-        bool is_water_ok   :1;
-        bool is_land_ok    :1;
-        bool i_only_forward:1;
-
-        float i_caster_x;
-        float i_caster_y;
-        float i_caster_z;
-        float i_last_distance_from_caster;
-        float i_to_distance_from_caster;
-        float i_cur_angle;
-        uint64 i_frightGUID;
-        TimeTracker i_nextCheckTime;
+        PathGenerator* _path;
+        uint64 _fleeTargetGUID;
+        TimeTracker _timer;
+        bool _interrupt;
 };
 
 class TimedFleeingMovementGenerator : public FleeingMovementGenerator<Creature>
 {
     public:
-        TimedFleeingMovementGenerator(uint64 fright, uint32 time) :
-            FleeingMovementGenerator<Creature>(fright),
-            i_totalFleeTime(time) {}
+        explicit TimedFleeingMovementGenerator(uint64 fleeTargetGUID, uint32 time) : FleeingMovementGenerator<Creature>(fleeTargetGUID), _totalFleeTime(time) { }
 
         MovementGeneratorType GetMovementGeneratorType() { return TIMED_FLEEING_MOTION_TYPE; }
-        bool Update(Unit*, uint32);
-        void Finalize(Unit*);
+        bool Update(Unit*, uint32) override;
+        void Finalize(Unit*) override;
 
     private:
-        TimeTracker i_totalFleeTime;
+        TimeTracker _totalFleeTime;
 };
 
 #endif
