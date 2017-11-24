2bb9c52d52aa2d81ce5fb6733c6f03a950528120
Riztazz <felianther15@gmail.com>
2017-11-24 14:21:56 +0100

    Change skybox lights during algalon quest event and misc fixes to creature visibility during siad event

---
+++ sql/updates/world/2017_11_24_00_smart_scripts.sql
@@ -0,0 +1,42 @@
+SET @MARCIA:= 28742;
+
+UPDATE `creature_template` SET `ScriptName` = "npc_marcia_dalaran_skybox" WHERE `entry` = @MARCIA;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 1612802;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(1612802, 9, 0 , 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 1, 1, 0, 0, 0, 0, 11, @MARCIA, 100, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 1 , 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 32, 32, 0, 0, 0, 0, 10, 74529, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 2 , 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 32, 32, 0, 0, 0, 0, 10, 74530, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 3 , 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 32, 32, 0, 0, 0, 0, 10, 74531, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 4 , 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 32, 32, 0, 0, 0, 0, 10, 74532, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 5 , 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 32, 32, 0, 0, 0, 0, 10, 74533, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 6 , 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 32, 32, 0, 0, 0, 0, 10, 74534, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 7 , 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 30, 30, 0, 0, 0, 0, 10, 74528, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 8 , 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 30, 30, 0, 0, 0, 0, 10, 74535, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 9 , 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 30, 30, 0, 0, 0, 0, 10, 74536, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 10, 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 30, 30, 0, 0, 0, 0, 10, 74537, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 11, 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 31, 31, 0, 0, 0, 0, 10, 74538, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 12, 0, 0, 0, 100, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Say Line 2'),
+(1612802, 9, 13, 0, 0, 0, 100, 0, 10000, 10000, 0, 0, 1, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Say Line 3'),
+(1612802, 9, 14, 0, 0, 0, 100, 0, 10000, 10000, 0, 0, 1, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Say Line 3'),
+(1612802, 9, 15, 0, 0, 0, 100, 0, 10000, 10000, 0, 0, 1, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Say Line 3'),
+(1612802, 9, 16, 0, 0, 0, 100, 0, 8000, 8000, 0, 0, 1, 5, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Say Line 3'),
+(1612802, 9, 17, 0, 0, 0, 100, 0, 10000, 10000, 0, 0, 1, 6, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Say Line 3'),
+(1612802, 9, 18, 0, 0, 0, 100, 0, 14000, 14000, 0, 0, 1, 7, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Say Line 3'),
+(1612802, 9, 19, 0, 0, 0, 100, 0, 1, 1, 0, 0, 45, 2, 2, 0, 0, 0, 0, 11, @MARCIA, 100, 0, 0, 0, 0, 0, 'Rhonin - on script - set data on marcia'),
+(1612802, 9, 20, 0, 0, 0, 100, 0, 60000, 60000, 0, 0, 45, 29, 29, 0, 0, 0, 0, 10, 74538, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 21, 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 28, 28, 0, 0, 0, 0, 10, 74528, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 22, 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 28, 28, 0, 0, 0, 0, 10, 74529, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 23, 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 28, 28, 0, 0, 0, 0, 10, 74530, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 24, 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 28, 28, 0, 0, 0, 0, 10, 74531, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 25, 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 28, 28, 0, 0, 0, 0, 10, 74532, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 26, 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 28, 28, 0, 0, 0, 0, 10, 74533, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 27, 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 28, 28, 0, 0, 0, 0, 10, 74534, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 28, 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 28, 28, 0, 0, 0, 0, 10, 74535, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 29, 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 28, 28, 0, 0, 0, 0, 10, 74536, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 30, 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 28, 28, 0, 0, 0, 0, 10, 74537, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 31, 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 28, 28, 0, 0, 0, 0, 10, 74538, 28332, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Generic Trigger LAB (Large AOI)'),
+(1612802, 9, 32, 0, 0, 0, 100, 0, 5000, 5000, 0, 0, 45, 1, 1, 0, 0, 0, 0, 19, 34044, 0, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Brann Bronzebeard'),
+(1612802, 9, 33, 0, 0, 0, 100, 0, 1000, 1000, 0, 0, 11, 51347, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Brann Bronzebeard'),
+(1612802, 9, 34, 0, 0, 0, 100, 0, 1000, 1000, 0, 0, 45, 3, 3, 0, 0, 0, 0, 11, @MARCIA, 100, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Set Data on Brann Bronzebeard'),
+(1612802, 9, 35, 0, 0, 0, 100, 0, 1000, 1000, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Rhonin - Script 3 - Despawn');
--- src/server/scripts/Northrend/zone_dalaran.cpp
+++ src/server/scripts/Northrend/zone_dalaran.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 
+ * Copyright (C)
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -90,7 +90,7 @@ enum DisguiseEvent
     ACTION_SHIRTS                = 2,
     ACTION_PANTS                = 3,
     ACTION_UNMENTIONABLES        = 4,
-    
+
     EVENT_INTRO_DH1                = 1,
     EVENT_INTRO_DH2                = 2,
     EVENT_INTRO_DH3                = 3,
@@ -131,8 +131,8 @@ public:
         {
             _events.Reset();
             _aquanosGUID = 0;
-        }  
-        
+        }
+
         void SetData(uint32 type, uint32 data)
         {
             switch(type)
@@ -171,7 +171,7 @@ public:
             Talk(SAY_SHANDY_WATER + _lSource - 1);
             _canWash = true;
         }
-        
+
         void UpdateAI(uint32 diff)
         {
             _events.Update(diff);
@@ -187,7 +187,7 @@ public:
                         _events.ScheduleEvent(EVENT_INTRO_DH3, 6000);
                     else
                         RollTask();
-                    
+
                     _events.PopEvent();
                     break;
                 case EVENT_INTRO_DH3:
@@ -196,7 +196,7 @@ public:
                     _events.PopEvent();
                     break;
                 case EVENT_INTRO_DH4:
-                    Talk(SAY_SHANDY5);             
+                    Talk(SAY_SHANDY5);
                     _events.ScheduleEvent(EVENT_INTRO_DH5, 3000);
                     _events.PopEvent();
                     break;
@@ -225,16 +225,16 @@ public:
             uint8 _lCount;
             uint32 _lSource;
             uint32 _resetTime;
-            
+
             bool _canWash;
     };
-    
+
     bool OnGossipHello(Player* player, Creature* creature)
     {
         if (creature->IsQuestGiver())
             player->PrepareQuestMenu(creature->GetGUID());
 
-        if (player->GetQuestStatus(QUEST_SUITABLE_DISGUISE_A) == QUEST_STATUS_INCOMPLETE || 
+        if (player->GetQuestStatus(QUEST_SUITABLE_DISGUISE_A) == QUEST_STATUS_INCOMPLETE ||
             player->GetQuestStatus(QUEST_SUITABLE_DISGUISE_H) == QUEST_STATUS_INCOMPLETE)
         {
             if(player->GetTeamId() == TEAM_ALLIANCE)
@@ -246,13 +246,13 @@ public:
         player->SEND_GOSSIP_MENU(player->GetGossipTextId(creature), creature->GetGUID());
         return true;
     }
-    
+
     bool OnGossipSelect(Player* player, Creature* creature, uint32 /*sender*/, uint32 action)
     {
         switch (action)
         {
             case GOSSIP_ACTION_INFO_DEF:
-                player->CLOSE_GOSSIP_MENU(); 
+                player->CLOSE_GOSSIP_MENU();
                 creature->SetUInt32Value(UNIT_NPC_FLAGS, 0);
                 creature->AI()->SetData(ACTION_SHANDY_INTRO, 0);
                 break;
@@ -753,6 +753,75 @@ class npc_minigob_manabonk : public CreatureScript
     }
 };
 
+enum marciaEnum
+{
+    NPC_GENERIC_LAB_TRIGGER             = 28332,
+    DALARAN_ZONE                        = 4395,
+    SKYBOX_DEFAULT                      = 1802,
+    SKYBOX_EVENT                        = 1773
+};
+
+class npc_marcia_dalaran_skybox : public CreatureScript
+{
+public:
+    npc_marcia_dalaran_skybox() : CreatureScript("npc_marcia_dalaran_skybox") { }
+
+    struct npc_marcia_dalaran_skybox_AI : public ScriptedAI
+    {
+        npc_marcia_dalaran_skybox_AI(Creature* creature) : ScriptedAI(creature) { }
+
+        void SetData(uint32 type, uint32 value) override
+        {
+            if (type == 1 && value == 1)
+            {
+                std::list<Creature*> tempList;
+                me->GetCreatureListWithEntryInGrid(tempList, NPC_GENERIC_LAB_TRIGGER, 300.0f);
+                for (auto i : tempList)
+                    if (Creature* creature = i->ToCreature())
+                        creature->setActive(true);
+            }
+            else if (type == 2 && value == 2)
+            {
+                WorldPacket data(SMSG_OVERRIDE_LIGHT, 12);
+                data << uint32(SKYBOX_DEFAULT); // Light.dbc entry (map default)
+                data << uint32(SKYBOX_EVENT);     // Light.dbc entry (override)
+                data << uint32(500);
+                SendPacketToPlayers(&data);
+            }
+            else if (type == 3 && value == 3)
+            {
+                std::list<Creature*> tempList;
+                me->GetCreatureListWithEntryInGrid(tempList, NPC_GENERIC_LAB_TRIGGER, 300.0f);
+                for (auto i : tempList)
+                    if (Creature* creature = i->ToCreature())
+                        creature->setActive(false);
+
+                WorldPacket data(SMSG_OVERRIDE_LIGHT, 12);
+                data << uint32(SKYBOX_DEFAULT); // Light.dbc entry (map default)
+                data << uint32(0);     // Light.dbc entry (override)
+                data << uint32(5000);
+                SendPacketToPlayers(&data);
+            }
+        }
+
+        void SendPacketToPlayers(WorldPacket const* data) const
+        {
+            Map::PlayerList const& players = me->GetMap()->GetPlayers();
+            if (!players.isEmpty())
+                for (Map::PlayerList::const_iterator itr = players.begin(); itr != players.end(); ++itr)
+                    if (Player* player = itr->GetSource())
+                        if (player->GetZoneId() == DALARAN_ZONE)
+                            if (player->GetSession())
+                                player->GetSession()->SendPacket(data);
+        }
+    };
+
+    CreatureAI* GetAI(Creature* creature) const override
+    {
+        return new npc_marcia_dalaran_skybox_AI(creature);
+    }
+};
+
 void AddSC_dalaran()
 {
     // our
@@ -761,6 +830,7 @@ void AddSC_dalaran()
     new npc_shandy_dalaran();
     new npc_archmage_landalock();
     new npc_archmage_timear();
+    new npc_marcia_dalaran_skybox();
 
     // theirs
     new npc_mageguard_dalaran();
