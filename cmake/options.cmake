5a106e5fc76b013fbe5e8603ae8ef135c965eaea
Afgann <Marcin60000@gmail.com>
2017-11-28 19:13:45 +0100

    CMake:
    - Use source_groups to represent the source tree.
    
    (cherry picked from commit dfa1056d4403febe666c6bc64d4c541132af7d03)

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -17,3 +17,6 @@ option(WITH_WARNINGS    "Show all warnings during compile"                      
 option(WITH_COREDEBUG   "Include additional debug-code in core"                       0)
 option(WITH_MESHEXTRACTOR "Build meshextractor (alpha)"                               0)
 option(WITHOUT_GIT      "Disable the GIT testing routines"                            0)
+set(WITH_SOURCE_TREE    "hierarchical" CACHE STRING "Build the source tree for IDE's.")
+set_property(CACHE WITH_SOURCE_TREE PROPERTY STRINGS no flat hierarchical hierarchical-folders)
+option(WITH_VS_FOLDERS  "Enables Organizational Folders in Visual Studio"             0) 
