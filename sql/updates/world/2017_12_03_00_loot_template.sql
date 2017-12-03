2718e26be27201729dfc26a15a2cdc4e54d86c47
Riztazz <felianther15@gmail.com>
2017-12-03 08:04:42 +0100

    DB/Creature: Mottled Boar skinning loot template

---
+++ sql/updates/world/2017_12_03_00_loot_template.sql
@@ -0,0 +1,2 @@
+DELETE FROM `skinning_loot_template` WHERE `entry` = 100008 AND `item` = 2319;
+UPDATE `skinning_loot_template` SET `ChanceOrQuestChance` = 48 WHERE `item` = 783 AND `entry` = 100008;
