5204eb54bdb329a475878086951c80547957a9dd
Riztazz <felianther15@gmail.com>
2017-12-10 13:27:45 +0100

    rollback algalon fix for now

--- src/server/scripts/Northrend/Ulduar/Ulduar/boss_algalon_the_observer.cpp
+++ src/server/scripts/Northrend/Ulduar/Ulduar/boss_algalon_the_observer.cpp
@@ -740,7 +740,7 @@ class boss_algalon_the_observer : public CreatureScript
                         // Summon Chest
                         if (GameObject* go = me->SummonGameObject(RAID_MODE(GO_ALGALON_CHEST, GO_ALGALON_CHEST_HERO), 1632.1f, -306.561f, 417.321f, 4.69494f, 0, 0, 0, 1, 0))
                         {
-                            go->RemoveFromOwner();
+                            //go->RemoveFromOwner();
                             go->SetUInt32Value(GAMEOBJECT_FLAGS, 0);
                         }
                         events.PopEvent();
