0d69e2601deddf4016cf11dafdf759c3941cdfcf
sitowsky <sitowsky@posteo.net>
2018-01-08 06:29:24 +0100

    Character restore sql updates typo.

--- sql/updates/world/2018_01_08_00_trinity_string.sql
+++ sql/updates/world/2018_01_08_00_trinity_string.sql
@@ -1,4 +1,1 @@
 DELETE FROM `trinity_string` WHERE `trinity_string`.`entry` = 1025;
-
-DELETE FROM `reserved_name` WHERE `reserved_name`.`name` = 'Default';
-INSERT INTO `reserved_name` (`name`) VALUES ('Default');
