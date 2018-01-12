9a9895a92b206b23cbe650de8a956ef7848edaa9
Riztazz <felianther15@gmail.com>
2018-01-12 08:35:47 +0100

    Db/Quest: what the dragons know text correction

---
+++ sql/updates/world/2018_01_12_00_smart_scripts.sql
@@ -0,0 +1,2 @@
+UPDATE `smart_scripts` SET `action_param1` = 7 WHERE `entryorguid` = 2799000 AND `id` = 17;
+UPDATE `smart_scripts` SET `action_param1` = 6 WHERE `entryorguid` = 2799001 AND `id` = 17;
