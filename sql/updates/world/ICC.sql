af922db879f225415ab8c146396e9f002a9104ea
Afgann <Marcin60000@gmail.com>
2018-01-20 00:41:47 +0100

    forgot about 25 ;x
    (cherry picked from commit 65060c0bedc2a21f009c58aa5743607bed2fcc93)

--- sql/updates/world/ICC.sql
+++ sql/updates/world/ICC.sql
@@ -2,7 +2,7 @@
 DELETE FROM `smart_scripts` WHERE `entryorguid`=37023 AND `source_type`=0;
 UPDATE `creature_template` SET `AIName`="", `ScriptName`="npc_plague_scientist" WHERE `entry`=37023;
 -- Deathspeaker High Priest
-UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1`=50 WHERE `creature_id`=36829;
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1`=50 WHERE `creature_id` IN(36829,38074);
 -- Web Wrap
 DELETE FROM `smart_scripts` WHERE `entryorguid`=38028 AND `source_type`=0;
 UPDATE `creature_template` SET `mechanic_immune_mask`=`mechanic_immune_mask`|32, `AIName`="", `ScriptName`="npc_icc_web_wrap" WHERE `entry`=38028;
