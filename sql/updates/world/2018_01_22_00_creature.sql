47d87b56a1e3d5e8faa71229f1db102bfd93d590
Riztazz <felianther15@gmail.com>
2018-01-22 11:14:15 +0100

    missing part of sql

--- sql/updates/world/2018_01_22_00_creature.sql
+++ sql/updates/world/2018_01_22_00_creature.sql
@@ -102,3 +102,9 @@ INSERT INTO `creature_addon` (`guid`, `path_id`) VALUES
 
 UPDATE `creature` SET `MovementType` = 2 WHERE `guid` = @CGUID + 0;
 UPDATE `creature` SET `MovementType` = 2 WHERE `guid` = @CGUID + 9;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @CENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(30083, 0, 0, 0, 4, 0, 100, 7, 0, 0, 0, 0, 11, 56729, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Marauding Geist - On Aggro - Cast \'Frenzied Leap\''),
+(30083, 0, 1, 0, 25, 0, 100, 0, 0, 0, 0, 0, 48, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Marauding Geist - On Reset - Set Active");
+
