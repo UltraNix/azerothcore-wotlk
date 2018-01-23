b58468dc8c61ab94db8fa0159d835be1a1e372a7
Piootrek <piotrek9_11@o2.pl>
2018-01-23 14:08:03 +0100

    Changed Valanyr fragments drop rate

---
+++ sql/updates/world/2018_01_23_01_valanyr_fragments.sql
@@ -0,0 +1,19 @@
+UPDATE creature_loot_template SET ChanceOrQuestChance = 8.5 WHERE entry = 34175;
+UPDATE creature_loot_template SET ChanceOrQuestChance = 8.5  WHERE entry = 34003;
+UPDATE creature_loot_template SET ChanceOrQuestChance = 8.5 WHERE entry = 33885;
+UPDATE creature_loot_template SET ChanceOrQuestChance = 8.5 WHERE entry = 33724;
+UPDATE creature_loot_template SET ChanceOrQuestChance = 8.5 WHERE entry = 33694;
+UPDATE creature_loot_template SET ChanceOrQuestChance = 19.5 WHERE entry = 33693;
+UPDATE creature_loot_template SET ChanceOrQuestChance = 8.5 WHERE entry = 33692;
+UPDATE creature_loot_template SET ChanceOrQuestChance = 8.5 WHERE entry = 33449;
+UPDATE creature_loot_template SET ChanceOrQuestChance = 8.5 WHERE entry = 33190;
+
+UPDATE gameobject_loot_template SET ChanceOrQuestChance = 19.5 WHERE entry = 26950;
+UPDATE gameobject_loot_template SET ChanceOrQuestChance = 19.5 WHERE entry = 26974;
+UPDATE gameobject_loot_template SET ChanceOrQuestChance = 8.5 WHERE entry = 26946;
+UPDATE gameobject_loot_template SET ChanceOrQuestChance = 8.5 WHERE entry = 26963;
+UPDATE gameobject_loot_template SET ChanceOrQuestChance = 19.5 WHERE entry = 26967;
+UPDATE gameobject_loot_template SET ChanceOrQuestChance = 8.5 WHERE entry = 26929;
+UPDATE gameobject_loot_template SET ChanceOrQuestChance = 8.5 WHERE entry = 26955;
+UPDATE gameobject_loot_template SET ChanceOrQuestChance = 19.5 WHERE entry = 26956;
+UPDATE gameobject_loot_template SET ChanceOrQuestChance = 19.5 WHERE entry IN(27081,27079,26962,26960);
