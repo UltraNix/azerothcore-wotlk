0afc9f3a424f055cca86d96665e88fb13402f184
sitowsky <sitowsky@posteo.net>
2018-01-24 14:12:08 +0100

    StrictPlayerNames on Feronis, Angrathar.

--- sql/updates/world_progressive/patch_322.sql
+++ sql/updates/world_progressive/patch_322.sql
@@ -1,8 +1,8 @@
-#
-# === Content 3.2.2 (Argent Tournament) ===
-#
+--
+-- === Content 3.2.2 (Argent Tournament) ===
+--#
 
-# Remove disables for raids and instances for current content
+-- Remove disables for raids and instances for current content
 DELETE FROM `disables` WHERE `sourceType` = 2 AND `entry` IN (249);
 
-# =============================================================================================
+-- =============================================================================================
