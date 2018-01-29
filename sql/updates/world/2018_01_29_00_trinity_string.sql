a405402827795a38cbdc4f136f555310f6dde2aa
Piootrek <piotrek9_11@o2.pl>
2018-01-29 14:49:21 +0100

    Changed BG announcements colors

---
+++ sql/updates/world/2018_01_29_00_trinity_string.sql
@@ -0,0 +1,6 @@
+UPDATE trinity_string SET content_default = '|cffffcc00The Battle for|r |cffff3300%s (%u-%u)|r |cffffcc00has begun!|r' WHERE entry = 717; 
+UPDATE trinity_string SET content_default = '|cffffcc00The Battle for|r |cffff3300Wintergrasp|r |cffffcc00has begun!|r' WHERE entry = 20078;
+UPDATE trinity_string SET content_default = '|cffffcc00Queue status for %s (level %u to %u):|r\n|cffffcc00Alliance in queue: %u (need at least %u).|r\n|cffffcc00Horde in queue: %u (need at least %u).|r' WHERE entry = 711;
+UPDATE trinity_string SET content_default = '|cffffcc00Queue status for %s (level %u to %u):|r\n|cffffcc00Alliance in queue: %u + random queue (need at least %u).|r\n|cffffcc00Horde in queue: %u + random queue (need at least %u).|r' WHERE entry = 712; 
+UPDATE trinity_string SET content_default = '|cffffcc00Queue status for %s (level %u to %u):|r\n|cffffcc00Alliance in queue: %u + specific queue.|r\n|cffffcc00Horde in queue: %u + specific queue.|r' WHERE entry = 713;
+UPDATE trinity_string SET content_default = '|cffffcc00The Battle for|r |cffff3300%s (80)|r |cffffcc00has begun!|r"' WHERE entry = 788;
