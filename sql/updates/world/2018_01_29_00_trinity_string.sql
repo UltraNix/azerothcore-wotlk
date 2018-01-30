540ee6866aac874792a4379e9b883b155bd7df82
sitowsky <sitowsky@posteo.net>
2018-01-30 04:58:27 +0100

    typo

--- sql/updates/world/2018_01_29_00_trinity_string.sql
+++ sql/updates/world/2018_01_29_00_trinity_string.sql
@@ -4,4 +4,4 @@ UPDATE trinity_string SET content_default = '|cffffcc00Queue status for %s (leve
 UPDATE trinity_string SET content_default = '|cffffcc00Queue status for %s (level %u to %u):\n|cffffcc00Alliance in queue: %u + random queue (need at least %u).\n|cffffcc00Horde in queue: %u + random queue (need at least %u).|r' WHERE entry = 712; 
 UPDATE trinity_string SET content_default = '|cffffcc00Queue status for %s (level %u to %u):\n|cffffcc00Alliance in queue: %u + specific queue.\n|cffffcc00Horde in queue: %u + specific queue.|r' WHERE entry = 713;
 UPDATE trinity_string SET content_default = '|cffffcc00The Battle for |cffff3300%s (80) |cffffcc00has begun!|r' WHERE entry = 788;
-UPDATE trinity_string SET content_default = '|cffff9933%s' WHERE entry = 11000;
+UPDATE trinity_string SET content_default = '|cffff9933%s|r' WHERE entry = 11000;
