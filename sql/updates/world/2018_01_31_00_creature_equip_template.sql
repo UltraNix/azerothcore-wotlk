5a9b8ec5374c2b08673a65fc2efaf83984268e8e
Riztazz <felianther15@gmail.com>
2018-01-31 13:56:24 +0100

    DB/Creature: fix some cubic spells

---
+++ sql/updates/world/2018_01_31_00_creature_equip_template.sql
@@ -0,0 +1,6 @@
+UPDATE `creature_equip_template` SET `itemEntry3` = 28914 WHERE `entry` = 27560;
+UPDATE `creature_equip_template` SET `itemEntry3` = 1906 WHERE `entry` = 1162;
+UPDATE `creature_equip_template` SET `itemEntry3` = 2504 WHERE `entry` = 17664;
+UPDATE `creature_equip_template` SET `itemEntry3` = 29010 WHERE `entry` = 701;
+UPDATE `creature_equip_template` SET `itemEntry3` = 2504 WHERE `entry` = 27203;
+UPDATE `creature_equip_template` SET `itemEntry3` = 2504 WHERE `entry` = 27560;
