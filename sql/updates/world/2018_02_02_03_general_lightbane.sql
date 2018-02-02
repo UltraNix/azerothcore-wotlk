8cbf1236983d2c21f1f6ae94493426268133d31b
Riztazz <felianther15@gmail.com>
2018-02-02 20:14:31 +0100

    DB/Creature: General Lightbane

---
+++ sql/updates/world/2018_02_02_03_general_lightbane.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `minDmg` = 100, `maxDmg` = 200, `dmg_multiplier` = 4 WHERE `entry` = 29851;
+UPDATE `event_scripts` SET `x` = 8590.268, `y` = 2638.312, `z` = 652.353 WHERE `id` = 19423;
