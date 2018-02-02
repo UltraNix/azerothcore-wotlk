6293c2fe0e1e7d9aefff41918aef50a885dab251
Riztazz <felianther15@gmail.com>
2018-02-02 10:46:35 +0100

    Fix loot in low level zones

---
+++ sql/updates/world/2018_02_02_01_creature_loot_template.sql
@@ -0,0 +1,2 @@
+UPDATE creature_loot_template SET mincountOrRef = -24073 WHERE item = 24073 AND mincountOrRef = 1;
+UPDATE creature_loot_template SET mincountOrRef = -11111 WHERE item = 11111 AND mincountOrRef = 1;
