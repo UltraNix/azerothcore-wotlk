c523d14a942e8cdc95d33ed25406205d347287c5
sitowsky <sitowsky@posteo.net>
2018-02-02 05:04:10 +0100

    issue #70 and wolfs missing git add typo

--- sql/updates/world/2018_02_00_issue_tracker.sql
+++ sql/updates/world/2018_02_00_issue_tracker.sql
@@ -6,3 +6,5 @@ UPDATE `creature_template` SET `ScriptName` = 'npc_goldir' WHERE `creature_templ
 UPDATE `spell_proc_event` SET `procFlags` = '40' WHERE `spell_proc_event`.`entry` = 67653;
 -- #98
 UPDATE creature_template SET scriptname = "npc_elwynn_wolf" WHERE entry IN (69,299);
+-- #70
+UPDATE `smart_scripts` SET `event_phase_mask` = '0' WHERE `smart_scripts`.`entryorguid` =25216 AND `smart_scripts`.`id` =1
