377f8697095cf37a9f58141a2bd4b7c01cd8b1c9
sitowsky <sitowsky@posteo.net>
2018-02-04 16:40:19 +0100

    SQL typo.

--- sql/updates/world/2018_02_04_02_quest_it_goes_to_11.sql
+++ sql/updates/world/2018_02_04_02_quest_it_goes_to_11.sql
@@ -1,6 +1,6 @@
 DELETE FROM `conditions` WHERE `conditions`.`SourceEntry` = 44213;
 UPDATE `conditions` SET `SourceGroup` = '3' WHERE `conditions`.`SourceEntry` = 44214;
 
-DELETE FROM `world`.`creature` WHERE `creature`.`id` = 24648;
-INSERT INTO `world`.`creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES 
+DELETE FROM `creature` WHERE `creature`.`id` = 24648;
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES 
 (NULL, '24648', '571', '1', '1', '17188', '0', '1074.34', '-5043.678', '53.421', '5.007', '300', '5', '0', '92910', '0', '1', '0', '0', '0');
