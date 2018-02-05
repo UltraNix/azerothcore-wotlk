fb124d88faa13568b6dab18d51da5883f90cabcd
Riztazz <felianther15@gmail.com>
2018-02-05 01:21:44 +0100

    Update 2018_02_02_04_highland_mustang.sql

--- sql/updates/world/2018_02_02_04_highland_mustang.sql
+++ sql/updates/world/2018_02_02_04_highland_mustang.sql
@@ -3,7 +3,6 @@ DELETE FROM `smart_scripts` WHERE `entryorguid` = 26472;
 
 DELETE FROM `spell_script_names` WHERE `spell_id` IN (49282, 49285);
 INSERT INTO `spell_script_names` VALUES
-(49282, "spell_ride_highland_mustang"),
 (49285, "spell_hand_over_reins_49285");
 
 UPDATE `creature_template` SET `spell2` = 29577 WHERE `entry` = 26472;
