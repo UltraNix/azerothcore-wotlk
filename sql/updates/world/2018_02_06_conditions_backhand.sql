a31b6deb5cef3110e41cb22e7b8cfd9d0e6c3656
Dootz <k.kozubek@wp.pl>
2018-02-07 14:11:30 +0100

    Spell/Condition: Set target for Venture Co Ruffian's Backhand spell

---
+++ sql/updates/world/2018_02_06_conditions_backhand.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceEntry` = 52908;
+INSERT INTO `conditions` VALUES
+(13, 1, 52908, 0, 0, 34, 0, 1, 15, 0, 0, 0, 0, '', 'Backhand should target only hostile targets');
