07d7b6d12945b499ec3ff7d413f786db870acfe9
Dootz <k.kozubek@wp.pl>
2018-02-07 22:57:15 +0100

    DB/Conditions: Prevent spawn of multiple Death Knight Adepts

---
+++ sql/updates/world/2018_02_07_conditions_death_knight_adept.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceEntry` = 31316 AND `SourceTypeOrReferenceId` = 22;
+INSERT INTO `conditions` VALUES 
+(22, 1, 31316, 0, 0, 29, 1, 31318, 10, 0, 1, 0, 0, '', 'Ebon Blade Reaper - summon Death Knight Adept  - if he/`s not nearby');
