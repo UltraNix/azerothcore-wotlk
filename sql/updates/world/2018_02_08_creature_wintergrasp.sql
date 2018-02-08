4ef17525a2d8847b96a756edf1412553c371fef6
Riztazz <felianther15@gmail.com>
2018-02-08 16:44:32 +0100

    DB/Spells: resolve issue with traitorous teleport on wintergrasp

---
+++ sql/updates/world/2018_02_08_creature_wintergrasp.sql
@@ -0,0 +1,7 @@
+SET @CGUID:= 131721;
+
+UPDATE `creature` SET `position_x` = 5222.643, `position_y` = 2996.543, `position_z` = 406.966 WHERE `guid` = @CGUID;
+
+SET @CGUID1:= 131722;
+
+UPDATE `creature` SET `position_x` = 5303.305, `position_y` = 3013.127, `position_z` = 409.010 WHERE `guid` = @CGUID1;
