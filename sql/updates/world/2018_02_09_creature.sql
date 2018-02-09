3bef57581847c7aaef840376481401c34bf23d62
Name <wojtyas@gmail.com>
2018-02-09 11:53:12 +0100

    Reposition archers in DK starting zone and misc fixes to outland stair intro

---
+++ sql/updates/world/2018_02_09_creature.sql
@@ -0,0 +1,19 @@
+DELETE FROM `creature` WHERE `guid` IN (68311, 68313);
+INSERT INTO `creature` VALUES 
+(68311, 19005, 530, 1, 1, 0, 1, -270.735, 1527.63, 31.1836, 4.70438, 35, 0, 0, 143620, 0, 0, 0, 0, 0),
+(68313, 19005, 530, 1, 1, 0, 1, -112.343, 1891.33, 80.0147, 4.70438, 25, 0, 0, 143620, 0, 0, 0, 0, 0);
+-- =====================================================================================================
+UPDATE creature SET `position_x` = -268.245270, `position_y` = 1125.750000, `position_z` = 42.495678, `orientation` = 4.704379, `spawndist` = 0, `movementtype` = 0  WHERE `guid` IN(68313);
+UPDATE creature SET `position_x` = -237.204117, `position_y` = 1127.095947, `position_z` = 42.064510, `orientation` = 4.704379, `spawndist` = 0, `movementtype` = 0  WHERE `guid` IN(68311);
+UPDATE creature SET `position_x` = 2406.991943, `position_y` = -5802.561035, `position_z` = 153.689575, `orientation` = 1.438348, `spawndist` = 3, `movementtype` = 1  WHERE `guid` IN(130567);
+UPDATE creature SET `position_x` = 2428.502441, `position_y` = -5792.695312, `position_z` = 137.371902, `orientation` = 1.811412, `spawndist` = 3, `movementtype` = 1  WHERE `guid` IN(130721);
+UPDATE creature SET `position_x` = 2420.371826, `position_y` = -5791.771973, `position_z` = 141.504669, `orientation` = 1.634705, `spawndist` = 3, `movementtype` = 1  WHERE `guid` IN(130658);
+UPDATE creature SET `position_x` = 2413.393799, `position_y` = -5778.081055, `position_z` = 149.191925, `orientation` = 2.180557, `spawndist` = 3, `movementtype` = 1  WHERE `guid` IN(130815);
+UPDATE creature SET `position_x` = 2428.674805, `position_y` = -5767.937012, `position_z` = 149.001984, `orientation` = 2.468013, `spawndist` = 3, `movementtype` = 1  WHERE `guid` IN(130722);
+UPDATE creature SET `position_x` = 2418.812744, `position_y` = -5784.324219, `position_z` = 145.362030, `orientation` = 2.047825, `spawndist` = 0, `movementtype` = 0  WHERE `guid` IN(130602);
+UPDATE creature SET `position_x` = 2430.367676, `position_y` = -5783.949707, `position_z` = 142.435104, `orientation` = 1.961432, `spawndist` = 0, `movementtype` = 0  WHERE `guid` IN(130574);
+UPDATE creature SET `position_x` = 2421.098633, `position_y` = -5784.225098, `position_z` = 144.372711, `orientation` = 1.914308, `spawndist` = 0, `movementtype` = 0  WHERE `guid` IN(130608);
+UPDATE creature SET `position_x` = 1575.188843, `position_y` = -5790.408691, `position_z` = 119.468399, `orientation` = 0.058432, `spawndist` = 3, `movementtype` = 1  WHERE `guid` IN(128710);
+UPDATE creature SET `position_x` = 1572.747314, `position_y` = -5784.608887, `position_z` = 119.019020, `orientation` = 0.113410, `spawndist` = 3, `movementtype` = 1  WHERE `guid` IN(128707);
+UPDATE creature SET `position_x` = 1573.784424, `position_y` = -5774.657715, `position_z` = 117.319031, `orientation` = 0.097702, `spawndist` = 3, `movementtype` = 1  WHERE `guid` IN(128711);
+UPDATE creature SET `position_x` = 1592.520996, `position_y` = -5788.234863, `position_z` = 116.954720, `orientation` = 5.740793, `spawndist` = 3, `movementtype` = 1  WHERE `guid` IN(128713);
