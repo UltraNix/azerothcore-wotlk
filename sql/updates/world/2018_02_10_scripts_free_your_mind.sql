85cac8d10810c0a2cb5c0282203ed58eb3bea62a
Dootz <k.kozubek@wp.pl>
2018-02-10 00:59:28 +0100

    Quest:Free your mind - prevent creature despawn after respawn

---
+++ sql/updates/world/2018_02_10_scripts_free_your_mind.sql
@@ -0,0 +1,8 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 29770 AND `id` = 24;
+UPDATE `smart_scripts` SET `action_param1` = 30000, `comment` = "Lady Nightswood - On 1% HP - Despawn After 30 Seconds" WHERE `entryorguid` = 29770 AND `id` = 6;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 29769 AND `id` = 24;
+UPDATE `smart_scripts` SET `action_param1` = 30000, `comment` = "Vile - On 1% HP - Despawn After 30 seconds" WHERE `entryorguid` = 29769 AND `id` = 6;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 29840 AND `id` = 24;
+UPDATE `smart_scripts` SET `action_param1` = 30000, `comment` = "The Leaper - On 1% HP - Despawn After 30 seconds" WHERE `entryorguid` = 29840 AND `id` = 6;
