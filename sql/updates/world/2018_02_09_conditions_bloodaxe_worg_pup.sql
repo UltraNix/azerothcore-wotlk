f4f192c4fd9b05cd53cfff9d07bf70e2bcee19b8
Dootz <k.kozubek@wp.pl>
2018-02-09 18:52:29 +0100

    DB/Conditions - Quest Kibler's Exotic Pets - allow use of quest item only near Worg Pup

---
+++ sql/updates/world/2018_02_09_conditions_bloodaxe_worg_pup.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceEntry` = 15998;
+INSERT INTO `conditions` VALUES
+(17, 0, 15998, 0, 0, 29, 0, 10221, 10, 0, 0, 97, 0, '', 'Capture Worg Pup can be used near Bloodaxe Worg Pup');
