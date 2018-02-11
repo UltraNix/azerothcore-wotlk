e7ea357d40d228df8a04d5769dc0f397d02f219f
Dootz <k.kozubek@wp.pl>
2018-02-11 16:36:15 +0100

    DB/Conditions: Quest Judgment Day Comes

---
+++ sql/updates/world/2018_02_11_conditions_judgment_day_comes.sql
@@ -0,0 +1,6 @@
+DELETE FROM `conditions` WHERE `SourceEntry` IN (13227, 13226);
+INSERT INTO `conditions` VALUES
+(19, 0, 13227, 0, 0, 14, 0, 13036, 0, 0, 0, 0, 0, '', 'Offer Judgment Day Comes - If Quest:Honor Above All Else none'),
+(20, 0, 13227, 0, 0, 14, 0, 13036, 0, 0, 0, 0, 0, '', 'Offer Judgment Day Comes - If Quest:Honor Above All Else none'),
+(19, 0, 13226, 0, 0, 14, 0, 13036, 0, 0, 0, 0, 0, '', 'Offer Judgment Day Comes - If Quest:Honor Above All Else none'),
+(20, 0, 13226, 0, 0, 14, 0, 13036, 0, 0, 0, 0, 0, '', 'Offer Judgment Day Comes - If Quest:Honor Above All Else none');
