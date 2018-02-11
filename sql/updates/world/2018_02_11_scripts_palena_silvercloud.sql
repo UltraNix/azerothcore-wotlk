a495231f3497eefca0d5758056009a09da2bb6df
Dootz <k.kozubek@wp.pl>
2018-02-11 23:45:28 +0100

    DB/Scripts: On Quest:The High Commander Halford Wyrmbane Accepted start wintergarde Taxi

---
+++ sql/updates/world/2018_02_11_scripts_palena_silvercloud.sql
@@ -0,0 +1,3 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 26881 AND `id` = 4;
+INSERT INTO `smart_scripts` VALUES
+(26881, 0, 4, 0, 19, 0, 100, 0, 12174, 0, 0, 0, 85, 48013, 2, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Palena Silvercloud - On Quest High Commander Halford Wyrmbane Accepted - Invoker Cast Hippogryph Taxi to Wintergarde');
