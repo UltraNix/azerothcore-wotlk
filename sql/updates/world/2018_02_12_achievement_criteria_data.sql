ffb76bff65e7cac0f2812ceba0a7f1fcbaa59058
Afgann <Marcin60000@gmail.com>
2018-02-12 00:52:08 +0100

    Defenseless missing entry.

---
+++ sql/updates/world/2018_02_12_achievement_criteria_data.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id`=6803;
+INSERT INTO `achievement_criteria_data` (`criteria_id`, `type`, `ScriptName`) VALUES
+(6803, 11, "achievement_defenseless");
