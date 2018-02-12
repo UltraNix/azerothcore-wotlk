090ce9918df850f8291fa33b5cccc55adb39aacd
Dootz <k.kozubek@wp.pl>
2018-02-12 20:34:50 +0100

    DB/Scripts: Quest: To Stars Rest - add gryphon taxi

---
+++ sql/updates/world/2018_02_12_scripts_rodney_wells.sql
@@ -0,0 +1,3 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 26878 AND `id` = 3;
+INSERT INTO `smart_scripts` VALUES
+(26878, 0, 3, 0, 19, 0, 100, 0, 12440, 0, 0, 0, 85, 49511, 2, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Rodney Wells - On Quest To Stars`Rest! Accepted - Invoker Cast Gryphon Taxi to Stars Rest');
