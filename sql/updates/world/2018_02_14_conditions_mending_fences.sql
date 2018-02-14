912d31606a3d8c98991f0f2d20673634da4b4efb
Dootz <k.kozubek@wp.pl>
2018-02-14 22:15:12 +0100

    DB/Quest: Show Spark of Hope only if Mending Fences is completed

---
+++ sql/updates/world/2018_02_14_conditions_mending_fences.sql
@@ -0,0 +1,4 @@
+DELETE FROM `conditions` WHERE `SourceEntry` = 12956 AND `ConditionValue1` = 12915;
+INSERT INTO `conditions` VALUES
+(19, 0, 12956, 0, 0, 28, 0, 12915, 0, 0, 0, 0, 0, '', 'Requires quest rewarded'),
+(20, 0, 12956, 0, 0, 28, 0, 12915, 0, 0, 0, 0, 0, '', 'Requires quest rewarded');
