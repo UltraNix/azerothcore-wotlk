d71bfb9dc55d27bbc8472665eea253169cef0651
Dootz <k.kozubek@wp.pl>
2018-02-14 22:15:36 +0100

    DB/NPC: Fordragon Battle Steed

---
+++ sql/updates/world/2018_02_14_vehicle_fordragon_steed.sql
@@ -0,0 +1,5 @@
+DELETE FROM `vehicle_template_accessory` WHERE `entry` = 27761;
+INSERT INTO `vehicle_template_accessory` VALUES
+(27761, 27535, 0, 0, 'Fordragon Battle Steed', 6, 30000);
+
+DELETE FROM `creature` WHERE `id` = 27535;
