94ac2477333d64277085134c48485a0f0d551209
Dootz <k.kozubek@wp.pl>
2018-02-14 02:42:52 +0100

    DB/Scripts: Echo of ymiron - lich king event fix

---
+++ sql/updates/world/2018_02_14_scripts_echo_of_ymiron.sql
@@ -0,0 +1,5 @@
+UPDATE `creature_template` SET `flags_extra` = 2 WHERE `entry` = 24248; 
+UPDATE `smart_scripts` SET `action_param2` = 70, `target_type` = 21, `target_param1` = 5 WHERE `entryorguid` = 2424801 AND `id` = 3;
+UPDATE `smart_scripts` SET `target_type` = 21, `target_param1` = 5 WHERE `entryorguid` = 2424801 AND `id` = 4;
+UPDATE `smart_scripts` SET `action_param2` = 70, `target_type` = 21, `target_param1` = 5 WHERE `entryorguid` = 2424800 AND `id` = 10;
+UPDATE `smart_scripts` SET `target_type` = 21, `target_param1` = 5 WHERE `entryorguid` = 2424800 AND `id` = 11;
