a9ff6da7720011a8ff6fd9c2b5a478ac5b4e24ff
Afgann <Marcin60000@gmail.com>
2018-02-15 22:42:11 +0100

    Gundrak Raptor skinning.
    (cherry picked from commit 4f3756eec65f35a0e5254f458bfd6aacdc6736c1)

--- sql/updates/world/2018_02_13_afgann_fixes.sql
+++ sql/updates/world/2018_02_13_afgann_fixes.sql
@@ -1,2 +1,9 @@
 -- Wooly Mammoth Bull
 UPDATE `creature_template` SET `RegenHealth`=1 WHERE `entry`=25743;
+
+-- Gundrak Raptor skinning
+DELETE FROM `skinning_loot_template` WHERE `entry`=70216;
+INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(70216, 33568, 0, 1, 1, 1, 8),
+(70216, 44128, 1, 1, 1, 1, 2);
+UPDATE `creature_template` SET `skinloot`=70216 WHERE `entry`=29334;
