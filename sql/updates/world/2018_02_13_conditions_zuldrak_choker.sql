46c911174a27b72df35aedfc84a4a21e3e0f8a36
Dootz <k.kozubek@wp.pl>
2018-02-15 13:56:13 +0100

    Choker followup

--- sql/updates/world/2018_02_13_conditions_zuldrak_choker.sql
+++ sql/updates/world/2018_02_13_conditions_zuldrak_choker.sql
@@ -1,1 +1,4 @@
-UPDATE `conditions` SET `ConditionTypeOrReference` = 9 WHERE `SourceGroup` = 28519 AND `SourceEntry` = 38673; 
+UPDATE `conditions` SET `ConditionTypeOrReference` = 28 WHERE `SourceGroup` = 28519 AND `SourceEntry` = 38673 AND `ElseGroup` = 0; 
+DELETE FROM `conditions` WHERE `SourceGroup` = 28519 AND `SourceEntry` = 38673 AND `ElseGroup` = 1;
+INSERT INTO `conditions` VALUES
+(1, 28519, 38673, 0, 1, 8, 0, 12238, 0, 0, 0, 0, 0, '','');
