ec82d818d200772548e08c5eca6789c890cc552a
Afgann <Marcin60000@gmail.com>
2018-02-15 22:43:22 +0100

    Disable pathfinding for some creatures.
    (cherry picked from commit b2779855bcd0f02c701576431617e10c7fecb1cb)

--- sql/updates/world/2018_02_13_afgann_fixes.sql
+++ sql/updates/world/2018_02_13_afgann_fixes.sql
@@ -7,3 +7,12 @@ INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `l
 (70216, 33568, 0, 1, 1, 1, 8),
 (70216, 44128, 1, 1, 1, 1, 2);
 UPDATE `creature_template` SET `skinloot`=70216 WHERE `entry`=29334;
+
+-- Disable pathfinding for some creatures
+UPDATE `creature_template` SET `flags_extra`=`flags_extra`|0x20000000 WHERE `entry` IN 
+(
+	26536, -- Mindless Servant
+	30791, -- Mindless Servant (Heroic)
+	26674, -- Darkweb Hatchling
+	31335  -- Darkweb Hatchling (Heroic)
+);
