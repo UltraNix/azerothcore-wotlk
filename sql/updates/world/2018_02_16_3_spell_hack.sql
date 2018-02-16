c4cb3a6f5cc6a36edf89aaa93b17b6fcb4af4e79
Riztazz <felianther15@gmail.com>
2018-02-16 13:18:48 +0100

    missing sql

---
+++ sql/updates/world/2018_02_16_3_spell_hack.sql
@@ -0,0 +1,5 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (51963, 59172, 48577);
+INSERT INTO `spell_script_names` VALUES
+(51963, "spell_ebon_gargoyle_strike_hack_requested"),
+(59172, "spell_chaos_bolt_damage_hack"),
+(48577, "(spell_ferocious_bite_damage_hack");
