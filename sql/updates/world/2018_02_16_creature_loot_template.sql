ff19590150a6d9792c5a184eb4ce148920547f2a
Dootz <k.kozubek@wp.pl>
2018-02-16 23:04:10 +0100

    DB/Item: some quest items to drop only if on quest

---
+++ sql/updates/world/2018_02_16_creature_loot_template.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -100 WHERE `item` = 3265;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -100 WHERE `item` = 3264;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -100 WHERE `entry` = 1890 AND `item` = 6281;
