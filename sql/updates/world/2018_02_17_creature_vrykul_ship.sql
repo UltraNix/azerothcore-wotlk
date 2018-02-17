511b69abe8560128854b143731f99a9f7a0845e2
Dootz <k.kozubek@wp.pl>
2018-02-17 22:33:23 +0100

    DB/NPC: Vrykul ship- position correction

---
+++ sql/updates/world/2018_02_17_creature_vrykul_ship.sql
@@ -0,0 +1,3 @@
+UPDATE `creature` SET `position_x` = 2881.01, `position_y` = 4834.08, `position_z` = -0.59, `orientation` = 0.93 WHERE `guid` = 111273;
+UPDATE `creature` SET `position_x` = 2658.73, `position_y` = 4752.06, `position_z` = -0.59 WHERE `guid` = 111274; 
+UPDATE `creature` SET `position_x` = 2991.84, `position_y` = 4701.04, `position_z` = -1.58, `orientation` = 6.1 WHERE `guid` = 111270;
