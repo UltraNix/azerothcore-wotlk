dc7ad53dc91ec84d9438914a1fd5afeb8b6fb41f
Dootz <k.kozubek@wp.pl>
2018-02-17 13:21:03 +0100

    DB/NPC: Mindless servant - reduce chance for rare loot

---
+++ sql/updates/world/2018_02_17_creature_loot_template_mindless_servant.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 0.2 WHERE `entry` = 26536 AND `item` = 26002;
+UPDATE `creature_loot_template` SET  `ChanceOrQuestChance` = 0.2 WHERE `entry` = 26536 AND `item` = 26011;
+UPDATE `creature_loot_template` SET  `ChanceOrQuestChance` = 0.2 WHERE `entry` = 26536 AND `item` = 26012;
