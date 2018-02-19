f8f9418a37d9ebd71cac335ad49a16f149cb51dc
sitowsky <sitowsky@posteo.net>
2018-02-19 21:18:38 +0100

    WG > 3500 session fix.

--- src/server/game/Battlefield/Battlefield.cpp
+++ src/server/game/Battlefield/Battlefield.cpp
@@ -154,12 +154,6 @@ bool Battlefield::Update(uint32 diff)
 {
     if (m_Timer <= diff)
     {
-        if (!IsEnabled() || (!IsWarTime() && sWorld->GetActiveSessionCount() > 3500)) // if WG is disabled or there is more than 3500 connections, switch automaticly
-        {
-            m_isActive = true;
-            EndBattle(false);
-            return false;
-        }
         // Battlefield ends on time
         if (IsWarTime())
             EndBattle(true);
