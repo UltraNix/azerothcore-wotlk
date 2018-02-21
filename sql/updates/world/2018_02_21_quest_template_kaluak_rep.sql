2539227ba6dff54085d8ceb4c6b5020f5f6b709f
Dootz <k.kozubek@wp.pl>
2018-02-21 21:59:26 +0100

    DB/Quest: Kalu'ak  chain reputation

---
+++ sql/updates/world/2018_02_21_quest_template_kaluak_rep.sql
@@ -0,0 +1,5 @@
+UPDATE `quest_template` SET `RewardFactionValueId1` = 0, `RewardFactionValueIdOverride1` = 50000 WHERE `id` IN (11612,11623,11605,11607,11609,11655,11656,11660,11661,11949,11950,11968,11619,11456,11457,11958,12028,12030,12031,12016,12009,11945,11960,11960);
+UPDATE `quest_template` SET `RewardFactionValueId1` = 0, `RewardFactionValueIdOverride1` = 70000 WHERE `id` IN (11617,11610,11613,11458,11959,12017);
+UPDATE `quest_template` SET `RewardFactionValueId1` = 0, `RewardFactionValueIdOverride1` = 30000 WHERE `id` IN (11961,11620);
+UPDATE `quest_template` SET `RewardFactionValueId1` = 0, `RewardFactionValueIdOverride1` = 100000 WHERE `id` IN (11626,12032);
+ 
