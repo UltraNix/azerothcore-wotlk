366f598ee7307d48ebed17a6766cf68b0da47a1a
Dootz <k.kozubek@wp.pl>
2018-02-23 21:26:57 +0100

    DB/Quest: Parts for the Job - increase drop chance

---
+++ sql/updates/world/2018_02_23_creature_loot_template_shoveltusk.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -40 WHERE `item` = 33611 AND `entry` IN (23690,24791);
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -30 WHERE `item` = 33611 AND `entry` = 23691;
