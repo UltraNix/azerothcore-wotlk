dbce170d3a71d9a220d5bf75ea26bc96b9719560
Dootz <k.kozubek@wp.pl>
2018-02-26 23:01:39 +0100

    followup

--- sql/updates/world/2018_02_26_creature_loot_template_elwynn_wolf.sql
+++ sql/updates/world/2018_02_26_creature_loot_template_elwynn_wolf.sql
@@ -1,1 +1,1 @@
-UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 30 WHERE `entry` IN (69,299) AND `item` = 4865;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 30 WHERE `entry` IN (69,299) AND `item` IN (4865,7073,7074);
