833ecec308ade849faa06e9057b88d173c0fb15d
Afgann <Marcin60000@gmail.com>
2018-02-27 18:05:37 +0100

    ICC rep corrections.
    (cherry picked from commit f384d02bebcf9b3cf2fd5ad41d0d1224337d0ae1)

---
+++ sql/updates/world/2018_02_23_icc_rep.sql
@@ -0,0 +1,49 @@
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` IN 
+(
+	38508,
+	38596,
+	38597,
+	38598, -- Blood Beast
+	38184,
+	38185, -- Corrupted Ymirjar
+	37116,
+	38256,
+	38693,
+	38694, -- Skybreaker Sorcerer
+	36969,
+	38408,
+	38689,
+	38690, -- Skybreaker Rifleman
+	36978,
+	38407,
+	38687,
+	38688, -- Skybreaker Mortar Soldier
+	36950,
+	38406,
+	38685,
+	38686, -- Skybreaker Marine
+	36961,
+	38261,
+	38691,
+	38692, -- Skybreaker Sergeant
+	37117,
+	38257,
+	38677,
+	38678, -- Kor'kron Battle-Mage
+	36968,
+	38403,
+	38675,
+	38676, -- Kor'kron Axethrower
+	36982,
+	38405,
+	38681,
+	38682, -- Kor'kron Rocketeer
+	36957,
+	38404,
+	38679,
+	38680, -- Kor'kron Reaver
+	36960,
+	38262,
+	38683,
+	38684  -- Kor'kron Sergeant
+);
