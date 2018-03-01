4a7fa8a144d64ee8b55fd793f80ccd4eb910b2df
Dootz <k.kozubek@wp.pl>
2018-03-01 21:22:05 +0100

    DB/Quest: Brunnhildar Village unlock dailies after completing prequests

---
+++ sql/updates/world/2018_03_01_quest_template_daily_storm_peaks.sql
@@ -0,0 +1,4 @@
+UPDATE `quest_template` SET `PrevQuestId` = 12906 WHERE `id` = 13422;
+UPDATE `quest_template` SET `PrevQuestId` = 12925 WHERE `id` = 13425;
+UPDATE `quest_template` SET `PrevQuestId` = 12997 WHERE `id` = 13424;
+UPDATE `quest_template` SET `PrevQuestId` = 12971 WHERE `id` = 13423;
