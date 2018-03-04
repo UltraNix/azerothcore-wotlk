cec4222aeded16914b31b00659271f3966b16193
Dootz <k.kozubek@wp.pl>
2018-03-04 23:49:35 +0100

    Varian/Thrall Battle for Undercity dmg adj

--- sql/updates/world/2018_03_04_creature_thrall_varian_boost.sql
+++ sql/updates/world/2018_03_04_creature_thrall_varian_boost.sql
@@ -1,1 +1,1 @@
-UPDATE `creature_template` SET `mindmg` = 463, `maxdmg` = 640, `attackpower` = 726, `dmg_multiplier` = 7 WHERE `entry` IN (32401, 31650)
+UPDATE `creature_template` SET `mindmg` = 463, `maxdmg` = 640, `attackpower` = 726, `dmg_multiplier` = 6.5 WHERE `entry` IN (32401, 31650)
