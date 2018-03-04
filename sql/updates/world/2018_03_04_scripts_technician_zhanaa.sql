5ea8db9bf729e4e0c689f1524e9b8f139796ff8d
Dootz <k.kozubek@wp.pl>
2018-03-04 13:07:11 +0100

    DB/Quest: The Emitter - add missing cutscene

---
+++ sql/updates/world/2018_03_04_scripts_technician_zhanaa.sql
@@ -0,0 +1,3 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 17071 AND `id` = 1;
+INSERT INTO `smart_scripts` VALUES
+(17071, 0, 1, 0, 20, 0, 100, 1, 9312, 0, 0, 0, 80, 1707100, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Technician Zhanaa - On Quest \'The Emitter\' Finished - Run Script');
