85bf6137ba4ce680bbe334333b4033574396bdc1
Dootz <k.kozubek@wp.pl>
2018-03-06 01:19:10 +0100

    DB/Text: Add sound to some lines

---
+++ sql/updates/world/2018_03_05_creature_text_sound.sql
@@ -0,0 +1,11 @@
+UPDATE `creature_text` SET `sound` = 6209 WHERE `entry` = 11218 AND `groupid` = 0;
+UPDATE `creature_text` SET `sound` = 10160 WHERE `entry` = 17455 AND `groupid` = 0;
+UPDATE `creature_text` SET `sound` = 10161 WHERE `entry` = 17455 AND `groupid` = 1;
+
+UPDATE `creature_text` SET `sound` = 1040 WHERE `entry` = 19380 AND `groupid` = 0;
+UPDATE `creature_text` SET `sound` = 2943 WHERE `entry` = 27439 AND `groupid` = 5;
+
+UPDATE `creature_text` SET `sound` = 12009 WHERE `entry` = 28636 AND `groupid` = 0;
+UPDATE `creature_text` SET `sound` = 14201 WHERE `entry` = 29621 AND `groupid` = 0;
+UPDATE `creature_text` SET `sound` = 14205 WHERE `entry` = 29621 AND `groupid` = 1;
+UPDATE `creature_text` SET `sound` = 14207 WHERE `entry` = 29621 AND `groupid` = 2;
