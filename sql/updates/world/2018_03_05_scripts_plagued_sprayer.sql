1c4c5fe2e4ab0336e35934a7fcffd1730d2abacb
Dootz <k.kozubek@wp.pl>
2018-03-06 01:15:53 +0100

    DB/Quest: A Tangled Skein - fix double quest credit for single objective

---
+++ sql/updates/world/2018_03_05_scripts_plagued_sprayer.sql
@@ -0,0 +1,6 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 2827400;
+INSERT INTO `smart_scripts` VALUES
+(2827400, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 11, 53236, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Plagued Sprayer - Action 0 - Cast huge explosion on self'),
+(2827400, 9, 1, 0, 0, 0, 100, 0, 0, 0, 0, 0, 11, 51314, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Plagued Sprayer - Action 2 - Cast summon broken GO sprayer on self'),
+(2827400, 9, 2, 0, 0, 0, 100, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Plagued Sprayer - Action 3 - Despawn'),
+(2827400, 9, 3, 0, 0, 0, 100, 0, 0, 0, 0, 0, 47, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Plagued Sprayer - Action 4 - Set unseen');
