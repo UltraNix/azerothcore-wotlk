abc9e9c75343f0601bf37590d90fdd5fd10af1e2
Afgann <Marcin60000@gmail.com>
2018-03-07 00:16:40 +0100

    ICC:
    - Set spawn time of trash to 7d.
    
    (cherry picked from commit b9df3c0e7894af23886ddae72b906b80d2ee2ba5)

---
+++ sql/updates/world/2018_03_01_icc_trash.sql
@@ -0,0 +1,55 @@
+UPDATE `creature` SET `spawntimesecs`=604800 WHERE `id` IN 
+(
+	37011, -- The Damned
+	36724, -- Servant of the Throne
+	37012, -- Ancient Skeletal Soldier
+	36725, -- Nerub'ar Broodkeeper
+	37007, -- Deathbound Ward
+	36805, -- Deathspeaker Servant
+	36808, -- Deathspeaker Zealot
+	36811, -- Deathspeaker Attendant
+	36807, -- Deathspeaker Disciple
+	36829, -- Deathspeaker High Priest
+	37021, -- Skybreaker Vicar
+	37144, -- Skybreaker Marksman
+	37017, -- Skybreaker Assasin
+	37027, -- Skybreaker Hierophant
+	36998, -- Skybreaker Protector
+	37004, -- Skybreaker Dreadblade
+	37148, -- Skybreaker Summoner
+	37016, -- Skybreaker Luminary
+	37003, -- Skybreaker Vindicator
+	37030, -- Kor'kron Primalist
+	37035, -- Kor'kron Vanquisher
+	37033, -- Kor'kron Invoker
+	37028, -- Kor'kron Stalker
+	37031, -- Kor'kron Oracle
+	37032, -- Kor'kron Defender
+	37544, -- Spire Gargoyle
+	37546, -- Frenzied Abomination
+	38490, -- Rotting Frost Giant
+	37545, -- Spire Minion
+	37230, -- Spire Frostwyrm
+	37098, -- Val'kyr Herald
+	37022, -- Blighted Abomination
+	37023, -- Plague Scientist
+	10404, -- Pustilating Horror
+	37038, -- Vengeful Fleshreaper
+	36880, -- Decaying Colossus
+	37217, -- Precious
+	37025, -- Stinky
+	37664, -- Darkfallen Archmage
+	37595, -- Darkfallen Blood Knight
+	37571, -- Darkfallen Advisor
+	37663, -- Darkfallen Noble
+	37666, -- Darkfallen Tactician
+	37662, -- Darkfallen Commander
+	37665, -- Darkfallen Lieutenant
+	37134, -- Ymirjar Huntress
+	37132, -- Ymirjar Battle-Maiden
+	37133, -- Ymirjar Warlord
+	37127, -- Ymirjar Frostbinder
+	38125, -- Ymirjar Deathbringer
+	37531, -- Frostwarden Handler
+	37532, -- Frostwing Whelp
+);
