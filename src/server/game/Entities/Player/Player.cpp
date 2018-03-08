97d7e069accea65c93a676339d43d67fa14af808
sitowsky <sitowsky@posteo.net>
2018-03-08 23:46:32 +0100

    Premium Sickness only below 80 at Angrathar realm.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -5304,7 +5304,8 @@ void Player::ResurrectPlayer(float restore_percent, bool applySickness)
     UpdateObjectVisibility();
 
     // Premium service
-    if (GetSession()->IsPremiumServiceActive(PREMIUM_NO_RESSURECTION_SICKNESS))
+    bool allowPremium = sWorld->getBoolConfig(CONFIG_SPECIAL_ANGRATHAR) == true ? getLevel() < 80 /* Angrthar affect only below 80 level */ : getLevel() <= 80 /* Feronis affect every1 */;
+    if (GetSession()->IsPremiumServiceActive(PREMIUM_NO_RESSURECTION_SICKNESS) && allowPremium)
         return;
 
     if(!applySickness)
