55c42a2444c8a16ec173ee9e3510d19e19585733
Dootz <k.kozubek@wp.pl>
2018-03-10 22:32:35 +0100

    DB/NPC: Add Pattern: Fur Lining - Arcane Resist to loot table

---
+++ sql/updates/world/2018_03_10_creature_loot_pattern_fur.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature_loot_template` WHERE `entry` IN (32297,31702) AND `item` = 44563;
+INSERT INTO `creature_loot_template` VALUES
+(32297, 44563, 0.18, 1, 0, 1, 1),
+(31702, 44563, 0.08, 1, 0, 1, 1);
