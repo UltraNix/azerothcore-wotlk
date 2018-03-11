d807b1c5483237d68799acfb0e7628003f78edb0
Riztazz <felianther15@gmail.com>
2018-03-11 19:34:03 +0100

    DB/Loot: Fix drakuru skull not disappearing

---
+++ sql/updates/world/2018_03_11_drakkuru_gameobject_loot.sql
@@ -0,0 +1,3 @@
+UPDATE `gameobject_template` SET `AIName` = "" WHERE `entry` = 191458;
+UPDATE `gameobject_loot_template` SET `groupid` = 0 WHERE `entry` = 25108 AND `item` = 43059;
+UPDATE `gameobject_loot_template` SET `groupid` = 1 WHERE `entry` = 25108 AND `item` = 40425;
