7f0822cb21c840ff12d6565f58dadb706f2fd6fd
Dootz <k.kozubek@wp.pl>
2018-03-12 21:30:19 +0100

    DB/NPC: Cultist Shard Watcher - fix casting times

---
+++ sql/updates/world/2018_03_12_scripts_cultist_shard_watcher.sql
@@ -0,0 +1,2 @@
+UPDATE `smart_scripts` SET `event_param1` = 5000, `event_param2` = 7000 WHERE `entryorguid` = 32349 AND `id` = 1;
+UPDATE `smart_scripts` SET `event_param2` = 3000 WHERE `entryorguid` = 32349 AND `id` = 2;
