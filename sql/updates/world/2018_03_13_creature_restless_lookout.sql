0237da71729e5969a954bb32f02a520685241099
Dootz <k.kozubek@wp.pl>
2018-03-13 20:05:03 +0100

    DB/Quest: Honor is for the Weak - fix incorrect spawns

---
+++ sql/updates/world/2018_03_13_creature_restless_lookout.sql
@@ -0,0 +1,3 @@
+UPDATE `creature` SET `id` = 30951 WHERE `guid` IN (121775, 121776, 121777);
+UPDATE `creature` SET `position_x` = 7903.92, `position_y` = 2836.02, `position_z` = 498.73, `MovementType` = 0 WHERE `guid` = 121775;
+UPDATE `creature` SET `position_x` = 7942.03, `position_y` = 2836.31, `position_z` = 485.73, `MovementType` = 0 WHERE `guid` = 121777;
