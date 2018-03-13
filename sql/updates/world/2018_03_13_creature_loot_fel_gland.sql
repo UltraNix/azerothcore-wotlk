1792571c31bc0f1d9103a5ba9c84a84b73219245
Dootz <k.kozubek@wp.pl>
2018-03-14 00:29:07 +0100

    DB/Quest: A Slow Death - adjust drop rate

---
+++ sql/updates/world/2018_03_13_creature_loot_fel_gland.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -96 WHERE `item` = 32502 AND `entry` = 21901;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -81 WHERE `item` = 32502 AND `entry` = 21462;
