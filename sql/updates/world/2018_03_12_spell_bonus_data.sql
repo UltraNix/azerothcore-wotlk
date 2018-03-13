3283d8402532e0323ee2fcb6e335658f0169d626
Afgann <Marcin60000@gmail.com>
2018-03-13 22:12:22 +0100

    Shadow Ward coeff.
    (cherry picked from commit 146d9159edc1b3aea667e584c55c4821c6f1d790)

---
+++ sql/updates/world/2018_03_12_spell_bonus_data.sql
@@ -0,0 +1,8 @@
+DELETE FROM `spell_bonus_data` WHERE `entry` IN (6229, 11739, 11740, 28610, 47890, 47891);
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `comments`) VALUES
+(6229, 0.806, "Shadow Ward"),
+(11739, 0.806, "Shadow Ward"),
+(11740, 0.806, "Shadow Ward"),
+(28610, 0.806, "Shadow Ward"),
+(47890, 0.806, "Shadow Ward"),
+(47891, 0.806, "Shadow Ward");
