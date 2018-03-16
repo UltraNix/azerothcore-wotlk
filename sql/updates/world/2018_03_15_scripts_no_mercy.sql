0569c1ae27ae09b461e69709a94428ab5d4886e2
Dootz <k.kozubek@wp.pl>
2018-03-16 10:32:38 +0100

    No Mercy followup

--- sql/updates/world/2018_03_15_scripts_no_mercy.sql
+++ sql/updates/world/2018_03_15_scripts_no_mercy.sql
@@ -1,6 +1,6 @@
-UPDATE `smart_scripts` SET `action_type` = 49, `action_param1` = 0, `target_type` = 21, `target_param1` = 10, `comment` = "Chancellor Amai - Script - Attack Start" WHERE `entryorguid` = 2738100 AND `id` = 3;
-UPDATE `smart_scripts` SET `action_type` = 49, `action_param1` = 0, `target_type` = 21, `target_param1` = 10, `comment` = "Deathguard Schneider - Script - Attack Start" WHERE `entryorguid` = 2737600 AND `id` = 3;
-UPDATE `smart_scripts` SET `action_type` = 49, `action_param1` = 0, `target_type` = 21, `target_param1` = 10, `comment` = "Engineer Burke - Script - Attack Start" WHERE `entryorguid` = 2737900 AND `id` = 3;
-UPDATE `smart_scripts` SET `action_type` = 49, `action_param1` = 0, `target_type` = 21, `target_param1` = 10, `comment` = "Senior Scrivener Barriga - Script - Attack Start" WHERE `entryorguid` = 2737800 AND `id` = 3;
+UPDATE `smart_scripts` SET `action_type` = 49, `action_param1` = 0, `target_type` = 21, `target_param1` = 20, `comment` = "Chancellor Amai - Script - Attack Start" WHERE `entryorguid` = 2738100 AND `id` = 3;
+UPDATE `smart_scripts` SET `action_type` = 49, `action_param1` = 0, `target_type` = 21, `target_param1` = 20, `comment` = "Deathguard Schneider - Script - Attack Start" WHERE `entryorguid` = 2737600 AND `id` = 3;
+UPDATE `smart_scripts` SET `action_type` = 49, `action_param1` = 0, `target_type` = 21, `target_param1` = 20, `comment` = "Engineer Burke - Script - Attack Start" WHERE `entryorguid` = 2737900 AND `id` = 3;
+UPDATE `smart_scripts` SET `action_type` = 49, `action_param1` = 0, `target_type` = 21, `target_param1` = 20, `comment` = "Senior Scrivener Barriga - Script - Attack Start" WHERE `entryorguid` = 2737800 AND `id` = 3;
 
 UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 536870912 WHERE `entry` IN (27381,27376,27379,27378);
