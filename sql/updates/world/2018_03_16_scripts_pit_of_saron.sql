006d40c99bc49cb4e54f28399117770c599b443a
Dootz <k.kozubek@wp.pl>
2018-03-16 10:33:20 +0100

    Instance/PitOfSaron: Link in formation group after second boss

---
+++ sql/updates/world/2018_03_16_scripts_pit_of_saron.sql
@@ -0,0 +1,3 @@
+UPDATE `smart_scripts` SET `action_param1` = 15, `target_type` = 1, `target_param1` = 0 WHERE `entryorguid` = 36893 AND `id` = 4;
+UPDATE `smart_scripts` SET `action_param1` = 15, `target_type` = 1, `target_param1` = 0 WHERE `entryorguid` = 36840 AND `id` = 4;
+UPDATE `smart_scripts` SET `action_param1` = 15, `target_type` = 1, `target_param1` = 0 WHERE `entryorguid` = 36892 AND `id` = 1;
