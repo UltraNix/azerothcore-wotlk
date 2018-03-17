005a735bd609ea8cae1338a0e1fb33e7b8e97b83
Riztazz <felianther15@gmail.com>
2018-03-17 17:44:45 +0100

    missing sql

---
+++ sql/updates/world/2018_03_17_spell_script_names_sapphiron.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (28531, 55799);
+INSERT INTO `spell_script_names` VALUES
+(28531, "spell_sapphiron_frost_aura"),
+(55799, "spell_sapphiron_frost_aura");
