f2d52e900d60967e582bbb750967f0a2092bea9c
Dootz <k.kozubek@wp.pl>
2018-03-18 20:12:45 +0100

    cursed scarab followup

--- sql/updates/world/2018_03_18_scripts_cursed_scarab.sql
+++ sql/updates/world/2018_03_18_scripts_cursed_scarab.sql
@@ -1,1 +1,1 @@
-UPDATE `smart_scripts` SET `event_param1` = 36561, `action_param1` = 0 WHERE `entryorguid` = 21306 AND `id` = 5;
+UPDATE `smart_scripts` SET `action_param1` = 0 WHERE `entryorguid` = 21306 AND `id` = 5;
