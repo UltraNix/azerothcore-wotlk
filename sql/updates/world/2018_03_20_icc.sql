0e43bc997a7a85f74baeef9ef3a52091f67f8bc7
Afgann <Marcin60000@gmail.com>
2018-03-20 23:28:09 +0100

    ICC:
    - Melee corrections.

--- sql/updates/world/2018_03_20_icc.sql
+++ sql/updates/world/2018_03_20_icc.sql
@@ -14,3 +14,19 @@ DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_rotface_vile_gas_
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (72285, 'spell_rotface_vile_gas_trigger'),
 (72288, 'spell_rotface_vile_gas_trigger');
+-- Terenas
+UPDATE `creature_template` SET `mindmg`=3000, `maxdmg`=4000, `dmg_multiplier`=4.5 WHERE `entry`=36823;
+UPDATE `creature_template` SET `mindmg`=0, `maxdmg`=0, `dmg_multiplier`=1 WHERE `entry`=38579;
+UPDATE `creature_template` SET `mindmg`=3000, `maxdmg`=4000, `dmg_multiplier`=3 WHERE `entry`=39217;
+-- Ymirjar Battle-Maiden
+UPDATE `creature_template` SET `baseattacktime`=1000 WHERE `entry` IN (37132, 38132);
+-- Precious
+UPDATE `creature_template` SET `baseattacktime`=1000 WHERE `entry` IN (37217, 38103);
+-- The Damned
+UPDATE `creature_template` SET `baseattacktime`=1000 WHERE `entry`=38061;
+-- Vampiric Fiend
+UPDATE `creature_template` SET `dmgschool`=5, `dmg_multiplier`=7.5 WHERE `entry` IN (37901, 38600);
+-- Lord Marrowgar
+UPDATE `creature_template` SET `mindmg`=9400, `maxdmg`=10200, `dmg_multiplier`=10 WHERE `entry`=37959;
+-- Blood Beast
+UPDATE `creature_template` SET `mindmg`=1500, `maxdmg`=2000, `dmg_multiplier`=10 WHERE `entry` IN (38508, 38596, 38597, 38598);
