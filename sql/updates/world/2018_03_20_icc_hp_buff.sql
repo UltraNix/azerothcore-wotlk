2b9443c7f5289f282cbf8e2ed1dcaafe5c0c17db
Afgann <Marcin60000@gmail.com>
2018-03-20 23:21:11 +0100

    ICC:
    - HP Buffs.
    
    (cherry picked from commit 57f736f476c6c1bacc7377359de826df075d49ae)

---
+++ sql/updates/world/2018_03_20_icc_hp_buff.sql
@@ -0,0 +1,10 @@
+-- Deathbringer Saurfang
+UPDATE `creature_template` SET `health_mod`=3716.9 WHERE `entry`=38583; -- 3150->3716.9, ~18% increase
+-- Professor Putricide
+UPDATE `creature_template` SET `health_mod`=4030.3 WHERE `entry`=38586; -- 3600->4030.3, ~12% increase
+-- Blood Queen Lana'thel
+UPDATE `creature_template` SET `health_mod`=5777.8 WHERE `entry`=38436; -- 5113->5777.8, ~12% increase
+-- Sindragosa
+UPDATE `creature_template` SET `health_mod`=3893.8 WHERE `entry`=38267; -- 3300->3893.8, ~18% increase
+-- The Lich King
+UPDATE `creature_template` SET `health_mod`=8284.8 WHERE `entry`=39168; -- 7397->8284.8, ~12% increase
