a865ee48bf74abdeb86325930f203855600f6fc2
Dootz <k.kozubek@wp.pl>
2018-03-21 23:58:24 +0100

    Return of High Chief followup

--- sql/updates/world/2018_03_21_scripts_roanauk.sql
+++ sql/updates/world/2018_03_21_scripts_roanauk.sql
@@ -1,6 +1,6 @@
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 2665400;
 INSERT INTO `smart_scripts` VALUES
-(2665400, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 18, 768, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, ''),
+(2665400, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 18, 768, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Roanauk Icemist - Script - Set Unit Flags'),
 (2665400, 9, 1, 0, 0, 0, 100, 0, 0, 0, 0, 0, 28, 47273, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Roanauk Icemist - Script - Remove Aura Icemist\'s Prison'),
 (2665400, 9, 2, 0, 0, 0, 100, 0, 0, 0, 0, 0, 45, 2, 2, 0, 0, 0, 0, 9, 26656, 0, 200, 0, 0, 0, 0, 'Roanauk Icemist - Script - Set Data 2 2 on Anub\'ar Prison'),
 (2665400, 9, 3, 0, 0, 0, 100, 0, 0, 0, 0, 0, 22, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Roanauk Icemist - Script - Set Phase 2'),
