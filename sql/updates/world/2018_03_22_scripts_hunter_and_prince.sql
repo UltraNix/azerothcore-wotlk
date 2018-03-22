017e67565ff24f0b43a4c3b4c5cda00b2772d284
Dootz <k.kozubek@wp.pl>
2018-03-22 22:48:37 +0100

    DB/Quest: Hunter and the Prince - despawn Illidan on quest fail

---
+++ sql/updates/world/2018_03_22_scripts_hunter_and_prince.sql
@@ -0,0 +1,3 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 31395 AND `id` = 6;
+INSERT INTO `smart_scripts` VALUES
+(31395, 0, 6, 0, 5, 0, 100, 0, 0, 0, 0, 32326, 41, 1000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Illidan Stormrage - On Arthas Killed - Despawn');
