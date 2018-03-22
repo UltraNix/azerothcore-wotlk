31b8b891044e8ccf388d67c92e1d4605b56401f3
Dootz <k.kozubek@wp.pl>
2018-03-22 22:54:03 +0100

    DB/Gameobject: Correct position of mining nodes in Redridge Mountains

---
+++ sql/updates/world/2018_03_22_gameobject_redridge_spawns.sql
@@ -0,0 +1,6 @@
+UPDATE `gameobject` SET `position_x` = -9666.03, `position_y` = -2035.57, `position_z` = 62.60 WHERE `guid` = 5242;
+UPDATE `gameobject` SET `position_x` = -9088.91, `position_y` = -3300.07, `position_z` = 102.70 WHERE `guid` = 5199;
+UPDATE `gameobject` SET `position_x` = -9080.11, `position_y` = -2768.02, `position_z` = 118.78 WHERE `guid` = 5632;
+UPDATE `gameobject` SET `position_x` = -9049.41, `position_y` = -2379.75, `position_z` = 133.93 WHERE `guid` = 5228;
+UPDATE `gameobject` SET `position_x` = -9162.98, `position_y` = -2096.76, `position_z` = 90.46 WHERE `guid` = 5149;
+UPDATE `gameobject` SET `position_x` = -9357.28, `position_y` = -1887.18, `position_z` = 72.95 WHERE `guid` = 5027;
