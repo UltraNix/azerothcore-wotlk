cb0994b0e52cc249b46d5235a653b40a7c09d9a3
Dootz <k.kozubek@wp.pl>
2018-03-25 12:52:27 +0200

    DB/Quest: Natura Armor - adjust drop rate

---
+++ sql/updates/world/2018_03_25_creature_loot_outland_q.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -100 WHERE `entry` = 18214 AND `item` = 24486;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -30 WHERE `entry` = 18128 AND `item` = 24426;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 30 WHERE `entry` = 18134 AND `item` = 24427;
