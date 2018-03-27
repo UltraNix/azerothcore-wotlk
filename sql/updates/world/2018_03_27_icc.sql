4383bf9cc8f6677ece2f0a7ffd2b098965a24245
Afgann <Marcin60000@gmail.com>
2018-03-27 22:12:12 +0200

    ICC:
    - Harvest Soul position correction.
    - Lord Marrowgar melee dmg correction.

---
+++ sql/updates/world/2018_03_27_icc.sql
@@ -0,0 +1,4 @@
+-- Harvest Soul
+UPDATE `spell_target_position` SET `target_position_z`=1051.99 WHERE `id`=72546;
+-- Lord Marrowgar
+UPDATE `creature_template` SET `mindmg`=8800, `maxdmg`=9700, `dmg_multiplier`=10 WHERE `entry`=37959;
