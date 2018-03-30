c1762fbc631253d0eb3b6ba4f14759c5f3a7ffc8
Dootz <k.kozubek@wp.pl>
2018-03-30 23:41:55 +0200

    DB/Quest: Dragonmaw race - fix bug with npc going underground during race

---
+++ sql/updates/world/2018_03_28_waypoints_dragonmaw_race.sql
@@ -0,0 +1,2 @@
+UPDATE `waypoints` SET `position_x` =  -5086.21, `position_y` = 53.50, `position_z` = 94.15 WHERE `entry` = 23345 AND `pointid` = 38;
+UPDATE `waypoints` SET `position_x` =  -5123.20, `position_y` = 48.70, `position_z` = 92.71 WHERE `entry` = 23345 AND `pointid` = 39;
