8ef5c272ac3d80fbff74bbdcbd62b852c6011071
Dootz <k.kozubek@wp.pl>
2018-03-30 23:47:54 +0200

    DB/Quest: Into the Wild Green Yonder - prevent npc stuck

---
+++ sql/updates/world/2018_03_30_conditions_quest_into_the_wild_green.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceEntry` = 56683 AND `ConditionTypeOrReference` = 1;
+INSERT INTO `conditions` VALUES
+(17, 0, 56683, 0, 0, 1, 0, 56687, 0, 0, 1, 0, 0, '', 'Grab Captured Crusader - Only if Proto Drake does not carry one already');
