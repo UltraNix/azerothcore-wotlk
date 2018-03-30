9d12c162c6de28d5f97b5fcb67fd6f8c3b7cfd86
Dootz <k.kozubek@wp.pl>
2018-03-30 23:40:04 +0200

    DB/Quest: Affaray - maybe it will prevent this weird bug for some ppl, no idea :(

---
+++ sql/updates/world/2018_03_28_quest_affaray.sql
@@ -0,0 +1,2 @@
+UPDATE `quest_template` SET `PrevQuestId` = 0, `NextQuestId` = 0, `NextQuestIdChain` = 0 WHERE `id` = 1718;
+UPDATE `quest_template` SET `PrevQuestId` = 1718, `NextQuestId` = 0, `NextQuestIdChain` = 0 WHERE `id` = 1719;
