1e2485eb5058f392576e5ddd5d6444f2ceaba5ec
Dootz <k.kozubek@wp.pl>
2018-03-30 23:43:49 +0200

    DB/Quest: Speaking with the Wind's Voice, Emergency Measures - adjust drop chance

---
+++ sql/updates/world/2018_03_29_creature_loot_q.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -35 WHERE `entry` = 29624 AND `item` = 41514;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -40 WHERE `entry` = 30164 AND `item` = 42510;
