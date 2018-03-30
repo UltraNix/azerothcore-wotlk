f38e2f6bb5ab6355313844996e6f26b63a5569f9
Dootz <k.kozubek@wp.pl>
2018-03-30 23:37:51 +0200

    DB/NPC: Mammoth Bull - fix fear spam

---
+++ sql/updates/world/2018_03_28_conditions_mammoth.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceEntry` = 25743 AND `SourceTypeOrReferenceId` = 22;
+INSERT INTO `conditions` VALUES
+(22, 2, 25743, 0, 0, 1, 0, 46260, 0, 0, 1, 0, 0, '', 'dont use sai fear while player-controlled');
