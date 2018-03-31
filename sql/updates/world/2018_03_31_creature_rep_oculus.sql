25e1c848a188c49b1feea23b032971200b903ba7
Dootz <k.kozubek@wp.pl>
2018-03-31 19:42:51 +0200

    DB/Instance: Oculus  normal - adjust onkill reputation

---
+++ sql/updates/world/2018_03_31_creature_rep_oculus.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 5, `RewOnKillRepValue2` = 5 WHERE `creature_id` IN (27633, 27635, 27638, 27651, 27653,27650);
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 50, `RewOnKillRepValue2` = 50 WHERE `creature_id` IN (27654, 27447, 27655, 27656);
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 15, `RewOnKillRepValue2` = 15 WHERE `creature_id` IN (27641);
