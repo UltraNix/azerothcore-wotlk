45ed20fc256e9ed3713ffa56c07d980db26a1179
Dootz <k.kozubek@wp.pl>
2018-04-01 19:37:08 +0200

    Betrayal - despawn portal, adjust skull despawn time

--- sql/updates/world/2018_03_30_scripts_betrayal.sql
+++ sql/updates/world/2018_03_30_scripts_betrayal.sql
@@ -2,6 +2,8 @@ UPDATE `gameobject_template` SET `AIName` = "SmartGameObjectAI" WHERE `entry` = 
 UPDATE `smart_scripts` SET `link` = 13 WHERE `entryorguid` = 28998 AND `id` = 11;
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 28998 AND `id` = 13;
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 191458 AND `source_type` = 1;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 202357 AND `source_type` = 1 AND `id` = 1;
 INSERT INTO `smart_scripts` VALUES
 (28998, 0, 13, 0, 61, 0, 100, 0, 0, 0, 0, 0, 45, 4, 4, 0, 0, 0, 0, 9, 28931, 0, 100, 0, 0, 0, 0, 'Overlord Drakuru - OOC - Set Data On Trolls'),
-(191458, 1, 0, 0, 1, 0, 100, 0, 120000, 120000, 120000, 120000, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Skull - OOC -Despawn');
+(191458, 1, 0, 0, 1, 0, 100, 0, 120000, 120000, 120000, 120000, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Skull - OOC -Despawn'),
+(202357, 1, 1, 0, 1, 0, 100, 0, 120000, 120000, 120000, 120000, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Drakurus Last Wish - OOC -Despawn');
