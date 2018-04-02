44b81fb959992a91b1a7cda66052c55e13e2bd85
Dootz <k.kozubek@wp.pl>
2018-04-03 00:41:02 +0200

    DB/Quest: Willix the importer - prevent Escort start at the end of dungeon

---
+++ sql/updates/world/2018_04_02_conditions_willix.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceEntry` = 4508 AND `SourceGroup` = 1;
+INSERT INTO `conditions` VALUES
+(22, 1, 4508, 0, 0, 30, 0, 69438, 10, 0, 0, 0, 0, '', 'Willix - start WP - only when in tent');
