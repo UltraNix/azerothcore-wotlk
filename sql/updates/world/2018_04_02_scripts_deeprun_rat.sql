08679dca47bcac889c25856daee5737f852e7a3a
Dootz <k.kozubek@wp.pl>
2018-04-03 00:41:39 +0200

    DB/Quest: Deeprun Rat - despawn rats after credit

---
+++ sql/updates/world/2018_04_02_scripts_deeprun_rat.sql
@@ -0,0 +1,8 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 13016;
+INSERT INTO `smart_scripts` VALUES
+(13016, 0, 0, 1, 8, 0, 100, 0, 21050, 0, 0, 0, 29, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Deeprun Rat - On Spellhit - Follow Player'),
+(13016, 0, 1, 2, 61, 0, 100, 0, 0, 0, 0, 0, 41, 10000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Deeprun Rat - On Spellhit - Follow Player'),
+(13016, 0, 2, 3, 61, 0, 100, 0, 0, 0, 0, 0, 36, 13017, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Deeprun Rat - On Spellhit - Update Template'),
+(13016, 0, 3, 4, 61, 0, 100, 0, 0, 0, 0, 0, 33, 12997, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Deeprun Rat - On Spellhit - Quest Credit'),
+(13016, 0, 4, 0, 61, 0, 100, 0, 0, 0, 0, 0, 11, 21051, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Deeprun Rat - On Spellhit - Cast Spell'),
+(13016, 0, 5, 0, 8, 0, 100, 0, 21052, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Deeprun Rat - On Spellhit - Die');
