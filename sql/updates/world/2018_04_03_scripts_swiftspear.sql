82ac4923ce9c1e8e60bfbcb87f4d2b8d8b6c8f5d
Dootz <k.kozubek@wp.pl>
2018-04-03 11:26:21 +0200

    DB/Quest: Memories of Stormhoof - add event

---
+++ sql/updates/world/2018_04_03_scripts_swiftspear.sql
@@ -0,0 +1,5 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 30395;
+INSERT INTO `smart_scripts` VALUES
+(30395, 0, 0, 1, 62, 0, 100, 0, 9906, 1, 0, 0, 11, 56760, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Chieftain Swiftspear - On Gossip Option 0 Selected - Cast \'Trigger Swiftspear Signal\''),
+(30395, 0, 1, 2, 61, 0, 100, 0, 0, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Chieftain Swiftspear - On Gossip Option 0 Selected - Close Gossip'),
+(30395, 0, 2, 0, 61, 0, 100, 0, 0, 0, 0, 0, 80, 3039500, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Chieftain Swiftspear - On Gossip Option 0 Selected - Run Script');
