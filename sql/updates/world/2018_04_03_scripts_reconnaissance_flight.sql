e6b872854c388d842ef66d8db5e2e8e94c94c579
Dootz <k.kozubek@wp.pl>
2018-04-03 15:04:21 +0200

    DB/Quest: Reconnaissance flight - remove auras from player before flight

---
+++ sql/updates/world/2018_04_03_scripts_reconnaissance_flight.sql
@@ -0,0 +1,4 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 28746;
+INSERT INTO `smart_scripts` VALUES
+(28746, 0, 0, 1, 19, 0, 100, 0, 12671, 0, 0, 0, 28, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Pilot Vic - On Quest \'Reconnaissance Flight\' Taken - Remove Auras from Player\''),
+(28746, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 11, 52256, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Pilot Vic - On Quest \'Reconnaissance Flight\' Taken - Cast \'Vic\'s Flying Machine Validate\'');
