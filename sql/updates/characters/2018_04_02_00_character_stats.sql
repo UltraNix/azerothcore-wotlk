5cfe211ee55797bbcd09df3de01660197b4e39ed
Riztazz <felianther15@gmail.com>
2018-04-03 09:09:48 +0200

    Fix sql

--- sql/updates/characters/2018_04_02_00_character_stats.sql
+++ sql/updates/characters/2018_04_02_00_character_stats.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `character_stats` 
 ADD `spellHit` INT(10)  UNSIGNED NOT NULL DEFAULT '0' AFTER `regMana`, 
 ADD `meleeHit` INT(10)  UNSIGNED NOT NULL DEFAULT '0' AFTER `spellHit`, 
-ADD `rangeHit` INT(10)  UNSIGNED NOT NULL DEFAULT '0' AFTER `meleeHit`, 
+ADD `rangeHit` INT(10)  UNSIGNED NOT NULL DEFAULT '0' AFTER `meleeHit`;
