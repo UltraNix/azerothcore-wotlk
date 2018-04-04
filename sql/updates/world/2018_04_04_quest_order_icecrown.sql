c46c64d9411133547511bc92499ead980a1718af
Dootz <k.kozubek@wp.pl>
2018-04-04 14:20:55 +0200

    DB/Quest: Icecrown quest Opportunity - order

---
+++ sql/updates/world/2018_04_04_quest_order_icecrown.sql
@@ -0,0 +1,6 @@
+DELETE FROM `conditions` WHERE `SourceEntry` = 13258 AND `SourceTypeOrReferenceId` IN (19,20);
+INSERT INTO `conditions` VALUES
+(19, 0, 13258, 0, 1, 8, 0, 13224, 0, 0, 0, 0, 0, '', 'Show Opportunity after Shadow Vault and Orgrim Hammer'),
+(20, 0, 13258, 0, 1, 8, 0, 13224, 0, 0, 0, 0, 0, '', 'Show Opportunity after Shadow Vault and Orgrim Hammer'),
+(19, 0, 13258, 0, 1, 8, 0, 12899, 0, 0, 0, 0, 0, '', 'Show Opportunity after Shadow Vault and Orgrim Hammer'),
+(20, 0, 13258, 0, 1, 8, 0, 12899, 0, 0, 0, 0, 0, '', 'Show Opportunity after Shadow Vault and Orgrim Hammer');
