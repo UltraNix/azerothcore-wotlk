5e533e4dbf07e33e7a94e1fac22c9ab81ccbcc8a
Dootz <k.kozubek@wp.pl>
2018-04-05 23:40:53 +0200

    DB/GO: fix spawn pos of some deposits in durotar and lights chapel

---
+++ sql/updates/world/2018_04_05_go_deposit_lights_chapel.sql
@@ -0,0 +1,1 @@
+UPDATE `gameobject` SET `position_z` = 56.60 WHERE `guid` IN (9539, 74339, 74338, 74337, 74336, 74335);
---
+++ sql/updates/world/2018_04_05_go_durotar_vein.sql
@@ -0,0 +1,2 @@
+UPDATE `gameobject` SET `position_x` = 1118.02, `position_y` = -4472.65, `position_z` = 20.47 WHERE `guid` = 4788;
+UPDATE `gameobject` SET `position_x` = 833.57, `position_y` = -4095.59, `position_z` = 27.027 WHERE `guid` IN (12378, 5361);
