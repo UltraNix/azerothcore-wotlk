4d5f91b80def5d1ec56cd5a49778b7be76a8e7c7
Dootz <k.kozubek@wp.pl>
2018-04-05 23:41:16 +0200

    DB/Quest: Retest now - adjust drop chance

---
+++ sql/updates/world/2018_04_05_creature_loot_retest_now.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -32 WHERE `entry` = 32236 AND `item` = 44301;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -49 WHERE `entry` = 32279 AND `item` = 44301;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -50 WHERE `entry` = 32276 AND `item` = 44301;
