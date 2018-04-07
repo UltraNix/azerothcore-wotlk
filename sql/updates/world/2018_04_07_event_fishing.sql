2564294f61b9b8a1adb733a1d7a64901e99925b0
Dootz <k.kozubek@wp.pl>
2018-04-07 15:17:45 +0200

    DB/Event: Fishin events update start time

---
+++ sql/updates/world/2018_04_07_event_fishing.sql
@@ -0,0 +1,5 @@
+UPDATE `game_event` SET `start_time` = '2018-04-15 01:00:00' WHERE `eventEntry` = 14;
+UPDATE `game_event` SET `start_time` = '2018-04-15 14:00:00' WHERE `eventEntry` = 15;
+UPDATE `game_event` SET `start_time` = '2018-04-15 14:00:00' WHERE `eventEntry` = 62;
+UPDATE `game_event` SET `start_time` = '2018-04-14 13:00:00' WHERE `eventEntry` = 63;
+UPDATE `game_event` SET `start_time` = '2018-04-14 14:00:00' WHERE `eventEntry` = 64;
