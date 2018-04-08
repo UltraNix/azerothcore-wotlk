26fa4aabf66a480e9ea2abc0d07e36bc9637d12d
Dootz <k.kozubek@wp.pl>
2018-04-08 21:25:36 +0200

    DB/Go: Reduce Northrend herb & mininng resp time

---
+++ sql/updates/world/2018_04_08_go_herb_vein.sql
@@ -0,0 +1,3 @@
+UPDATE `gameobject` SET `spawntimesecs` = 180 WHERE `id` IN (191019,190171,190170) AND `spawntimesecs` < 2700;
+UPDATE `gameobject` SET `spawntimesecs` = 180 WHERE `spawntimesecs` < 900 AND `id` IN (189973, 190169);
+UPDATE `gameobject` SET `spawntimesecs` = 180 WHERE `id` IN (189981,191133,189980) AND `spawntimesecs` < 600;
