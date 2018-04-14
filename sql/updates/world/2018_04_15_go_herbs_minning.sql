271a9b8d55c1bd72e9555796edeb99874b395b0e
Dootz <k.kozubek@wp.pl>
2018-04-15 00:23:45 +0200

    DB/GO: Herbs/minning - increase number of spawn points, Reduce respawn time of Adder's Tongue

---
+++ sql/updates/world/2018_04_15_go_herbs_minning.sql
@@ -0,0 +1,17 @@
+UPDATE `pool_template` SET `max_limit` = 90 WHERE `entry` = 9908;
+UPDATE `pool_template` SET `max_limit` = 60 WHERE `entry` = 9905;
+UPDATE `pool_template` SET `max_limit` = 70 WHERE `entry` = 9907;
+UPDATE `pool_template` SET `max_limit` = 25 WHERE `entry` = 896;
+UPDATE `pool_template` SET `max_limit` = 40 WHERE `entry` = 9904;
+
+UPDATE `pool_template` SET `max_limit` = 40 WHERE `entry` = 987;
+UPDATE `pool_template` SET `max_limit` = 50 WHERE `entry` = 986;
+UPDATE `pool_template` SET `max_limit` = 50 WHERE `entry` = 985;
+UPDATE `pool_template` SET `max_limit` = 30 WHERE `entry` = 984;
+UPDATE `pool_template` SET `max_limit` = 50 WHERE `entry` = 983;
+UPDATE `pool_template` SET `max_limit` = 60 WHERE `entry` = 982;
+UPDATE `pool_template` SET `max_limit` = 50 WHERE `entry` = 981;
+UPDATE `pool_template` SET `max_limit` = 50 WHERE `entry` = 980;
+UPDATE `pool_template` SET `max_limit` = 30 WHERE `entry` = 979;
+
+UPDATE `gameobject` SET `spawntimesecs` = 180 WHERE `id` = 191019;
