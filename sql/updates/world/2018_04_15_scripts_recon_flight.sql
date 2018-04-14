9ba3da50f24663ef447e6269a0ae44a210652e7b
Dootz <k.kozubek@wp.pl>
2018-04-15 00:26:22 +0200

    DB/Quest: Reconn Flight - revert previous change

---
+++ sql/updates/world/2018_04_15_scripts_recon_flight.sql
@@ -0,0 +1,3 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 28746;
+INSERT INTO `smart_scripts` VALUES
+(28746, 0, 0, 0, 19, 0, 100, 0, 12671, 0, 0, 0, 11, 52256, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Pilot Vic - On Quest \'Reconnaissance Flight\' Taken - Cast \'Vic\'s Flying Machine Validate\'');
