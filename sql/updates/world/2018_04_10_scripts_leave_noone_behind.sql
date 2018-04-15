e990a617631748db76286a55a126e009b80ff31f
Dootz <k.kozubek@wp.pl>
2018-04-15 17:10:13 +0200

    DB/Quest: Leave no one behind - fix issue with quest credit

---
+++ sql/updates/world/2018_04_10_scripts_leave_noone_behind.sql
@@ -0,0 +1,2 @@
+UPDATE `smart_scripts` SET `action_param2` = 2 WHERE `entryorguid` IN (28136,28142,28148) AND `id` = 5;
+UPDATE `smart_scripts` SET `event_phase_mask` = 0, `event_flags` = 1 WHERE `entryorguid` IN (28136,28142,28148) AND `id` = 8;
