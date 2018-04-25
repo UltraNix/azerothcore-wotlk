a16e7aebb69f9c62ab2e2486d62c4a30144bea0c
Dootz <k.kozubek@wp.pl>
2018-04-25 18:30:54 +0200

    DB/Item: Allow use of King Mrgl-Mrgl's Spare Suit only within the Winterfin Caverns and at Winterfin Village

---
+++ sql/updates/world/2018_04_25_conditions_mgrl.sql
@@ -0,0 +1,4 @@
+DELETE FROM `conditions` WHERE `SourceEntry` = 45278;
+INSERT INTO `conditions` VALUES 
+(17, 0, 45278, 0, 1, 23, 0, 4033, 0, 0, 0, 39, 0, '', ''),
+(17, 0, 45278, 0, 2, 23, 0, 4097, 0, 0, 0, 39, 0, '', '');
