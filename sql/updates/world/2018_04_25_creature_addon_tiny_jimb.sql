b67d6332343e0d3b253ea51f24d4ad346887b920
Dootz <k.kozubek@wp.pl>
2018-04-25 20:10:57 +0200

    DB/NPC: 'Tiny' Jimb  correct position, and anim

---
+++ sql/updates/world/2018_04_25_creature_addon_tiny_jimb.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature_addon` WHERE `guid` = 88294;
+INSERT INTO `creature_addon` VALUES
+(88294, 0, 0, 1, 0, 0, '');
+UPDATE `creature` SET `position_x` = 592.57, `position_y` = -2791.61, `position_z` = 0.16 WHERE `guid` = 88294;
