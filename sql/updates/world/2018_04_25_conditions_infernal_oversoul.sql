9a88ace1caf662af219b91f7d7b5a2a221d266ac
Dootz <k.kozubek@wp.pl>
2018-04-25 20:02:31 +0200

    DB/Quest: The Summoning Chamber - prevent multiple spawns of Infernal Oversoul

---
+++ sql/updates/world/2018_04_25_conditions_infernal_oversoul.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceEntry` = 37285;
+INSERT INTO `conditions` VALUES
+(17, 0, 37285, 0, 0, 29, 0 , 21735,30, 0, 1, 0, 0, '', 'summon Infernal oversoul only if he`s not present');
