638aa4bb5a832a339f3203ab4e8445c94c1c2be1
Dootz <k.kozubek@wp.pl>
2018-04-25 19:44:57 +0200

    DB/Quest: Screecher Spirits - Yeh'kinya's Bramble should be usable only on vale screechers

---
+++ sql/updates/world/2018_04_25_conditions_screecher_spirit.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceEntry` = 12699 AND `SourceTypeOrReferenceId` = 17;
+INSERT INTO `conditions` VALUES
+(17, 0, 12699, 0, 0, 29, 0, 5307, 10, 0, 0, 0, 0, '', 'Summon Screecher Spirit should target Vale Screecher');
