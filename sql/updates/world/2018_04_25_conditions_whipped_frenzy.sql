43b7ce6ff8f750dffd5292b8056ba29171f8a61e
Dootz <k.kozubek@wp.pl>
2018-04-25 20:24:51 +0200

    DB/Spell: Whipped Frenzy should not target players

---
+++ sql/updates/world/2018_04_25_conditions_whipped_frenzy.sql
@@ -0,0 +1,4 @@
+select * from conditions where sourcetypeorreferenceid = 13 and sourceentry = 34086;
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 13 AND  `SourceEntry` = 34086;
+INSERT INTO `conditions` VALUES
+(13, 1, 34086, 0, 0, 31, 0, 4, 0, 0, 1, 0, 0, '', 'Whipped Frenzy should no target players');
