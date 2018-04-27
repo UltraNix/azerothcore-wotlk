36c3ed33532c1225569ed6b09ebadaac52fcefe4
Dootz <k.kozubek@wp.pl>
2018-04-27 15:21:30 +0200

    followup Zol'Maz Stronghol Cache - spell_script_names

--- sql/updates/world/2018_04_27_conditions_hex_cache.sql
+++ sql/updates/world/2018_04_27_conditions_hex_cache.sql
@@ -1,3 +1,7 @@
 DELETE FROM `conditions` WHERE `SourceEntry` = 52644 AND `SourceTypeOrReferenceId` = 13;
 INSERT INTO `conditions` VALUES
 (13, 2, 52644, 0, 0, 31, 0, 4, 0, 0, 0, 0, 0, '', 'ZolMaz Cache trap Should target player');
+
+DELETE FROM `spell_script_names` WHERE `spell_id` = 52644;
+INSERT INTO `spell_script_names` VALUES
+(52644, 'spell_q12709_hexed_cache');
