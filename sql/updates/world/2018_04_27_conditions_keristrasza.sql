2dc073437443ded84edfbd17bcbe4398ac038572
Dootz <k.kozubek@wp.pl>
2018-04-27 18:51:08 +0200

    DB/Quest: Saragosa's End - enable teleport down gossip option when on platform

---
+++ sql/updates/world/2018_04_27_conditions_keristrasza.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceGroup` = 9262 AND `SourceEntry` = 1 AND `ConditionTypeOrReference` = 29;
+INSERT INTO `conditions` VALUES
+(15, 9262, 1, 0, 1, 29, 0, 26265, 50, 0, 0, 0, 0, '', 'Show gossip if at Saragosa\'s platform'); 
