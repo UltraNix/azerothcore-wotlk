01c8fbbe4e29937f2f0d829960cec0bbc148b403
Dootz <k.kozubek@wp.pl>
2018-04-28 17:47:08 +0200

    DB/Quest: Ancient Elven Masonry - remove double spawns

---
+++ sql/updates/world/2018_04_28_go_elven_masonry.sql
@@ -0,0 +1,4 @@
+UPDATE `gameobject` SET `position_x` = 5418.83, `position_y` = -790.33, `position_z` = 158.49 WHERE `guid` = 59549;
+UPDATE `gameobject` SET `position_x` = 5480.07, `position_y` = -770.02, `position_z` = 151.42 WHERE `guid` = 59540;
+UPDATE `gameobject` SET `position_x` = 5444.65, `position_y` = -752.65, `position_z` = 156.29 WHERE `guid` = 59560;
+UPDATE `gameobject` SET `position_x` = 5284.57, `position_y` = -682.26, `position_z` = 165.46 WHERE `guid` = 59525;
