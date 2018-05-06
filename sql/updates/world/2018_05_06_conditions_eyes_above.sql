59e4cb12876200a6c71e3df396c5e50da6b1ad93
Dootz <k.kozubek@wp.pl>
2018-05-06 13:05:38 +0200

    DB/Quest: Eyes above - Silver Feather should target Imperial Eagle

---
+++ sql/updates/world/2018_05_06_conditions_eyes_above.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceEntry` = 49546;
+INSERT INTO `conditions` VALUES
+(17, 0, 49546, 0, 0, 31, 1, 3, 26369, 0, 0, 0, 0, '', 'Silver Feather should target Imperial Eagle');
