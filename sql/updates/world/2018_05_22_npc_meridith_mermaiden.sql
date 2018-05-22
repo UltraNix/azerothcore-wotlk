2d56072190a675b4122342ede5185ae7a970db91
Dootz <k.kozubek@wp.pl>
2018-05-22 23:41:35 +0200

    DB/NPC: Meridith Mermaiden - correct gossip

---
+++ sql/updates/world/2018_05_22_npc_meridith_mermaiden.sql
@@ -0,0 +1,2 @@
+UPDATE `conditions` SET `NegativeCondition` = 0 WHERE `SourceGroup` = 6658 AND `SourceEntry` = 7916;
+UPDATE `conditions` SET `NegativeCondition` = 1 WHERE `SourceGroup` = 6658 AND `SourceEntry` = 7917;
