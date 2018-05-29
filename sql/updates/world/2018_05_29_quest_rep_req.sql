1e1209b956d8cbf0bfe1a5984ba1207a97d01d9e
Dootz <k.kozubek@wp.pl>
2018-05-29 23:06:06 +0200

    DB/Quest: Stormpike/Frostwolf Insignia chain - add reputation requirements

---
+++ sql/updates/world/2018_05_29_quest_rep_req.sql
@@ -0,0 +1,13 @@
+-- Horde
+UPDATE `quest_template` SET `RequiredMinRepFaction` = 729, `RequiredMinRepValue`= 3000 WHERE `id` = 7163;
+UPDATE `quest_template` SET `RequiredMinRepFaction` = 729, `RequiredMinRepValue`= 6000 WHERE `id` = 7164;
+UPDATE `quest_template` SET `RequiredMinRepFaction` = 729, `RequiredMinRepValue`= 9000 WHERE `id` = 7165;
+UPDATE `quest_template` SET `RequiredMinRepFaction` = 729, `RequiredMinRepValue`= 21000 WHERE `id` = 7166;
+UPDATE `quest_template` SET `RequiredMinRepFaction` = 729, `RequiredMinRepValue`= 21999 WHERE `id` = 7167;
+
+-- Alliance
+UPDATE `quest_template` SET `RequiredMinRepFaction` = 730, `RequiredMinRepValue`= 3000 WHERE `id` = 7168;
+UPDATE `quest_template` SET `RequiredMinRepFaction` = 730, `RequiredMinRepValue`= 6000 WHERE `id` = 7169;
+UPDATE `quest_template` SET `RequiredMinRepFaction` = 730, `RequiredMinRepValue`= 9000 WHERE `id` = 7170;
+UPDATE `quest_template` SET `RequiredMinRepFaction` = 730, `RequiredMinRepValue`= 21000 WHERE `id` = 7171;
+UPDATE `quest_template` SET `RequiredMinRepFaction` = 730, `RequiredMinRepValue`= 21999 WHERE `id` = 7172;
