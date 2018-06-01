8f553d94421dcb7cdddd5b5024997eb74ce4c84f
Riztazz <felianther15@gmail.com>
2018-06-01 09:47:54 +0200

    revert b5efa492dab4800596ca435091eb876ad38f9098

--- src/server/game/Movement/Spline/MoveSplineInit.h
+++ src/server/game/Movement/Spline/MoveSplineInit.h
@@ -105,12 +105,12 @@ namespace Movement
          * @param path - array of points, shouldn't be empty
          * @param pointId - Id of fisrt point of the path. Example: when third path point will be done it will notify that pointId + 3 done
          */
-        void MovebyPath(const PointsArray& path, int32 pointId = 0, bool updateDestZ = false );
+        void MovebyPath(const PointsArray& path, int32 pointId = 0);
 
         /* Initializes simple A to B motion, A is current unit's position, B is destination
          */
-        void MoveTo(const Vector3& destination, bool generatePath = false, bool forceDestination = false, bool updateDestZ = false );
-        void MoveTo(float x, float y, float z, bool generatePath = false, bool forceDestination = false, bool updateDestZ = false );
+        void MoveTo(const Vector3& destination, bool generatePath = false, bool forceDestination = false);
+        void MoveTo(float x, float y, float z, bool generatePath = false, bool forceDestination = false);
 
         /* Sets Id of fisrt point of the path. When N-th path point will be done ILisener will notify that pointId + N done
          * Needed for waypoint movement where path splitten into parts
