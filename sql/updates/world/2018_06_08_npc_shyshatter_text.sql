2b0080e756e5edbe5c82eb2ae1c29153847998f1
Dootz <k.kozubek@wp.pl>
2018-06-08 21:43:43 +0200

    DB/Quest: Dragonmaw Race: Captain Skyshatter - correct npc texts

---
+++ sql/updates/world/2018_06_08_npc_shyshatter_text.sql
@@ -0,0 +1,5 @@
+DELETE FROM `creature_text` WHERE `entry` = 23348;
+INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `TextRange`, `comment`) VALUES 
+(23348, 0, 0, 'I weep for you, $N. You really have no idea what you\'ve gotten yourself into...', 12, 0, 100, 0, 0, 0, 0, 'Captain Skyshatter'),
+(23348, 1, 0, 'Prepare a funeral pyre! $N has challenged Skyshatter!', 14, 0, 100, 0, 0, 0, 0, 'Captain Skyshatter'),
+(23348, 2, 0, 'I... I am undone... The new top orc is $N!', 14, 0, 100, 0, 0, 0, 0, 'Captain Skyshatter');
