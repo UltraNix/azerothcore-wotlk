c4fc14f317cd21d7787a47c31b9914aea7045883
Dootz <k.kozubek@wp.pl>
2018-06-10 01:06:30 +0200

    DB/Quest: Healing with Roses & Jinxing the Walls - drop up to 2 quest items for faction that controls WG

---
+++ sql/updates/world/2018_06_09_loot_wg_quest.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -100, `maxcount` = 2 WHERE `entry` = 34300 AND `item` IN (43324,44809);
+
+UPDATE `creature_loot_template` SET `maxcount` = 2 WHERE `entry` = 30877 AND `item` IN (43322,44808);
