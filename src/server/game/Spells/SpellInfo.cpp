e4123206a5dc6d1060133176c61b9be0f1bd0cc9
Dootz <k.kozubek@wp.pl>
2018-06-12 23:10:09 +0200

    [Ulduar]Core/Spell: Saronite Vapors - should be a debuff

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -2587,6 +2587,7 @@ bool SpellInfo::_IsPositiveEffect(uint8 effIndex, bool deep) const
                 case 61987: // Avenging Wrath Marker
                 case 61988: // Divine Shield exclude aura
                 case 72998: // Shadow Prison (Blood Prince Council, ICC Encounter)
+                case 63322: // Saronite Vapors (General Vezax, Ulduar)
                     return false;
                 case 30877: // Tag Murloc
                 case 61716: // Rabbit Costume
