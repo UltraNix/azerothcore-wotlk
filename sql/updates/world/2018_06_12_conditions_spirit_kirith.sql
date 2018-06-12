9ad84322d7e6596ec3bd55b1c1c25afbac99b891
Dootz <k.kozubek@wp.pl>
2018-06-12 18:55:15 +0200

    DB/NPC: Spirit of Kirith - prevent multiple spawns

---
+++ sql/updates/world/2018_06_12_conditions_spirit_kirith.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceEntry` = 10853;
+INSERT INTO `conditions` VALUES
+(17, 0, 10853, 0, 0, 29, 0, 7729, 50, 0, 1, 0, 0, '', 'Cant summon Spirit of Kirith within 50 yards of Spirit of Kirith');
