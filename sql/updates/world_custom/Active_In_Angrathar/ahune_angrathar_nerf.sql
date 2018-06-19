431e75839bbc774b94d8eea9cc8a4d63bf12cfc2
Dootz <k.kozubek@wp.pl>
2018-06-19 21:23:16 +0200

    [Angrathar]DB/Item: Nerf Ahune loot

---
+++ sql/updates/world_custom/Active_In_Angrathar/ahune_angrathar_nerf.sql
@@ -0,0 +1,6 @@
+UPDATE `item_loot_template` SET `item` = 35514 WHERE `item` = 54806;
+UPDATE `gameobject_loot_template` SET `item` = 35496 WHERE `item` = 54801;
+UPDATE `gameobject_loot_template` SET `item` = 35495 WHERE `item` = 54802;
+UPDATE `gameobject_loot_template` SET `item` = 35494 WHERE `item` = 54804;
+UPDATE `gameobject_loot_template` SET `item` = 35497 WHERE `item` = 54805;
+DELETE FROM `gameobject_loot_template` WHERE `item` = 54803;
