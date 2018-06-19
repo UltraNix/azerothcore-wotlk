67596c48a2bd7d8b3df3fc090ff93f5033502eef
Dootz <k.kozubek@wp.pl>
2018-06-20 00:58:25 +0200

    DB/NPC: Zulfarrak rare boss - spawn adjust spawn chance

---
+++ sql/updates/world/2018_06_20_npc_zulfarrak.sql
@@ -0,0 +1,8 @@
+DELETE FROM `pool_creature` WHERE `pool_entry` IN (1784,1785,1786);
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES 
+(138257, 1786, 30, 'Zulfarrak rare boss'),
+(42615, 1786, 70, 'Zulfarrak rare boss'),
+(38014, 1784, 70, 'Zulfarrak rare boss'),
+(138256, 1785, 30, 'Zulfarrak rare boss'),
+(38018, 1785, 70, 'Zulfarrak rare boss'),
+(138255, 1784, 30, 'Zulfarrak rare boss');
