d540b25b212fd6472528eab69e50b7e8afcac813
Dootz <k.kozubek@wp.pl>
2018-06-20 23:08:51 +0200

    DB/NPC: Hive ambusher - lower skinning drop chance

---
+++ sql/updates/world/2018_06_20_npc_hive_ambusher.sql
@@ -0,0 +1,3 @@
+UPDATE `skinning_loot_template` SET `ChanceOrQuestChance` = 10 WHERE `entry` = 13301 AND `item` = 20498;
+UPDATE `skinning_loot_template` SET `ChanceOrQuestChance` = 20 WHERE `entry` = 13301 AND `item` = 20499;
+UPDATE `skinning_loot_template` SET `ChanceOrQuestChance` = 0.5 WHERE `entry` = 13301 AND `item` = 20500;
