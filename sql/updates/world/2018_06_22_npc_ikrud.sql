0de04ec939884e87acf2fe8368e4c39e345e61dd
Dootz <k.kozubek@wp.pl>
2018-06-22 12:57:06 +0200

    DB/NPC: Iikrud Magthrull - update AI, nerf succubus dmg

---
+++ sql/updates/world/2018_06_22_npc_ikrud.sql
@@ -0,0 +1,4 @@
+UPDATE `creature_template` SET `dmg_multiplier` = 0.8, `mindmg` = 30, `maxdmg` = 45, `attackpower` = 40 WHERE `entry` = 10928;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 3664 AND `id` = 3;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+(3664, 0, 3, 0, 2, 0, 100, 0, 0, 50, 66500, 76300, 11, 6487, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Ilkrud Magthrull - Between 0-50% Health - Cast \'Ilkrud\'s Guardians\' (No Repeat)');
