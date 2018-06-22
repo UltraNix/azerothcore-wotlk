1954c8da1d0016d221e5e661368b29971188d320
Dootz <k.kozubek@wp.pl>
2018-06-22 15:36:06 +0200

    DB/Quest: Remove deprecated AB quests

---
+++ sql/updates/world/2018_06_22_quest_ab.sql
@@ -0,0 +1,13 @@
+DELETE FROM `disables` WHERE `entry` IN 
+(8155, 8156, 8154, 8080, 8297, 8162, 8161, 8160, 8123, 8299);
+INSERT INTO `disables` VALUES
+(1, 8155, 0, '', '', 'Deprecated quest Arathi Basin'),
+(1, 8156, 0, '', '', 'Deprecated quest Arathi Basin'),
+(1, 8154, 0, '', '', 'Deprecated quest Arathi Basin'),
+(1, 8080, 0, '', '', 'Deprecated quest Arathi Basin'),
+(1, 8297, 0, '', '', 'Deprecated quest Arathi Basin'),
+(1, 8162, 0, '', '', 'Deprecated quest Arathi Basin'),
+(1, 8161, 0, '', '', 'Deprecated quest Arathi Basin'),
+(1, 8160, 0, '', '', 'Deprecated quest Arathi Basin'),
+(1, 8123, 0, '', '', 'Deprecated quest Arathi Basin'),
+(1, 8299, 0, '', '', 'Deprecated quest Arathi Basin');
