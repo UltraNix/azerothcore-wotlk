91eaeb22b0bc842d469664cc46060034efbeaee3
Dootz <k.kozubek@wp.pl>
2018-06-25 20:20:56 +0200

    follow up Patroll, Congratulations

--- sql/updates/world/2018_06_21_quest_patroll.sql
+++ sql/updates/world/2018_06_21_quest_patroll.sql
@@ -3,8 +3,7 @@ UPDATE `creature_template` SET `AIName` = "SmartAI" WHERE `entry` = 28039;
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 28039;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 (28039, 0, 0, 1, 19, 0, 100, 0, 12596, 0, 0, 0, 85, 51506, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Commander Kunz - on quest accept Patrol - reset quest'),
-(28039, 0, 1, 2, 61, 0, 100, 0, 0, 0, 0, 0, 85, 51509, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Commander Kunz - on quest accept Patrol - reset quest'),
-(28039, 0, 2, 0, 61, 0, 100, 0, 0, 0, 0, 0, 11, 51573, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Commander Kunz - on quest accept Patrol - cast On Patrol'),
+(28039, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 85, 51509, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Commander Kunz - on quest accept Patrol - reset quest'),
 (28039, 0, 4, 5, 19, 0, 100, 0, 12501, 0, 0, 0, 85, 53712, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Commander Kunz - on quest accept Troll Patrol - reset quest'),
 (28039, 0, 5, 6, 61, 0, 100, 0, 0, 0, 0, 0, 85, 53713, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Commander Kunz - on quest accept Troll Patrol - reset quest'),
 (28039, 0, 6, 7, 61, 0, 100, 0, 0, 0, 0, 0, 85, 53715, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Commander Kunz - on quest accept Troll Patrol - reset quest'),
