03e021ed504e66ff92aacbd194f8e5ac1d47b618
Dootz <k.kozubek@wp.pl>
2018-06-26 17:40:51 +0200

    DB/Go: Add missing Bonfire focus in Shattrath

---
+++ sql/updates/world/2018_06_26_go_flame.sql
@@ -0,0 +1,3 @@
+DELETE FROM `gameobject` WHERE `guid` = 200991;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `VerifiedBuild`) VALUES 
+(200991, 181371, 530, 1, 1, -1747.85, 5326.29, -12.4281, 4.40434, 0, 0, 0.807217, -0.590254, 180, 100, 1, 0);
