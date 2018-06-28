2c3375d355e60c832e6d4eb63a43d07ff115b399
Dootz <k.kozubek@wp.pl>
2018-06-28 22:22:51 +0200

    DB/NPC: Festival Loremaster - add missing spawn

---
+++ sql/updates/world/2018_06_26_npc_festival_loremaster.sql
@@ -0,0 +1,3 @@
+DELETE FROM `creature` WHERE `guid` = 249571;
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES
+(249571, 16817, 0, 1, 1, 0, 0, -8831.73, 854.756, 99.1285, 5.71407, 300, 0, 0, 3052, 0, 0, 0, 0, 0);
