daaddd1a57b99dd5d675e5426c6a40bece6253f5
Dootz <k.kozubek@wp.pl>
2018-07-04 22:04:07 +0200

    DB/Item: Design: Defender's Twilight Opal - adjust drop rate

---
+++ sql/updates/world/2018_07_04_loot_design_jc.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 1.8 WHERE `entry` = 29370 AND `item` = 41820;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 0.8 WHERE `entry` = 29376 AND `item` = 41820;
