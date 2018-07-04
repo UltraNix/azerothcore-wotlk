ab106459db0118bc13bad325691634b428c09bed
Dootz <k.kozubek@wp.pl>
2018-07-04 23:05:25 +0200

    Instance/Temple of Ahn'Qiraj: Prevent multiple Ouro spawns

---
+++ sql/updates/world/2018_07_04_conditions_ouro.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceEntry` = 26061 AND `SourceTypeOrReferenceId` = 17;
+INSERT INTO `conditions` VALUES
+(17, 0, 26061, 0, 0, 29, 0, 15517, 400, 0, 1, 0, 0, '', 'Summon Ouro can be used if Ouro not summoned');
