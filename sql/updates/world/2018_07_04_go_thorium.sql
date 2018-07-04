2867d2788f47dccfcd85e0dd922016d47814a997
Dootz <k.kozubek@wp.pl>
2018-07-04 22:02:40 +0200

    DB/GO: Increase Thorium spawn rate

---
+++ sql/updates/world/2018_07_04_go_thorium.sql
@@ -0,0 +1,3 @@
+UPDATE `pool_template` SET `max_limit` = 25 WHERE `entry` = 1708;
+UPDATE `pool_template` SET `max_limit` = 45 WHERE `entry` = 1709;
+UPDATE `pool_template` SET `max_limit` = 35 WHERE `entry` = 9916;
