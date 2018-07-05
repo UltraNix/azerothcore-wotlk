def0e504321d9fd98a729d1e5e6b11760800cb7f
Dootz <k.kozubek@wp.pl>
2018-07-05 21:19:15 +0200

    DB/NPC: Honor Hold Archer - AI added

---
+++ sql/updates/world/2018_07_05_npc_honor_hold_archer.sql
@@ -0,0 +1,3 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 16896;
+INSERT INTO `smart_scripts` VALUES
+(16896, 0, 0, 0, 0, 0, 100, 0, 1000, 1000, 8000, 12000, 11, 29120, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Honor Hold Archer - In Combat - Cast Shoot Bow');
