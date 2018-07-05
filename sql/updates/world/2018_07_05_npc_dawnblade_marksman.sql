d28162a3f4830a9f29a5d944a63d22a5116e2c4a
Dootz <k.kozubek@wp.pl>
2018-07-05 21:20:14 +0200

    DB/NPC: Dawnblade Marksman - AI added

---
+++ sql/updates/world/2018_07_05_npc_dawnblade_marksman.sql
@@ -0,0 +1,4 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 24979 AND `id` IN (2,3);
+INSERT INTO `smart_scripts` VALUES
+(24979, 0, 2, 0, 0, 0, 100, 0, 1000, 3000, 8000, 12000, 11, 37847, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Dawnblade Marksman - In Combat - Cast Immolation Arrow'),
+(24979, 0, 3, 0, 0, 0, 100, 0, 3000, 7000, 15000, 20000, 11, 35935, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Dawnblade Marksman - In Combat - Cast Immolation');
