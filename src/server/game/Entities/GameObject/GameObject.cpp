b6e9e5ba5a69cee6c8bc23b05a4343bc30f5ca51
Afgann <Marcin60000@gmail.com>
2018-07-06 18:02:51 +0200

    Revert "Trap target selector correction."
    This reverts commit 6aeff2cf17434f836fc7a459c9f4adb714cfdf3c.

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -582,13 +582,11 @@ void GameObject::Update(uint32 diff)
                         if (owner->IsInSanctuary() && owner->GetTypeId() == TYPEID_PLAYER && !owner->ToPlayer()->duel)
                             return;
 
-                        float radius_temp = radius * 1.8f;
-
-                        Trinity::NearestUnfriendlyNoTotemUnitInObjectRangeCheck checker(this, owner, radius_temp);
-                        Trinity::UnitLastSearcher<Trinity::NearestUnfriendlyNoTotemUnitInObjectRangeCheck> searcher(this, target, checker);
-                        VisitNearbyGridObject(radius_temp, searcher);
+                        Trinity::AnyUnfriendlyNoTotemUnitInObjectRangeCheck checker(this, owner, radius);
+                        Trinity::UnitSearcher<Trinity::AnyUnfriendlyNoTotemUnitInObjectRangeCheck> searcher(this, target, checker);
+                        VisitNearbyGridObject(radius, searcher);
                         if (!target)
-                            VisitNearbyWorldObject(radius_temp, searcher);
+                            VisitNearbyWorldObject(radius, searcher);
                     }
                     else                                        // environmental trap
                     {
