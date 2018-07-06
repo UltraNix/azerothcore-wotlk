050f32cf24e6e6f87cfe24bf135ce97458bf54d2
Dootz <k.kozubek@wp.pl>
2018-07-06 22:47:23 +0200

    DB/Item: Essence of Infinity - drop chance adjusted

---
+++ sql/updates/world/2018_07_06_loot_essence_of_infinity.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -51 WHERE `item` = 31753 AND `entry` = 17839;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -65 WHERE `item` = 31753 AND `entry` = 21104;
