617b4475b60d2d47946cef33a4f43a6a7d9a22bb
Riztazz <felianther15@gmail.com>
2018-07-10 08:50:53 +0200

    Fix sql 2018_07_09_spell_script_names.sql

--- sql/updates/world/2018_07_09_spell_script_names.sql
+++ sql/updates/world/2018_07_09_spell_script_names.sql
@@ -1,3 +1,3 @@
-DELETE FROM `spell_script_names` `ScriptName`='spell_sha_elemental_devastation';
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_sha_elemental_devastation';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
 (-30160, 'spell_sha_elemental_devastation');
