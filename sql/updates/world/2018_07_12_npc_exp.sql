08ec19d9358abe146426a0251fe754e46a18df5f
Dootz <k.kozubek@wp.pl>
2018-07-12 23:44:51 +0200

    DB/NPC: Fix exploit in Karazhan questline

---
+++ sql/updates/world/2018_07_12_npc_exp.sql
@@ -0,0 +1,6 @@
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 64 WHERE `entry` = 22890;
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 0 WHERE `creature_id` = 22890;
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 64 WHERE `entry` = 22891;
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 0 WHERE `creature_id` = 22891;
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 64 WHERE `entry` = 22892;
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 0 WHERE `creature_id` = 22892;
