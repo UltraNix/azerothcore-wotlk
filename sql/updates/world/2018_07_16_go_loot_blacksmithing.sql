01aa2033c195fec2ac45f198d2690ed9182c694a
Dootz <k.kozubek@wp.pl>
2018-07-16 12:02:30 +0200

    DB/Gameobject: Blacksmithing plans - correct loot table

---
+++ sql/updates/world/2018_07_16_go_loot_blacksmithing.sql
@@ -0,0 +1,6 @@
+DELETE FROM `gameobject_loot_template` WHERE `entry` = 11524;
+INSERT INTO `gameobject_loot_template` VALUES
+(11524, 12830, 30, 1, 0, 1, 1),
+(11524, 12827, 23, 1, 0, 1, 1),
+(11524, 11615, 9,  1, 0, 1, 1),
+(11524, 11614, 38, 1, 0, 1, 1);
