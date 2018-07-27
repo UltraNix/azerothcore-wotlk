9bdc4b835821f25b2ffb2d4ab6e009b546735955
Dootz <dootz.sunwell@gmail.com>
2018-07-28 00:43:36 +0200

    DB/NPC: Stone watcher - add texts TC

---
+++ sql/updates/world/2018_07_28_text_stone_watcher.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature_text` WHERE `entry`=7918;
+INSERT INTO `creature_text` (`entry`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `TextRange`, `comment`) VALUES
+(7918,0,0,"Salutations!  I am a guardian of entry.  Please use the proper discs for access to Uldum.",12,0,100,0,0,0,0,"Stone Watcher of Norgannon"),
+(7918,1,0,"Your discs will be returned to you once you access the pedestal.",12,0,100,0,0,0,0,"Stone Watcher of Norgannon");
