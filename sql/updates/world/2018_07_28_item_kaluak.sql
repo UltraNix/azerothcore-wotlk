6fb7fcf3aacbb344a8a1faca9928c02f7d59602c
Dootz <dootz.sunwell@gmail.com>
2018-07-28 23:47:33 +0200

    DB/Item: Mastercraft Kalu'ak Fishing Pole  will grant Bonus Feral Attack Power

---
+++ sql/updates/world/2018_07_28_item_kaluak.sql
@@ -0,0 +1,5 @@
+UPDATE `item_template` SET
+`spellid_1` = 59731, `spelltrigger_1` = 1, `spellcooldown_1` = -1, 
+`spellid_1` = 11789, `spelltrigger_1` = 1, `spellcooldown_1` = -1, 
+`spellid_1` = 49585, `spelltrigger_1` = 1, `spellcooldown_1` = -1
+WHERE `entry` = 44050;
