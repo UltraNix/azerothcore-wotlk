5575cfc25ca8dc9678b2046e45298e367ae63da8
Dootz <dootz.sunwell@gmail.com>
2018-08-04 18:29:44 +0200

    DB/Quest: Heart of the mountain - spawn gameobject

---
+++ sql/updates/world/2018_08_04_go_heart_mountain.sql
@@ -0,0 +1,3 @@
+DELETE FROM `gameobject` WHERE `guid` = 5625;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES 
+(5625, 165554, 230, 1, 1, 802.907, -356.401, -48.9423, 0, 0, 0, 0, 1, 7200, 100, 1);
