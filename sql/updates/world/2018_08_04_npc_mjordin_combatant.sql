f9c24b948267b11b556b553ff2aebd4ee0dd6e4f
Dootz <dootz.sunwell@gmail.com>
2018-08-04 19:31:34 +0200

    DB/NPC: Mjordin combatant - add combat ai

---
+++ sql/updates/world/2018_08_04_npc_mjordin_combatant.sql
@@ -0,0 +1,6 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 30037 AND `id` IN (3,4,5,6);
+INSERT INTO `smart_scripts` VALUES
+(30037, 0, 3, 0, 4, 0, 50, 0, 0, 0, 0, 0, 11, 61227, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Mjordin Combatant- In Combat - Cast Jump Attack'),
+(30037, 0, 4, 0, 0, 0, 100, 0, 5000, 10000, 10000, 15000, 11, 32736, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Mjordin Combatant- In Combat - Cast Mortal Strike'),
+(30037, 0, 5, 0, 0, 0, 100, 0, 10000, 15000, 15000, 25000, 11, 50370, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Mjordin Combatant- In Combat - Cast Sunder Armor'),
+(30037, 0, 6, 0, 0, 0, 100, 0, 20000, 25000, 30000, 35000, 11, 49807, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Mjordin Combatant- In Combat - Cast Whirlwind');
