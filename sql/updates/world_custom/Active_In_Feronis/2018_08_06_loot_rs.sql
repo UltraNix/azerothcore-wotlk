e3a21dd20634003519b0f2660660c7b539dba903
Dootz <dootz.sunwell@gmail.com>
2018-08-06 20:41:37 +0200

    [Feronis] RS mini boss emblems

--- sql/updates/world_custom/Active_In_Feronis/2018_08_06_loot_rs.sql
+++ sql/updates/world_custom/Active_In_Feronis/2018_08_06_loot_rs.sql
@@ -1,1 +1,2 @@
 UPDATE `creature_loot_template` SET `item` = 49426 WHERE `item` = 47241 AND `entry` IN (39863, 39864)
+UPDATE `creature_loot_template` SET `item` = 49426 WHERE `item` = 47241 AND `entry` IN (39946, 39948, 39947);
