361a4a6a55716c888727f9c1451a1e39dc75bfdc
Dootz <dootz.sunwell@gmail.com>
2018-08-09 00:43:34 +0200

    DB/NPC: Scourge Deathspeaker - add AI

---
+++ sql/updates/world/2018_08_08_npc_scourge_deathspeaker.sql
@@ -0,0 +1,3 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 27615 AND `id` = 4;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(27615, 0, 4, 0, 0, 1, 100, 0, 3000, 3000, 3400, 4800, 11, 52282, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Scourge Deathspeaker - In Combat- Cast \'Fireball\' (No Repeat)');
