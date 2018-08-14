a205af20a41c541a7092bc79283cd4a2bf38af7d
Dootz <dootz.sunwell@gmail.com>
2018-08-14 18:42:04 +0200

    DB/NPC: Anubar cultist - add missing spawns

---
+++ sql/updates/world/2018_08_14_npc_anubar_cultist.sql
@@ -0,0 +1,6 @@
+DELETE FROM `creature` WHERE `guid` BETWEEN 154225 AND 154228;
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES 
+(154225, 26319, 571, 1, 1, 23973, 0, 3849.70, 2181.38, 124.34, 3.56, 300, 5, 0, 7432, 7332, 1, 0, 0, 0),
+(154226, 26319, 571, 1, 1, 23973, 0, 3845.77, 2214.99, 124.19, 3.24, 300, 5, 0, 7432, 7332, 1, 0, 0, 0),
+(154227, 26319, 571, 1, 1, 23973, 0, 3848.37, 2271.59, 123.57, 2.74, 300, 5, 0, 7432, 7332, 1, 0, 0, 0),
+(154228, 26319, 571, 1, 1, 23973, 0, 3865.96, 2304.56, 123.56, 300, 5, 0, 7432, 7332, 1, 0, 0, 0);
