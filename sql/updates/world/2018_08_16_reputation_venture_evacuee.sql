9f4dd18020e23c5e508851d80cbdbc8094287735
Dootz <dootz.sunwell@gmail.com>
2018-08-16 21:17:38 +0200

    DB/NPC: Venture co Evacuee - add onkill reputation

---
+++ sql/updates/world/2018_08_16_reputation_venture_evacuee.sql
@@ -0,0 +1,3 @@
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` = 27830;
+INSERT INTO `creature_onkill_reputation` (`creature_id`, `RewOnKillRepFaction1`, `RewOnKillRepFaction2`, `MaxStanding1`, `IsTeamAward1`, `RewOnKillRepValue1`, `MaxStanding2`, `IsTeamAward2`, `RewOnKillRepValue2`, `TeamDependent`) VALUES 
+(27830, 21, 0, 5, 0, 25, 0, 0, 0, 0);
