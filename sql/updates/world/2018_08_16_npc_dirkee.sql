5cf1bc5b72c40a73992b5078cedb31ac96a5f2b2
Dootz <dootz.sunwell@gmail.com>
2018-08-16 21:17:20 +0200

    DB/NPC: Dirkee - visible in all phases

---
+++ sql/updates/world/2018_08_16_npc_dirkee.sql
@@ -0,0 +1,3 @@
+UPDATE `creature` SET `phaseMask` = 7 WHERE `guid` = 152011;
+UPDATE `creature` SET `phaseMask` = 3 WHERE `guid` = 152012;
+UPDATE `creature` SET `phaseMask` = 5 WHERE `guid` = 152013;
