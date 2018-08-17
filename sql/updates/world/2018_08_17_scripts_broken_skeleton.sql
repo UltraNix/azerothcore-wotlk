6fda67b82b544bbab277990258834e6922aec657
Dootz <dootz.sunwell@gmail.com>
2018-08-17 22:07:24 +0200

    DB/NPC: Broken Skeleton - will have chance to spawn Crippler on death

---
+++ sql/updates/world/2018_08_17_scripts_broken_skeleton.sql
@@ -0,0 +1,3 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 16805 AND `id` = 1;
+INSERT INTO `smart_scripts` VALUES
+(16805, 0, 1, 0, 6, 0, 5, 1, 0, 0, 0, 0, 12, 18689, 6, 60000, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Broken Skeleton - On Death - Summon Crippler');
