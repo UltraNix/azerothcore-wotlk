370c8e6ea8617d70b622e3e9b887eec0f9d54f41
Dootz <dootz.sunwell@gmail.com>
2018-08-23 03:00:38 +0200

    DB/Quest: Intercept the Reinforcements & Keeping the Enemy at Bay - dragonhawk can be used if you have quest taken/completed

---
+++ sql/updates/world/2018_08_23_conditions_dragonhawk.sql
@@ -0,0 +1,4 @@
+DELETE FROM `conditions` WHERE `SourceGroup` = 9143;
+INSERT INTO `conditions` VALUES 
+(15, 9143, 0, 0, 0, 8, 0, 11542, 0, 0, 1, 0, 0, '', 'Show gossip option if player has quest 11542 but not rewarded'),
+(15, 9143, 0, 0, 1, 8, 0, 11543, 0, 0, 1, 0, 0, '', 'Show gossip option if player has quest 11543 but not rewarded');
