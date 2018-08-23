3e950cac93a2e216eee51069a0f54ef8879c3e1e
Dootz <dootz.sunwell@gmail.com>
2018-08-23 03:01:18 +0200

    DB/Quest: Banish the Demons & Banish More Demons - fix double quest credit

---
+++ sql/updates/world/2018_08_23_scripts_banishing_demons.sql
@@ -0,0 +1,18 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 19973 AND `id` = 1;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 20557 AND `id` = 1;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 22195 AND `id` = 1;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 22201 AND `id` = 1;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 22204 AND `id` = 1;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 22291 AND `id` = 1;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 22327 AND `id` = 1;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 22392 AND `id` = 1;
+
+-- If you need to restore it for some reason, you're welcome
+-- INSERT INTO `smart_scripts` VALUES (19973, 0, 1, 0, 6, 1, 100, 0, 0, 0, 0, 0, 33, 23327, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 'Abyssal Flamebringer - On Just Died - Quest Credit \'Banish the Demons\' (Phase 1) (No Repeat)');
+-- INSERT INTO `smart_scripts` VALUES (20557, 0, 1, 0, 6, 1, 100, 0, 0, 0, 0, 0, 33, 23327, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 'Wrath Hound - On Just Died - Quest Credit \'Banish the Demons\'');
+-- INSERT INTO `smart_scripts` VALUES (22195, 0, 1, 0, 6, 1, 100, 0, 0, 0, 0, 0, 33, 23327, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 'Wrath Speaker - On Just Died - Quest Credit \'Banish the Demons\'');
+-- INSERT INTO `smart_scripts` VALUES (22201, 0, 1, 0, 6, 1, 100, 0, 0, 0, 0, 0, 33, 23327, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 'Fear Whisperer - On Just Died - Quest Credit \'Banish the Demons\'');
+-- INSERT INTO `smart_scripts` VALUES (22204, 0, 1, 0, 6, 1, 100, 0, 0, 0, 0, 0, 33, 23327, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 'Fear Fiend - On Just Died - Quest Credit \'Banish the Demons\'');
+-- INSERT INTO `smart_scripts` VALUES (22291, 0, 1, 0, 6, 1, 100, 0, 0, 0, 0, 0, 33, 23327, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 'Furnace Guard - On Just Died - Quest Credit \'Banish the Demons\'');
+-- INSERT INTO `smart_scripts` VALUES (22327, 0, 1, 0, 6, 1, 100, 0, 0, 0, 0, 0, 33, 23327, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 'Terror-Fire Guardian - On Just Died - Quest Credit \'Banish the Demons\'');
+-- INSERT INTO `smart_scripts` VALUES (22392, 0, 1, 0, 6, 1, 100, 0, 0, 0, 0, 0, 33, 23327, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 'Wrath Fiend - On Just Died - Quest Credit \'Banish the Demons\'');
