6ad9881050c5ac48cec05b0f091f806f00fa985a
Dootz <dootz.sunwell@gmail.com>
2018-08-26 17:25:42 +0200

    DB/NPC: Quel'Danas quest chaining

---
+++ sql/updates/world/2018_08_26_quest_template_queldanas_daily.sql
@@ -0,0 +1,8 @@
+UPDATE `quest_template` SET `PrevQuestId` = 11520 WHERE `id` = 11521;
+UPDATE `quest_template` SET `PrevQuestId` = 11545 WHERE `id` = 11548;
+UPDATE `quest_template` SET `PrevQuestId` = 11538 WHERE `id` = 11537;
+UPDATE `quest_template` SET `PrevQuestId` = 11532 WHERE `id` = 11533;
+UPDATE `quest_template` SET `PrevQuestId` = 11542 WHERE `id` = 11543;
+UPDATE `quest_template` SET `PrevQuestId` = 11539 WHERE `id` = 11540;
+UPDATE `quest_template` SET `PrevQuestId` = 11535 WHERE `id` = 11536;
+UPDATE `quest_template` SET `PrevQuestId` = 11520 WHERE `id` = 11521;
