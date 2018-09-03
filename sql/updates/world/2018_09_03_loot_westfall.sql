8796f8730c76edc83280381e9d4d83dbb20f9448
Dootz <dootz.sunwell@gmail.com>
2018-09-03 13:28:13 +0200

    DB/NPC: Westfall Furlbrows Pumpkin Farm area - reduce drop chance of green items

---
+++ sql/updates/world/2018_09_03_loot_westfall.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 5 WHERE `entry` = 114 AND `item` = 24100;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 4 WHERE `entry` = 114 AND `item` = 24103;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 6 WHERE `entry` = 114 AND `item` = 44008;
