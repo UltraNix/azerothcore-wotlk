afc2e7bdb530003f9da9a8a3cd962f9ab3ab5830
Dootz <dootz.sunwell@gmail.com>
2018-09-16 23:35:11 +0200

    DB/NPC: Necrolord - add AI

---
+++ sql/updates/world/2018_09_16_scripts_necrolord.sql
@@ -0,0 +1,5 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 24014 AND `id` IN (1,2,3);
+INSERT INTO `smart_scripts` VALUES
+(24014, 0, 1, 0, 0, 0, 100, 0, 3000, 6000, 5000, 8000, 11, 9613, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Necrolord - IC - Cast Shadow Bolt'),
+(24014, 0, 2, 0, 0, 0, 100, 0, 10000, 16000, 15000, 18000, 11, 17173, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Necrolord - IC - Cast Drain Life'),
+(24014, 0, 3, 0, 0, 0, 100, 0, 3000, 6000, 5000, 8000, 11, 18267, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 'Necrolord - IC - Cast Curse of Weakness');
