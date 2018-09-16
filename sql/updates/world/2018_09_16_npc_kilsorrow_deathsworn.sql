cebd102cf3634c5206b14e590abc8e23bfc5a611
Dootz <dootz.sunwell@gmail.com>
2018-09-16 23:30:54 +0200

    DB/NPC: Kil'sorrow Deathsworn's at camp gate - fix weird combat behavior

---
+++ sql/updates/world/2018_09_16_npc_kilsorrow_deathsworn.sql
@@ -0,0 +1,2 @@
+UPDATE `creature` SET `position_x` = -2769.22, `position_y` = 6433.90, `position_z` = 57.83, `orientation` = 6.12 WHERE `guid` = 60237;
+UPDATE `creature` SET `position_x` = -2770.88, `position_y` = 6424.93, `position_z` = 58.07, `orientation` = 6.11 WHERE `guid` = 60236;
