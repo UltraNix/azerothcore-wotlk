4e17f22577e220b7ab9f7be9a9af68fdd23422de
Dootz <dootz.sunwell@gmail.com>
2018-09-16 23:35:29 +0200

    DB/NPC: Necro Overlord - add AI

---
+++ sql/updates/world/2018_09_16_scripts_necro_overlord.sql
@@ -0,0 +1,3 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 24018 AND `id` IN (1);
+INSERT INTO `smart_scripts` VALUES
+(24018, 0, 1, 0, 4, 0, 100, 1, 0, 0, 0, 0, 11, 43559, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Necro Overlord Mezhen - IC - Cast Raise Dead');
