4088f768b6d8785adce11ce0b67cd7cc002a01f8
Dootz <dootz.sunwell@gmail.com>
2018-09-19 19:23:47 +0200

    DB/NPC: Brewfest

---
+++ sql/updates/world/2018_09_19_brewfest.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `faction` = 736 WHERE `entry` = 23795;
+DELETE FROM `npc_vendor` WHERE `entry` = 24495 AND `item` = 37737 AND `ExtendedCost` = 2397;
+DELETE FROM `npc_vendor` WHERE `entry` = 24495 AND `item` = 37599;
