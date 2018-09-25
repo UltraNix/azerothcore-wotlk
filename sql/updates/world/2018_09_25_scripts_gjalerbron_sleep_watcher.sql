4eb50071a565c727e56aa34742162e22f3cc94e9
Dootz <dootz.sunwell@gmail.com>
2018-09-25 15:23:34 +0200

    DB/NPC: Gjalerbron Sleep Watcher - add AI

---
+++ sql/updates/world/2018_09_25_scripts_gjalerbron_sleep_watcher.sql
@@ -0,0 +1,3 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 23989 AND `id` = 1;
+INSERT INTO `smart_scripts` VALUES
+(23989, 0, 1, 0, 0, 0, 100, 0, 10000, 12000, 10000, 15000, 11, 66290, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Combat - Cast Sleep');
