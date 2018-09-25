89afa6e8b7e3bcac4db84a77c58d965b6dd7e1e6
Dootz <dootz.sunwell@gmail.com>
2018-09-25 15:23:47 +0200

    DB/NPC: Westguard Sergeant - add AI

---
+++ sql/updates/world/2018_09_25_scripts_westguard_sergeant.sql
@@ -0,0 +1,3 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 24060;
+INSERT INTO `smart_scripts` VALUES
+(24060, 0, 0, 0, 13, 0, 100, 0, 4500, 14000, 0, 0, 11, 38233, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Target Casting - Cast \'Shield Bash\'');
