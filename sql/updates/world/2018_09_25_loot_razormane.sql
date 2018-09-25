43c95e53d32ff7fd0d56ff5ba63457bd36459f21
Dootz <dootz.sunwell@gmail.com>
2018-09-25 12:03:09 +0200

    DB/NPC: Razormane mobs - reduce drop chance of grey items

---
+++ sql/updates/world/2018_09_25_loot_razormane.sql
@@ -0,0 +1,9 @@
+DELETE FROM `creature_loot_template` WHERE `entry` = 3268 AND `item` = 7350;
+DELETE FROM `creature_loot_template` WHERE `entry` = 3267 AND `item` = 6517;
+DELETE FROM `creature_loot_template` WHERE `entry` = 3268 AND `item` = 6514;
+DELETE FROM `creature_loot_template` WHERE `entry` = 3268 AND `item` = 2963;
+INSERT INTO `creature_loot_template` VALUES 
+(3268, 7350, 76.467, 1, 0, 1, 1),
+(3267, 6517, 65.795, 1, 0, 1, 1),
+(3268, 6514, 37.217, 1, 0, 1, 1),
+(3268, 2963, 20.639, 1, 0, 1, 1);
