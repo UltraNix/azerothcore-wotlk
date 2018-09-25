0be7ccbfe7fca1fbd3d01842fd70c66ac4628177
Dootz <dootz.sunwell@gmail.com>
2018-09-25 15:24:17 +0200

    DB/NPC: Gjalerbron Warrior - add AI

---
+++ sql/updates/world/2018_09_25_scripts_gjalerbron_warrior.sql
@@ -0,0 +1,3 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 23991 AND `id` = 1;
+INSERT INTO `smart_scripts` VALUES
+(23991, 0, 1, 0, 0, 0, 100, 0, 10000, 12000, 10000, 15000, 11, 10966, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Combat - Cast Uppercut');
