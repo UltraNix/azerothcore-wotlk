57f38ab522eabd53c7e826fe687d3dd517da7e11
Riztazz <felianther15@gmail.com>
2018-09-28 10:48:16 +0200

    add missing query to sql

--- sql/updates/world/2018_09_28_00_remove_instance_bind_flag.sql
+++ sql/updates/world/2018_09_28_00_remove_instance_bind_flag.sql
@@ -1,1 +1,3 @@
 UPDATE `creature_template` SET `flags_extra` = `flags_extra` & ~1 WHERE `entry` IN (34796, 35438, 35439, 35440, 35144, 35511, 35512, 35513, 34799, 35514, 35515, 35516);
+
+UPDATE `gameobject` SET `spawnTimeSecs` = 86000 WHERE `guid` = 151175;
