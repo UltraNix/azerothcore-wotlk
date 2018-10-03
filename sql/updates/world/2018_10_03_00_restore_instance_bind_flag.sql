f008daced13a6427ed3d6893341e17966b417b79
Riztazz <felianther15@gmail.com>
2018-10-03 09:02:16 +0200

    Actually, set bind flags on northrend beasts only for heroic version of the encounter

--- sql/updates/world/2018_10_03_00_restore_instance_bind_flag.sql
+++ sql/updates/world/2018_10_03_00_restore_instance_bind_flag.sql
@@ -1,1 +1,3 @@
-UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 1 WHERE `entry` IN (34796, 35438, 35439, 35440, 35144, 35511, 35512, 35513, 34799, 35514, 35515, 35516);
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` & ~1 WHERE `entry` IN (34796, 35438, 35439, 35440, 35144, 35511, 35512, 35513, 34799, 35514, 35515, 35516);
+
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 1 WHERE `entry` IN (35439, 35440, 35512, 35513, 35515, 35516);
