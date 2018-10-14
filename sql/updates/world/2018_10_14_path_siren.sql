6983db585b33b28df8768caa7c72a2efac161b56
Dootz <dootz.sunwell@gmail.com>
2018-10-14 22:35:10 +0200

    DB/NPC: Coilskar Siren - add path

---
+++ sql/updates/world/2018_10_14_path_siren.sql
@@ -0,0 +1,5 @@
+DELETE FROM `creature_addon` WHERE `guid` IN (70713, 70714, 70715);
+INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
+(70713, 707130, 0, 0, 0, 0, ''),
+(70714, 707140, 0, 0, 0, 0, ''),
+(70715, 707150, 0, 0, 0, 0, '');
