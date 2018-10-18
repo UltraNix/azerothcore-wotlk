2b0c6ed666cb81fe556e1896a18dba27d0417740
Dootz <dootz.sunwell@gmail.com>
2018-10-18 22:02:52 +0200

    DB/NPC: Headless horseman - add missing items to loot table

---
+++ sql/updates/world/2018_10_18_loot_horseman.sql
@@ -0,0 +1,5 @@
+DELETE FROM `creature_loot_template` WHERE `entry` = 23682 AND `item` IN (37012, 38175, 33808);
+INSERT INTO `creature_loot_template` VALUES 
+(23682, 37012, 1, 1, 3, 1, 1),
+(23682, 33808, 3, 1, 3, 1, 1),
+(23682, 38175, 3, 1, 3, 1, 1);
