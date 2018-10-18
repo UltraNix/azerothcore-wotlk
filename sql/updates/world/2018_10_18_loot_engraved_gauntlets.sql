0b1996cf5b381e9b45332d5293fe32939decb080
Dootz <dootz.sunwell@gmail.com>
2018-10-18 22:02:11 +0200

    DB/Item: Add Engrave gauntlets to correct loot tables

---
+++ sql/updates/world/2018_10_18_loot_engraved_gauntlets.sql
@@ -0,0 +1,4 @@
+DELETE FROM `reference_loot_template` WHERE `entry` = 24033 AND `item` = 10232;
+DELETE FROM `reference_loot_template` WHERE `entry` = 34003 AND `item` = 10232;
+INSERT INTO `reference_loot_template` VALUES 
+(34003, 10232, 0, 1, 1, 1, 1);
