f91ffa987cda00583a9dd3027d90ecc34dfb6dce
Dootz <dootz.sunwell@gmail.com>
2018-11-15 20:31:50 +0100

    DB/Item: Update deisenchant_loot_template. Some items will now disenchant into 2-3 Infinite dust instead of 1-2

---
+++ sql/updates/world/2018_11_15_disenchant_loot.sql
@@ -0,0 +1,2 @@
+UPDATE `disenchant_loot_template` SET `mincountOrRef` = 2, `maxcount` = 3 WHERE `entry` = 15 AND `item` = 34054;
+UPDATE `disenchant_loot_template` SET `mincountOrRef` = 2, `maxcount` = 3 WHERE `entry` = 34 AND `item` = 34054;
