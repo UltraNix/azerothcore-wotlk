d20b9f940d3b569b8cbf13ffc20771bc2248555b
Dootz <dootz.sunwell@gmail.com>
2019-03-03 21:11:20 +0100

    DB/Event: Kaluak Fishing Derby & Fishing Extravaganza will start at 2pm server time

---
+++ sql/updates/world/2019_03_03_01_game_event.sql
@@ -0,0 +1,2 @@
+UPDATE `game_event` SET `start_time` = "2019-03-10 14:00:00" WHERE `eventEntry` IN (15, 62);
+UPDATE `game_event` SET `start_time` = "2019-03-09 14:00:00" WHERE `eventEntry` IN (63,64);
