e300d536e35cd391f9c89d0706e089aa1e019645
Riztazz <felianther15@gmail.com>
2019-03-08 14:57:23 +0100

    DB/Spells: Disallow recursive procs of Permeating Chill on sindragosa encounter

---
+++ sql/updates/world/2019_03_08_spell_proc_event.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc_event` WHERE `entry` = 70107;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(70107, 0, 0, 0, 0, 0, 12582972, 0, 0, 20, 1000);
