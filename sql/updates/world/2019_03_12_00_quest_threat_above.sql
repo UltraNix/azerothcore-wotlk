d0929148e33e9fd7621eac9c8aa393622a4e8a6b
Dootz <dootz.sunwell@gmail.com>
2019-03-12 22:59:47 +0100

    DB/Quest: Threat from above - resolve issue with double quest credit

---
+++ sql/updates/world/2019_03_12_00_quest_threat_above.sql
@@ -0,0 +1,4 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 22143 AND `id` = 4;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 22144 AND `id` = 4;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 22148 AND `id` = 2;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 23022 AND `id` = 12;
