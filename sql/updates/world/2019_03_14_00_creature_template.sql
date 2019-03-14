e695a03f3e1612532101dafd6459e5c55b7596cc
Riztazz <felianther15@gmail.com>
2019-03-14 12:43:21 +0100

    DB/Creature: Update spell data for mutated abomination, together with eb4a0a31681498f12763ac3134cc2cd05ff26057 fixes lack of GCD or any CD for Regurgitated Ooze

---
+++ sql/updates/world/2019_03_14_00_creature_template.sql
@@ -0,0 +1,4 @@
+UPDATE creature_template SET spell1 = 70360, spell2 = 70539, spell3 = 70542, spell4 = 0, spell5 = 0, spell6 = 71516, spell7 = 0, spell8 = 0 WHERE `entry` = 37672;
+UPDATE creature_template SET spell1 = 72527, spell2 = 72457, spell3 = 70542, spell4 = 0, spell5 = 0, spell6 = 71516, spell7 = 0, spell8 = 0 WHERE `entry` = 38605;
+UPDATE creature_template SET spell1 = 70360, spell2 = 72875, spell3 = 70542, spell4 = 0, spell5 = 0, spell6 = 71516, spell7 = 0, spell8 = 0 WHERE `entry` = 38786;
+UPDATE creature_template SET spell1 = 72527, spell2 = 72876, spell3 = 70542, spell4 = 0, spell5 = 0, spell6 = 71516, spell7 = 0, spell8 = 0 WHERE `entry` = 38787;
