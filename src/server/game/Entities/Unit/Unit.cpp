cea2d0c181b7a07df551813bfc3cc4434051e4dc
Dootz <dootz.sunwell@gmail.com>
2019-03-14 17:23:36 +0100

    Revert: Core/AI: Creatures with flag extra civilian should not attack - resolve BT#1967

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -9542,9 +9542,6 @@ bool Unit::Attack(Unit* victim, bool meleeAttack)
     if (HasAuraType(SPELL_AURA_SPIRIT_OF_REDEMPTION))
         return false;
 
-    if (victim->ToCreature() && victim->ToCreature()->IsCivilian())
-        return false;
-
     // remove SPELL_AURA_MOD_UNATTACKABLE at attack (in case non-interruptible spells stun aura applied also that not let attack)
     if (HasAuraType(SPELL_AURA_MOD_UNATTACKABLE))
         RemoveAurasByType(SPELL_AURA_MOD_UNATTACKABLE);
