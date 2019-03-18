d77c558fbd63ae68a3254e35d197d241e8a1f9c5
Dootz <dootz.sunwell@gmail.com>
2019-03-18 21:58:11 +0100

    DB/Loot: Enchanting increase number of Infinite Dust droped from disenchanting items (from 1-2 to 2-3 and from 3-5 to 4-7)

---
+++ sql/updates/world/2019_03_18_03_loot_disenchant.sql
@@ -0,0 +1,2 @@
+UPDATE `disenchant_loot_template` SET `mincountOrRef` = 2, `maxcount` = 3 WHERE `entry` IN (15,34);
+UPDATE `disenchant_loot_template` SET `mincountOrRef` = 4, `maxcount` = 7 WHERE `entry` IN (16,35);
