dc39a2e9b66f29af8b83920dad517aa85a63debc
Dootz <dootz.sunwell@gmail.com>
2019-03-25 22:04:28 +0100

    DB/GameEvent: Make sure Fishing events will start at correct time (Daylight Saving Time Changes 2019 :( )

---
+++ sql/updates/world/2019_03_25_02_game_event.sql
@@ -0,0 +1,2 @@
+UPDATE `game_event` SET `start_time` = "2019-03-31 14:00:00" WHERE `eventEntry` IN (15, 62);
+UPDATE `game_event` SET `start_time` = "2019-03-30 14:00:00" WHERE `eventEntry` IN (63,64);
