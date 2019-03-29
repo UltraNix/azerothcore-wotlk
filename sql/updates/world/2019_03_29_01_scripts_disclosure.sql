631974fcd1a07d52cd02d5c5e85c2693933d3390
Dootz <dootz.sunwell@gmail.com>
2019-03-29 21:34:10 +0100

    DB/Quest: Disclosure - possible fix for quest credit issues

---
+++ sql/updates/world/2019_03_29_01_scripts_disclosure.sql
@@ -0,0 +1,5 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 28948 AND `id` = 1;
+UPDATE `smart_scripts` SET `link` = 4 WHERE `entryorguid` = 28948 AND `id` = 0;
+UPDATE `smart_scripts` SET `event_type` = 61, `link` = 3 WHERE `entryorguid` = 28948 AND `id` = 2;
+UPDATE `smart_scripts` SET `link` = 0 WHERE `entryorguid` = 28948 AND `id` = 3;
+UPDATE `smart_scripts` SET `link` = 2 WHERE `entryorguid` = 28948 AND `id` = 22;
