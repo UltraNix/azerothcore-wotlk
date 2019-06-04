17dabe98183bf6ac4bcc43303a922aa9b013c34f
Dootz <dootz.sunwell@gmail.com>
2019-06-04 23:32:50 +0200

    DB/Scripts: Dreghood Brute - update script, should correctly evade

---
+++ sql/updates/world/2019_06_04_01_scripts_dreghood.sql
@@ -0,0 +1,3 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 16938 AND `id` = 15;
+INSERT INTO `smart_scripts` VALUES
+(16938, 0, 15, 0, 11, 0, 100, 0, 0, 0, 0, 0, 117, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'On Respawn - Enable Evade');
