afa6cd6877616f6487aabcbec40d31a7456722c0
Piootrek <piotrek9_11@o2.pl>
2019-06-19 03:02:56 +0200

    DB/Loot: Corrections for Magisters Terrace loot https://github.com/SunwellWoW/Sunwell-WOTLK/issues/26

---
+++ sql/updates/world/2019_06_19_00_creature_loot_template.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature_loot_template` WHERE  `entry` = 25562 AND `item` = 25025 AND `lootmode` = 1;
+DELETE FROM `creature_loot_template` WHERE  `entry` = 25573 AND `item` = 25026 AND `lootmode` = 1;
+DELETE FROM `creature_loot_template` WHERE  `entry` = 25560 AND `item` = 25027 AND `lootmode` = 1;
+DELETE FROM `creature_loot_template` WHERE  `entry` = 24857 AND `item` = 25028 AND `lootmode` = 1;
