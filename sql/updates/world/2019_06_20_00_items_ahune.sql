f4d6e65e510479a6799b4850af11b00afa4f6691
Dootz <dootz.sunwell@gmail.com>
2019-06-20 22:38:21 +0200

    DB/Items: Update Ahune drop

---
+++ sql/updates/world/2019_06_20_00_items_ahune.sql
@@ -0,0 +1,7 @@
+UPDATE `item_loot_template` SET `item` = 49426 WHERE `entry` = 54536 AND `item` = 40753;
+UPDATE `item_loot_template` SET `item` = 54806 WHERE `entry` = 54536 AND `item` = 35514;
+
+UPDATE `gameobject_loot_template` SET `item` = 54804 WHERE `entry` IN (28682, 28683) AND `item` = 35494;
+UPDATE `gameobject_loot_template` SET `item` = 54802 WHERE `entry` IN (28682, 28683) AND `item` = 35495;
+UPDATE `gameobject_loot_template` SET `item` = 54801 WHERE `entry` IN (28682, 28683) AND `item` = 35496;
+UPDATE `gameobject_loot_template` SET `item` = 54805 WHERE `entry` IN (28682, 28683) AND `item` = 35497;
