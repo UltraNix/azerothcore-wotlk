31eb121e5655ca6843a9fb17b87c380c7b9bd309
Piootrek <piotrek9_11@o2.pl>
2019-07-24 02:41:18 +0200

    Core/Command: Fixed .gobject near crash on Windows

---
+++ sql/updates/world/2019_07_24_00_trinity_string.sql
@@ -0,0 +1,2 @@
+DELETE FROM `trinity_string` WHERE `entry` = 517;
+INSERT INTO `trinity_string`(`entry`, `content_default`) VALUES (517, '%d (Entry: %d) - |cffffffff|Hgameobject:%llu|h[%s X:%f Y:%f Z:%f MapId:%d]|h|r');
