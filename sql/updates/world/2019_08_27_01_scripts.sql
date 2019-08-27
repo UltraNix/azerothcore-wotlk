6cc2a92bf803cb3fc563990b41f3d00e5b8bd015
Dootz <dootz.sunwell@gmail.com>
2019-08-27 22:47:23 +0200

    DB/Scripts: Bragok - adjustments to his script

---
+++ sql/updates/world/2019_08_27_01_scripts.sql
@@ -0,0 +1,2 @@
+UPDATE `smart_scripts` SET `action_param2` = 3, `action_param3` = 20000 WHERE `entryorguid` = 16227 AND `id` IN (0,1);
+UPDATE `smart_scripts` SET `event_chance` = 30 WHERE `entryorguid` = 16227 AND `id` = 0;
