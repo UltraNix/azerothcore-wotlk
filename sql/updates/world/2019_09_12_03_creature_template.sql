a8905b7d810fdf2b18542fae03e4e00c68a719f7
Piootrek <piotrek9_11@o2.pl>
2019-09-12 18:21:11 +0200

    DB/Scripts: Missing sql changes to Karazhan Chess Event

---
+++ sql/updates/world/2019_09_12_03_creature_template.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `ScriptName` = 'chess_move_trigger' WHERE `entry` = 22519;
+UPDATE `creature_template` SET `ScriptName` = 'npc_echo_of_medivh' WHERE `entry` = 16816;
+UPDATE `creature_template` SET `ScriptName` = 'npc_chess_status' WHERE `entry` = 22520;
