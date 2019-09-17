48700ca135e3f49b4ac3644681ff02a6f6543563
Afgann <Marcin60000@gmail.com>
2019-09-17 02:33:40 +0200

    Core/Spell: Auto attack won't be skipped if there are missing resources for next swing spell.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -2231,7 +2231,7 @@ void Unit::AttackerStateUpdate (Unit* victim, WeaponAttackType attType, bool ext
         return;                                             // ignore ranged case
 
     // melee attack spell casted at main hand attack only - no normal melee dmg dealt
-    if (attType == BASE_ATTACK && m_currentSpells[CURRENT_MELEE_SPELL] && !extra)
+    if (attType == BASE_ATTACK && m_currentSpells[CURRENT_MELEE_SPELL] && !extra && m_currentSpells[CURRENT_MELEE_SPELL]->CheckCast(true) == SPELL_CAST_OK)
         m_currentSpells[CURRENT_MELEE_SPELL]->cast();
     else
     {
