88cea607464eb9038b50fe4131abcfc0ffa8eb70
Piootrek <piotrek9_11@o2.pl>
2019-10-03 21:10:54 +0200

    Core/Spells: Fixed Death Coil range https://github.com/SunwellTracker/issues/issues/2451

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -14173,7 +14173,7 @@ float Unit::GetSpellMaxRangeForTarget(Unit const* target, SpellInfo const* spell
         return spellInfo->GetMaxRange();
     if (target == NULL)
         return spellInfo->GetMaxRange(true);
-    return spellInfo->GetMaxRange(!IsHostileTo(target));
+    return spellInfo->GetMaxRange(IsFriendlyTo(target));
 }
 
 float Unit::GetSpellMinRangeForTarget(Unit const* target, SpellInfo const* spellInfo) const
@@ -14182,7 +14182,7 @@ float Unit::GetSpellMinRangeForTarget(Unit const* target, SpellInfo const* spell
         return 0;
     if (spellInfo->RangeEntry->minRangeFriend == spellInfo->RangeEntry->minRangeHostile)
         return spellInfo->GetMinRange();
-    return spellInfo->GetMinRange(!IsHostileTo(target));
+    return spellInfo->GetMinRange(IsFriendlyTo(target));
 }
 
 
