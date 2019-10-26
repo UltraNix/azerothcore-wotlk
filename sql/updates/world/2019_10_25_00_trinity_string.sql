a22d5a9c5f47ec98f24286c1dbe034b9a96a064c
Piootrek <piotrek9_11@o2.pl>
2019-10-26 09:24:09 +0200

    typo

--- sql/updates/world/2019_10_25_00_trinity_string.sql
+++ sql/updates/world/2019_10_25_00_trinity_string.sql
@@ -2,7 +2,7 @@ DELETE FROM `trinity_string` WHERE `entry` = 11037;
 INSERT INTO `trinity_string`(`entry`, `content_default`) VALUES (11037, 'Your current experience multiplier is set to: %u');
 
 DELETE FROM `trinity_string` WHERE `entry` = 11038;
-INSERT INTO `trinity_string`(`entry`, `content_default`) VALUES (11037, 'In order to change your current xp multiplier please use command .xp 1-5');
+INSERT INTO `trinity_string`(`entry`, `content_default`) VALUES (11038, 'In order to change your current xp multiplier please use command .xp 1-5');
 
 DELETE FROM `trinity_string` WHERE `entry` = 11039;
-INSERT INTO `trinity_string`(`entry`, `content_default`) VALUES (11038, 'Your experience multiplier has been changed to: %u');
+INSERT INTO `trinity_string`(`entry`, `content_default`) VALUES (11039, 'Your experience multiplier has been changed to: %u');
