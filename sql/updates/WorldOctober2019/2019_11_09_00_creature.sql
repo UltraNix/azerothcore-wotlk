1f600ed040d5edb5f06c43e3bcaa39efd43efdc3
Riztazz <felianther15@gmail.com>
2019-11-09 13:06:36 +0100

    fix positions for two creatures after kokas changes

---
+++ sql/updates/WorldOctober2019/2019_11_09_00_creature.sql
@@ -0,0 +1,2 @@
+UPDATE `creature` SET `position_x` = 1378.12, `position_y` = 1976.69, `position_z` = 13.9847 WHERE `guid` = 18158;
+UPDATE `creature` SET `position_x` = 120.852, `position_y` = -1992.2, `position_z` = 93.781 WHERE `guid` = 20478;
