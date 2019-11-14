e61783a3198096a0c758876de001545244ca28f5
Piootrek <piotrek9_11@o2.pl>
2019-11-14 16:39:46 +0100

    DB: Reserved names

---
+++ sql/updates/characters/2019_11_14_00_reserved_names.sql
@@ -0,0 +1,6 @@
+INSERT INTO `reserved_name` VALUES 
+('announcement'),('anouncement'),('anouncemennt'),('announcemenn'),('giftcoin'),('freegift'),('frosthold'),
+('announcemen'),('you'),('admin'),('freecoin'),('announce'),('anouncemen'),('adminannounc'),('adminanounce'),
+('attention'),('atention'),('message'),('mesage'),('yourgift'),('system'),('sysannounce'),('yourcoins'),('warning'),
+('sunwell'),('nightbane'),('sunwel'),('angrathar'),('administrato'),('administracj'),('gift'),('event'),('contest'),
+('youwon'),('prize'),('jesus'),('jezus'),('hitler'),('niger'),('nigger');
