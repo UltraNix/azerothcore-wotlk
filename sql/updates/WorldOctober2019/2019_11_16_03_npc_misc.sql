b026d70136a83c9ba5e8800c88d6200722592574
Dootz <dootz.sunwell@gmail.com>
2019-11-16 23:19:28 +0100

    DB/NPC: Update creature spawnpos for npcs moved by koka in-game

---
+++ sql/updates/WorldOctober2019/2019_11_16_03_npc_misc.sql
@@ -0,0 +1,2 @@
+UPDATE `creature` SET `position_x` = -17.79, `position_y` = -1762.00, `position_z` = 92.06 WHERE `guid` = 20534;
+UPDATE `creature` SET `position_x` = -845.73, `position_y` = -2661.14, `position_z` = 91.78 WHERE `guid` = 18658;
