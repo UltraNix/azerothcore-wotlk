f7e949daeadbf999a1ca88b2249f7e93c82f8db5
Dootz <dootz.sunwell@gmail.com>
2019-11-18 21:53:50 +0100

    DB/NPC: Asarnan and Dealer Senzik correct position https://github.com/SunwellTracker/issues/issues/2450

---
+++ sql/updates/WorldOctober2019/2019_11_18_02_npc_misc.sql
@@ -0,0 +1,3 @@
+-- https://github.com/SunwellTracker/issues/issues/2450 Asarnan and Dealer Senzik
+UPDATE `creature` SET `position_x` = 4196.29, `position_y` = 3019.43, `position_z` = 339.30 WHERE `guid` = 69945;
+UPDATE `creature` SET `position_x` = 4200.39, `position_y` = 2998.34, `position_z` = 339.30 WHERE `guid` = 69943;
