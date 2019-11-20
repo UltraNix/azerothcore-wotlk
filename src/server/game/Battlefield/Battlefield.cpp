5e0755df4a9b84e2e038c71cc9194db2dea54158
Riztazz <felianther15@gmail.com>
2019-11-20 09:51:16 +0100

    Update Battlefield.cpp

--- src/server/game/Battlefield/Battlefield.cpp
+++ src/server/game/Battlefield/Battlefield.cpp
@@ -165,7 +165,7 @@ bool Battlefield::Update(uint32 diff)
 {
     if (m_Timer <= diff)
     {
-        if (!IsEnabled() || (!IsWarTime() && sWorld->GetPlayerCount() > 3800)) // if WG is disabled or there is more than 3500 connections, switch automaticly
+        if (!IsEnabled() || (!IsWarTime() && sWorld->GetPlayerCount() > 4500)) // if WG is disabled or there is more than 3500 connections, switch automaticly
         {
             m_isActive = true;
             EndBattle(false);
