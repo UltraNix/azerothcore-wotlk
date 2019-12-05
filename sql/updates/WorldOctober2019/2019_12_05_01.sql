825c155275942194ab4da0adfa47731b054a7f9d
Dootz <dootz.sunwell@gmail.com>
2019-12-05 21:25:45 +0100

    DB/Item: Add drakes as rewards for Glory naxx achievements

---
+++ sql/updates/WorldOctober2019/2019_12_05_01.sql
@@ -0,0 +1,4 @@
+DELETE FROM `achievement_reward` WHERE entry IN (2137, 2138);
+INSERT INTO `achievement_reward` VALUES 
+(2137, 0, 0, 44175, 4949, 'Glory of the Rider', 'Dear $N,$B$BI hope ye\'re doing well and that ye\'ve had time to recover from our adventures in Naxxramas.$B$BWe\'ve managed to capture hatchling of plagued drake.  ... We thought perhaps you\'d accept him as a gift.$B$BMay the Elemnts guide you$BThrall, son of Durotan', 0),
+(2138, 0, 0, 44164, 32346, 'Glory of the Rider', 'Dear $N,$B$BI hope ye\'re doing well and that ye\'ve had time to recover from our adventures in Naxxramas.$B$BWe\'ve managed to capture hatchling of black drake.  ... We thought perhaps you\'d accept him as a gift.$B$BGood luck,$BJaina Proudmoore', 0);
