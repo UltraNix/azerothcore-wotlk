b64146ac08b3e082270017ba65bb49bac5a1c6a7
Piootrek <piotrek9_11@o2.pl>
2020-01-18 20:14:58 +0100

    DB/Events: Fixed Lunar Festival date https://github.com/SunwellWoW/Sunwell-WOTLK/issues/144

--- sql/updates/WorldOctober2019/2020_01_18_00_game_event.sql
+++ sql/updates/WorldOctober2019/2020_01_18_00_game_event.sql
@@ -1,5 +1,5 @@
 DELETE FROM `holiday_dates` WHERE `id` = 423 AND `date_id` = 0;
-INSERT INTO `holiday_dates` VALUES (423, 0, 336708352, 20160); -- Add event to calendar
+INSERT INTO `holiday_dates` VALUES (423, 0, 336708352, 336); -- Add event to calendar
 UPDATE `game_event` SET `start_time`='2018-02-08 12:00:00' WHERE `eventEntry`=8; -- Change game_event date
 DELETE FROM `item_loot_template` WHERE  `entry`=54537 AND `item`=40753 AND `lootmode`=1; -- Delete Emblems from Heart-Shaped Box
 DELETE FROM `reference_loot_template` WHERE `item` IN ( 51804, 51805, 51806, 51807, 51808); -- Delete 226 necks from event boss
