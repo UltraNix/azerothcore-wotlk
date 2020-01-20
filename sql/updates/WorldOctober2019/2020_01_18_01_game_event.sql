f4df4111defa05c8a47b747b2d7adfd991e30a2a
Piootrek <piotrek9_11@o2.pl>
2020-01-20 17:36:51 +0100

    Fix Lunar Festival sql

--- sql/updates/WorldOctober2019/2020_01_18_01_game_event.sql
+++ sql/updates/WorldOctober2019/2020_01_18_01_game_event.sql
@@ -1,3 +1,3 @@
 DELETE FROM `holiday_dates` WHERE `id` = 327 AND `date_id` = 2;
-INSERT INTO `holiday_dates` VALUES (327, 2, packDate(20, 1, 24, 10), 336);
+INSERT INTO `holiday_dates` VALUES (327, 2, 335921792, 336);
 UPDATE `game_event` SET `start_time`='2018-01-24 10:00:00' WHERE `eventEntry`=7;
