15dadfff37472ad5e5e0bb00bfe462f264ce3d5f
dootz <k.kozubek@wp.pl>
2020-03-03 22:52:17 +0100

    DB/NPC: [Ulduar] Freya 10 man - reduce number of mobs in trash packs

---
+++ sql/updates/WorldCustom/world_ulduar/2020_03_03_01_misc_freya_trash.sql
@@ -0,0 +1,8 @@
+-- Freya trash front group 1
+UPDATE `creature` SET `spawnMask` = 2 WHERE `guid` IN (152359,137519,137523);
+-- Freya trash left group 1
+UPDATE `creature` SET `spawnMask` = 2 WHERE `guid` IN (152362, 152360, 152361);
+-- Freya trash left group 2
+UPDATE `creature` SET `spawnMask` = 2 WHERE `guid` IN (137525,137516,137521);
+-- Freya trash right group 1
+UPDATE `creature` SET `spawnMask` = 2 WHERE `guid` IN (152363, 152364,137522);
