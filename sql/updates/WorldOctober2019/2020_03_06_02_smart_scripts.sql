a83ae5b2273d15561b8701914d20a8d52a48c99b
Piootrek <piotrek9_11@o2.pl>
2020-03-06 18:22:51 +0100

    DB/Scripts: Reduced respawn timer of Brittle Revenant

--- sql/updates/WorldOctober2019/2020_03_06_02_smart_scripts.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 30160 AND `source_type` = 0 AND `id` = 2;
-INSERT INTO `smart_scripts` (`entryorguid`, `id`, `event_type`, `action_type`, `action_param1`, `target_type`, `comment`) VALUES (30160, 2, 6, 41, 180000, 1, 'On Death - Despawn after 180s');
