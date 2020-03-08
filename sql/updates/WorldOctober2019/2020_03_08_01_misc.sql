a75425561803e4d4675face8928e1e29638be192
dootz <k.kozubek@wp.pl>
2020-03-08 23:21:13 +0100

    DB/Quest: The Path of Redemption - increase quest items drop chance https://github.com/SunwellTracker/issues/issues/2896

---
+++ sql/updates/WorldOctober2019/2020_03_08_01_misc.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -20 WHERE `entry` = 27330 AND `item` = 37931;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -20 WHERE `entry` = 27332 AND `item` = 37931;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -20 WHERE `entry` = 27333 AND `item` = 37931;
