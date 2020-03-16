79c0daafa6912941d4058b77b2f222d52b0bfa53
Riztazz <felianther15@gmail.com>
2020-03-16 17:41:25 +0100

    missing log

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -9415,18 +9415,18 @@ bool ObjectMgr::IsGameObjectStaticTransport(uint32 entry)
 
 void ObjectMgr::RequestItemDestroy( Item * item )
 {
-    if (item->IsBag())
+    if ((item->GetSlot() >= INVENTORY_SLOT_BAG_START && item->GetSlot() < INVENTORY_SLOT_BAG_END) || (item->GetSlot() >= BANK_SLOT_BAG_START && item->GetSlot() < BANK_SLOT_BAG_END))
     {
         if (Player* player = item->GetOwner())
         {
-            sLog->outBagCrash("Bag %p (GUID: %d, LowGuid: %d, Entry: %d, Slot: %d, BagSlot: %d) is requested to be destroyed for player %p (Name: %s, GUID: %d, LowGuid: %d, AccountId: %d) and stil have it?: %d",
-                item, item->GetGUID(), item->GetGUIDLow(), item->GetEntry(), item->GetSlot(), item->GetBagSlot(), player, player->GetName().c_str(), player->GetGUID(),
+            sLog->outBagCrash("Bag %p (GUID: %d, LowGuid: %d, Entry: %d, Slot: %d, BagSlot: %d, IsBag: %d) is requested to be destroyed for player %p (Name: %s, GUID: %d, LowGuid: %d, AccountId: %d) and stil have it?: %d",
+                item, item->GetGUID(), item->GetGUIDLow(), item->GetEntry(), item->GetSlot(), item->GetBagSlot(), item->IsBag(), player, player->GetName().c_str(), player->GetGUID(),
                 player->GetGUIDLow(), player->GetSession()->GetAccountId(), player->GetItemByGuid(item->GetGUID()) != nullptr);
         }
         else
         {
-            sLog->outBagCrash("Bag %p (GUID: %d, LowGuid: %d, Entry: %d, Slot: %d, BagSlot: %d) is requested to be destroyed",
-                item, item->GetGUID(), item->GetGUIDLow(), item->GetEntry(), item->GetSlot(), item->GetBagSlot());
+            sLog->outBagCrash("Bag %p (GUID: %d, LowGuid: %d, Entry: %d, Slot: %d, BagSlot: %d, IsBag: %d) is requested to be destroyed",
+                item, item->GetGUID(), item->GetGUIDLow(), item->GetEntry(), item->GetSlot(), item->GetBagSlot(), item->IsBag());
         }
     }
 
