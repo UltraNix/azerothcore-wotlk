f64f7c81ed2e4fde932f4ed0fbbd943224409f17
Piootrek <piotrek9_11@o2.pl>
2020-03-23 09:29:30 +0100

    DB/Item: Added riding requirement to Bengal Tiger

--- sql/updates/WorldOctober2019/2020_03_22_01_item_template.sql
+++ sql/updates/WorldOctober2019/2020_03_22_01_item_template.sql
@@ -1,1 +1,1 @@
-UPDATE `item_template` SET `RequiredSkill`='0', `RequiredSkillRank`='0', ScriptName = 'item_bengal_tiger', spellid_1 = 0, spellcooldown_1 = -1, spellcategory_1 = 0, spellcategorycooldown_1 = 0 WHERE  `entry`=8630;
+UPDATE `item_template` SET `RequiredSkill`=762, `RequiredSkillRank`=150, ScriptName = 'item_bengal_tiger', spellid_1 = 0, spellcooldown_1 = -1, spellcategory_1 = 0, spellcategorycooldown_1 = 0 WHERE  `entry`=8630;
